5298668088bd0be34267eedfcfd39d7eb12d0580
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-03-28 17:24:36 +0900

    [Silver IV] Title: 다음 소수, Time: 876 ms, Memory: 96840 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // 케이스 수 입력
         int caseNum = Integer.parseInt(br.readLine());
@@ -16,12 +20,19 @@ public class Main {
             BigInteger nextPrimeNumber = new BigInteger(br.readLine());
 
             // 현재 숫자 소수 판별
-            if(nextPrimeNumber.isProbablePrime(10))
-                System.out.println(nextPrimeNumber);
+            if(nextPrimeNumber.isProbablePrime(10)) {
+                sb.append(nextPrimeNumber).append("\n");
+            }
 
-            // 아닌 경우 다음 소수 찾기
-            else
-                System.out.println(nextPrimeNumber.nextProbablePrime());
+                // 아닌 경우 다음 소수 찾기
+            else {
+                sb.append(nextPrimeNumber.nextProbablePrime()).append("\n");
+            }
         }
+
+        // 결과 출력
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
