bd571b6772306f68452384ea7ca1ecab1a0b83ed
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-03-28 19:42:15 +0900

    [Silver III] Title: 수열, Time: 188 ms, Memory: 22232 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -3,42 +3,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    // 날짜 수, 연속적인 날짜의 수
-    public static int days, checkDays, prefixSum;
-    // 온도 정보 배열
-    public static int temperature[];
-    // 온도 누적합 배열
-    public static int prefixTemperature[];
-    // 최고 온도
-    public static int result;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 날짜 수, 연속적인 날짜의 수 입력
-        days = Integer.parseInt(st.nextToken());
-        checkDays = Integer.parseInt(st.nextToken());
+        // 온도 측정 전체 날 입력
+        int days = Integer.parseInt(st.nextToken());
+        int daysData[] = new int[days+1];
+        int prefixData[] = new int[days+1];
+
+        // 연속된 날 수 입력
+        int checkDays = Integer.parseInt(st.nextToken());
 
         // 온도 정보 입력
-        temperature = new int[days+1];
         st = new StringTokenizer(br.readLine());
-        for(int idx=1; idx<=days; idx++) {
-            temperature[idx] = Integer.parseInt(st.nextToken());
+        for(int d=1; d<=days; d++) {
+            daysData[d] = Integer.parseInt(st.nextToken());
         }
 
-        // 초기 누적 온도
-        prefixSum = 0;
-        for(int idx=1; idx<checkDays+1; idx++) {
-            prefixSum += temperature[idx];
+        // 누적 합 정보 입력
+        prefixData[1] = daysData[1];
+        for(int p=2; p<=days; p++) {
+            prefixData[p] = prefixData[p-1] + daysData[p];
         }
 
-        // 최고 온도 구하기
-        result = prefixSum;
-        for(int idx=checkDays+1; idx<=days; idx++) {
-            prefixSum = prefixSum + temperature[idx] - temperature[idx-checkDays];
-            result = Math.max(result,prefixSum);
+        // 온도 최대 합 구하기
+        int startIdx;
+        int endIdx;
+        int result = Integer.MIN_VALUE;
+        for(int idx=1; idx+checkDays-1<=days; idx++) {
+            startIdx = idx;
+            endIdx = idx+checkDays-1;
+            result = Math.max(result,prefixData[endIdx]-prefixData[idx]+daysData[idx]);
         }
 
         // 결과 출력
