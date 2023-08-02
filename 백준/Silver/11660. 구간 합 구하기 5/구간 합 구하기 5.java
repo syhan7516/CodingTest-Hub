4848bfb8ae6b69a9fb9cd262ee8dba9e933f4cf0
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-02 16:36:27 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1016 ms, Memory: 133308 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,67 +1,68 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.*;
 
 public class Main {
-
-    // 크기, 횟수
-    public static int boardSize, testcase;
-    // 범위
-    public static int startX, startY, endX, endY;
-    // 행렬
-    public static int board[][];
-    // 누적합 배열
-    public static int DP[][];
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        // 보드 크기, 횟수 입력
-        st = new StringTokenizer(br.readLine());
-        boardSize = Integer.parseInt(st.nextToken());
-        testcase = Integer.parseInt(st.nextToken());
-
-        // 보드 만들기
-        board = new int[boardSize+1][boardSize+1];
-
-        // 누적합 배열 만들기
-        DP = new int[boardSize+1][boardSize+1];
-
-        // 보드 정보 입력
-        for(int a=1; a<=boardSize; a++) {
-            st = new StringTokenizer(br.readLine());
-            for(int b=1; b<=boardSize; b++) {
-                board[a][b] = Integer.parseInt(st.nextToken());
-                DP[a][b] = DP[a][b-1]+board[a][b];
-            }
-        }
-
-        // 회수 만큼 수행
-        for (int t=0; t<testcase; t++) {
-
-            // 결과
-            int result = 0;
-
-            // 범위 입력
-            st = new StringTokenizer(br.readLine());
-            startX = Integer.parseInt(st.nextToken());
-            startY = Integer.parseInt(st.nextToken());
-            endX = Integer.parseInt(st.nextToken());
-            endY = Integer.parseInt(st.nextToken());
-
-            // 범위 합 구하기
-            for (int idx=startX; idx<=endX; idx++) {
-                result = result+(DP[idx][endY]-DP[idx][startY-1]);
-            }
-
-            // 결과 저장
-            sb.append(result+"\n");
-        }
-        
-        // 결과 출력
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		
+		// 표의 크기 입력
+		int boardSize = Integer.parseInt(st.nextToken());
+		
+		// 구할 횟수 입력
+		int resCnt = Integer.parseInt(st.nextToken());
+		
+		// 보드 생성
+		int board[][] = new int[boardSize+1][boardSize+1];
+		
+		// 누접합 보드 생성
+		int prefixSumBoard[][] = new int[boardSize+1][boardSize+1];
+		
+		// 보드 정보 입력
+		for(int i=1; i<=boardSize; i++) {
+			st  = new StringTokenizer(br.readLine());
+			for(int j=1; j<=boardSize; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				prefixSumBoard[i][j] = prefixSumBoard[i-1][j] + board[i][j];
+			}
+		}
+		
+		// 합을 구할 구간 입력
+		for(int i=0; i<resCnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			// 좌표 입력
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			// 좌표가 같을 경우
+			if(x1==x2 && y1==y2)
+				// 결과 저장
+				sb.append(board[x1][y1]).append("\n");
+			
+			// 좌표가 다를 경우
+			else {
+				
+				// 필요한 구간의 합
+				int BigAreaSum = 0;
+				
+				// 넓은 구간에서 불필요한 구간 빼기
+				for(int j=y1; j<=y2; j++) {
+					BigAreaSum+=prefixSumBoard[x2][j]-prefixSumBoard[x1-1][j];
+				}	
+				
+				// 결과 저장
+				sb.append(BigAreaSum).append("\n");
+			}
+		}
+		
+		// 결과 출력
+		System.out.println(sb.toString());
+	}
 }
