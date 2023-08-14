1de79cbd28afd6e4a55eee6c98384424f0584c0f
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-14 17:54:19 +0900

    [Silver V] Title: 숫자 카드, Time: 1072 ms, Memory: 145408 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,48 +1,80 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+public class Main {	
+	
+	// 카드 찾기 메서드
+	static boolean solve(int arr[], int target, int start, int end) {
+		
+		// 위치 찾기
+		while(start<=end) {
+			
+			// 중간 값
+			int mid = (start+end)/2;
+			
+			// 찾은 경우
+			if(arr[mid]==target)
+				return true;
+			
+			// 값이 작은 경우
+			else if(arr[mid]<target)
+				start = mid+1;
+			
+			// 값이 큰 경우
+			else 
+				end = mid-1;
+		}
+		
+		// 없는 경우
+		return false;
+	}
 
-        // 결과 해시
-        Map<Integer,Integer> result = new HashMap<>();
-
-        // 숫자1 개수 입력
-        int firNumCnt = Integer.parseInt(br.readLine());
-
-        // 숫자1 카드 입력
-        int firNums[] = new int[firNumCnt];
-        st = new StringTokenizer(br.readLine());
-        for(int idx=0; idx<firNumCnt; idx++) {
-            firNums[idx] = Integer.parseInt(st.nextToken());
-        }
-
-        // 숫자2 개수 입력
-        int secNumCnt = Integer.parseInt(br.readLine());
-        int number = 0;
-        st = new StringTokenizer(br.readLine());
-        int secNums[] = new int[secNumCnt];
-        for(int idx=0; idx<secNumCnt; idx++) {
-            number = Integer.parseInt(st.nextToken());
-            secNums[idx] = number;
-            result.put(number,0);
-        }
-
-        // 숫자1 확인
-        for(int idx=0; idx<firNums.length; idx++) {
-            int count = 0;
-            if(result.get(firNums[idx])!=null) {
-                count = result.get(firNums[idx])+1;
-                result.put(firNums[idx],count);
-            }
-        }
-
-        // 결과 출력
-        for(int idx=0; idx<secNumCnt; idx++) {
-            System.out.print(result.get(secNums[idx])+" ");
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		// 상근이의 숫자 카드의 개수 입력
+		int firCardCnt = Integer.parseInt(br.readLine());
+		
+		// 상근이 카드 덱 생성
+		int firCard[] = new int[firCardCnt];
+		
+		// 상근이의 숫자 카드 정보 입력
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<firCardCnt; i++) {
+			firCard[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 상대방 카드 개수
+		int secCardCnt = Integer.parseInt(br.readLine());
+		
+		// 상대방 카드 덱 생성
+		int secCard[] = new int[secCardCnt];
+		
+		// 상대방 카드 정보 입력
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<secCardCnt; i++) {
+			secCard[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 상근이 카드 정렬
+		Arrays.sort(firCard);
+		
+		// 카드 확인
+		for(int i=0; i<secCardCnt; i++) {
+			
+			// 카드 찾기
+			boolean flag = solve(firCard,secCard[i],0,firCard.length-1);
+			
+			// 결과 저장
+			if(flag) sb.append(1+" ");
+			else sb.append(0+" ");
+		}
+		
+		// 결과 출력
+		System.out.println(sb.toString());
+	}
 }
