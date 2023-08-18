412fa4e9fa7e6a9c7af40f03eec675075b220c0d
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-18 14:02:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 20588 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,49 +5,68 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	
+	// 수빈이의 위치와 동생 위치
+	public static int subin, sister;
+	// 영역 배열
+	public static int area[];
+	
+	// 동생 찾기 메서드
+	static void solve() {
 
-        // 수빈, 동생 위치 입력
-        st = new StringTokenizer(br.readLine());
-        int firPoint = Integer.parseInt(st.nextToken());
-        int secPoint = Integer.parseInt(st.nextToken());
-
-        // 장소
-        int house[] = new int[100001];
-
-        // 이동
-        int result = 0;
-        Queue<Integer> nodes = new LinkedList<>();
-        nodes.offer(firPoint);
-
-        while(!nodes.isEmpty()) {
-            int curNode = nodes.poll();
-
-            // 찾은 경우
-            if(curNode==secPoint) {
-                result = house[curNode];
-                break;
-            }
-
-            int dir[] = {curNode+1,curNode-1,curNode*2};
-
-            for(int d=0; d<3; d++) {
-                // 진행할 방향
-                int nextDir = dir[d];
-
-                // 범위 확인
-                if(nextDir<0 || nextDir>100000 || house[nextDir]!=0)
-                    continue;
-
-                // 경로 추가
-                house[nextDir] = house[curNode]+1;
-                nodes.offer(nextDir);
-            }
-        }
-
-        // 결과 출력
-        System.out.println(result);
-    }
+		// 탐색 노드 저장 큐
+		Queue<Integer> queue = new LinkedList<>();
+		
+		// 수빈이 시작점 처리
+		queue.offer(subin);
+		area[subin] = 1;
+		
+		// 찾을 때까지 반복
+		while(!queue.isEmpty()) {
+			
+			// 현재 위치
+			int current = queue.poll();
+			
+			// 동생을 찾은 경우
+			if(current==sister) return;
+			
+			// 이동 가능한 위치
+			int p[] = {current-1,current+1,current*2};
+			
+			// 진행 방향
+			for(int i=0; i<3; i++) {
+				
+				// 범위 확인
+				if(p[i]<0 || p[i]>100000) continue;
+				
+				// 이미 방문한 경우
+				if(area[p[i]]>0) continue;
+				
+				// 그 외의 경우
+				area[p[i]] = area[current]+1;
+				
+				// 탐색 큐에 저장
+				queue.offer(p[i]);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 수빈이의 위치와 동생 위치 입력
+		st = new StringTokenizer(br.readLine());
+		subin = Integer.parseInt(st.nextToken());
+		sister = Integer.parseInt(st.nextToken());
+		
+		// 영역 배열 생성
+		area = new int[100001];
+		
+		// 동생 찾기
+		solve();
+		
+		// 결과 출력
+		System.out.println(area[sister]-1);
+	}
 }
