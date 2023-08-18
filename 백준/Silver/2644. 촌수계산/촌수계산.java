951c50f0c79d2141a733ef63db3642c7b162ba15
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-18 15:05:38 +0900

    [Silver II] Title: 촌수계산, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,54 +1,89 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static List<Integer>[] relation;
-	static boolean[] checked;
-	static int res = -1;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	// 사람 수, 두 사람, 관계 수, 결과
+	public static int saramCnt, first, second, relationCnt, result;
+	// 조직도 리스트
+	public static ArrayList<ArrayList<Integer>> relation;
+	// 조직도 방문 여부 배열
+	public static boolean visited[];
+	// 계산 성공 여부
+	public static boolean flag;
+
+	// 촌수 계산하기 메서드
+	static void solve(int num, int cnt) {
 		
-		int n = Integer.parseInt(br.readLine());
-		relation = new ArrayList[n+1];
-		checked = new boolean[n+1];
-		for(int i=1; i<n+1; i++) {
-			relation[i] = new ArrayList<>();
+		// 촌수를 찾은 경우
+		if(num==second) {
+			result = cnt;
+			flag = true;
+			return;
 		}
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 현재 사람 관계 크기
+		int currentSize = relation.get(num).size();
 		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		
-		int l = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<l; i++) {
-			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			relation[p].add(c);
-			relation[c].add(p);
-		}
-		
-		dfs(x,y, 0);
-		System.out.println(res);
-	}
-	
-	static void dfs(int start, int end, int cnt) {
-		if(start == end) {
-			res = cnt;
-			return; 
-		}
-		
-		checked[start] = true;
-		for(int i=0; i<relation[start].size(); i++) { 
-			int next = relation[start].get(i);
-			if(!checked[next]) {
-				dfs(next, end, cnt+1);
+		// 현재 사람 관계 확인
+		for(int i=0; i<currentSize; i++) {
+			
+			// 상대 확인
+			int connect = relation.get(num).get(i);
+			
+			// 촌수 계산이 안된 사람인 경우
+			if(!visited[connect]) {
+				visited[connect] = true;
+				solve(connect,cnt+1);
 			}
+			
+			// 탐색이 종료된 경우
+			if(flag) return;
 		}
+	} 
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 사람 수 입력
+		saramCnt = Integer.parseInt(br.readLine());
+		
+		// 두 사람 입력
+		st = new StringTokenizer(br.readLine());
+		first = Integer.parseInt(st.nextToken());
+		second = Integer.parseInt(st.nextToken());
+		
+		// 관계 리스트 생성
+		relation = new ArrayList<>();
+		for(int r=0; r<=saramCnt; r++)
+			relation.add(new ArrayList<>());
+		
+		// 관계 수 입력
+		relationCnt = Integer.parseInt(br.readLine());
+		
+		// 관계 정보 입력
+		for(int i=0; i<relationCnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			// 양방향 저장
+			relation.get(start).add(end);
+			relation.get(end).add(start);
+		}
+		
+		// 촌수 계산하기
+		result = 0;
+		visited = new boolean[saramCnt+1];
+		solve(first,0);
+		
+		// 만약 관계가 없는 경우
+		if(result==0) result = -1;
+		
+		// 결과 출력
+		System.out.println(result);
 	}
 }
