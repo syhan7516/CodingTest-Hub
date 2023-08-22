2d73994e2821d3517b87fb9b448b4be339d3b02a
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-22 09:22:32 +0900

    [Gold III] Title: 줄 세우기, Time: 480 ms, Memory: 45688 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,63 +1,89 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    // 사람 수, 비교 수
-    public static int personCnt, compareCnt;
-    // 인접 노드 리스트
-    public static ArrayList<ArrayList<Integer>> node;
-    // 진입 차수 배열
-    public static int degree[];
-    // 차수 0 저장 큐
-    public static Queue<Integer> queue;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 사람 수, 비교 수 입력
-        personCnt = Integer.parseInt(st.nextToken());
-        compareCnt = Integer.parseInt(st.nextToken());
-
-        // 기본 셋팅
-        node = new ArrayList<>();
-        for(int idx=0; idx<=personCnt; idx++)
-            node.add(new ArrayList<>());
-
-        degree = new int[personCnt+1];
-        Arrays.fill(degree,0);
-
-        // 비교 정보 입력
-        for(int idx=0; idx<compareCnt; idx++) {
-            st = new StringTokenizer(br.readLine());
-            int startNode = Integer.parseInt(st.nextToken());
-            int endNode = Integer.parseInt(st.nextToken());
-            node.get(startNode).add(endNode);
-            degree[endNode] += 1;
-        }
-
-        // 0차수 찾기
-        queue = new LinkedList<>();
-        for(int idx=1; idx<=personCnt; idx++) {
-            if(degree[idx]==0)
-                queue.offer(idx);
-        }
-
-        // 정렬
-        while(!queue.isEmpty()) {
-            // 0차수 노드
-            int nodeNum = queue.poll();
-            // 연결 정보 확인
-            for(int connect=0; connect<node.get(nodeNum).size(); connect++) {
-                int connectNode = node.get(nodeNum).get(connect);
-                degree[connectNode] -= 1;
-                if(degree[connectNode]==0)
-                    queue.offer(connectNode);
-            }
-            // 결과 출력
-            System.out.print(nodeNum+" ");
-        }
-    }
+	
+	// 학생 수, 비교 회수
+	public static int studentCnt, compareCnt;
+	// 관계 리스트 
+	public static ArrayList<ArrayList<Integer>> relation;
+	// 들어오는 간선 수 배열
+	public static int inDegree[];
+	// 결과 저장을 위한 빌더
+	public static StringBuilder sb;
+	
+	// 줄 세우기 메서드
+	static void solve() {
+		
+		// 정렬을 위한 큐 생성
+		Queue<Integer> queue = new LinkedList<>();
+				
+		// 시작점 찾기
+		for(int i=1; i<=studentCnt; i++) {
+			if(inDegree[i]==0) {
+				queue.offer(i);
+				sb.append(i).append(" ");
+			}
+		}
+		
+		// 큐가 빌 때까지 수행
+		while(!queue.isEmpty()) {
+			
+			// 현재 확인 노드
+			int current = queue.poll();
+			
+			// 연결 관계 확인
+			for(int i=0; i<relation.get(current).size(); i++) {
+				int node = relation.get(current).get(i);
+				inDegree[node]--;
+				
+				// 진입 차수가 0이 되는 경우
+				if(inDegree[node]==0) {
+					queue.offer(node);
+					sb.append(node).append(" ");
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 학생 수, 비교 회수 입력
+		st = new StringTokenizer(br.readLine());
+		studentCnt = Integer.parseInt(st.nextToken());
+		compareCnt = Integer.parseInt(st.nextToken());
+		
+		// 관계 리스트 생성
+		relation = new ArrayList<>();
+		
+		// 관계 리스트 셋팅
+		for(int i=0; i<=studentCnt; i++) {
+			relation.add(new ArrayList<>());
+		}
+		
+		// 들어오는 간선 수 배열 생성
+		inDegree = new int[studentCnt+1];
+		
+		// 비교 정보 입력
+		for(int i=0; i<compareCnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			relation.get(from).add(to);
+			inDegree[to]++;
+		}
+		
+		// 줄 세우기 수행
+		sb = new StringBuilder();
+		solve();
+		
+		// 결과 출력
+		System.out.println(sb.toString());
+	}
 }
