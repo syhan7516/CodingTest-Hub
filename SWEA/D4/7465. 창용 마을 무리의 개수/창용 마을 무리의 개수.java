161a9792d5a8e4b69c1413fc58eb05d393907e23
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-22 13:21:49 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 128 ms, Memory: 25,932 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,82 +1,86 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-    // 사람 수, 관계 수, 결과
-    public static int humanCnt, relationCnt, result;
-    // 방문 배열
-    public static boolean visited[];
-    // 관계 정보 리스트
-    public static ArrayList<ArrayList<Integer>> relation;
-
-    // 관계 확인 함수
-    static void solve(int node) {
-
-        // 연결된 관계 확인
-        for(int n=0; n<relation.get(node).size(); n++) {
-
-            // 확인 대상
-            int compareNode = relation.get(node).get(n);
-
-            // 미방문한 경우
-            if(!visited[compareNode]) {
-                visited[compareNode] = true;
-                solve(compareNode);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        // 테이스 케이스 수 입력
-        int caseNum = Integer.parseInt(br.readLine());
-
-        // 케이스 수 만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
-
-            // 사람 수, 관계 수 입력
-            st = new StringTokenizer(br.readLine());
-            humanCnt = Integer.parseInt(st.nextToken());
-            relationCnt = Integer.parseInt(st.nextToken());
-
-            // 관계 리스트 만들기
-            relation = new ArrayList<>();
-            for(int r=0; r<=humanCnt; r++)
-                relation.add(new ArrayList<>());
-
-            // 관계 정보 입력
-            for(int r=0; r<relationCnt; r++) {
-                st = new StringTokenizer(br.readLine());
-                int firRel = Integer.parseInt(st.nextToken());
-                int secRel = Integer.parseInt(st.nextToken());
-                relation.get(firRel).add(secRel);
-                relation.get(secRel).add(firRel);
-            }
-
-            // 관계 확인
-            result = 0;
-            visited = new boolean[humanCnt+1];
-            for(int n=1; n<=humanCnt; n++) {
-
-                // 미방문인 경우
-                if(!visited[n]) {
-                    visited[n] = true;
-                    solve(n);
-                    result++;
-                }
-            }
-
-            // 결과 저장
-            sb.append("#"+(caseIdx+1)+" "+result+"\n");
-        }
-
-        // 결과 출력
-        System.out.println(sb.toString());
-    }
+	
+	// 집합 개수, 연산 개수, 대상
+	public static int cnt, operationCnt, firNum, secNum;
+	// 대표 번호 배열
+	public static int parents[];
+	// 집합 셋
+	public static HashSet<Integer> set;
+	
+	// union
+	static void union(int a, int b) {
+		
+		// 각 집합이 속한 곳 가져오기
+		a = find(a);
+		b = find(b);
+		
+		// 집합 합치기
+		parents[b] = a;
+	}
+	
+	// find
+	static int find(int a) {
+		
+		// 대표 번호가 자기 자신일 경우
+		if(parents[a]==a) return a;
+		
+		// 아닐 경우
+		return parents[a] = find(parents[a]);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		// 테스트 케이스 수 입력
+		int caseNum = Integer.parseInt(br.readLine());
+		
+		// 케이스 수 만큼 수행
+		for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+			
+			// 집합 셋 생성
+			set = new HashSet<>();
+			
+			// 집합 개수, 연산 개수 입력
+			st = new StringTokenizer(br.readLine());
+			cnt = Integer.parseInt(st.nextToken());
+			operationCnt = Integer.parseInt(st.nextToken());
+			
+			// 대표 번호 배열 생성, 초기화
+			parents = new int[cnt+1];
+			for(int i=1; i<=cnt; i++)
+				parents[i] = i;
+			
+			int firNum = 0;
+			int secNum = 0;
+			
+			for(int i=0; i<operationCnt; i++) {
+				
+				// 연산 정보 입력
+				st = new StringTokenizer(br.readLine());
+				firNum = Integer.parseInt(st.nextToken());
+				secNum = Integer.parseInt(st.nextToken());
+				
+				// union
+				union(firNum,secNum);
+			}
+			
+			// 집합 확인
+			for(int i=1; i<=cnt; i++) {
+				set.add(find(i));
+			}
+			
+			// 결과 저장
+			sb.append("#").append(caseIdx+1).append(" ").append(set.size()).append("\n");
+		}
+		
+		// 결과 출력
+		System.out.println(sb.toString());
+	}
 }
