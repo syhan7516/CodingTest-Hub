1880e8a1e189a0d8715d41f6e784e4cfce8b55f4
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-23 09:22:24 +0900

    [Silver II] Title: 나무 자르기, Time: 476 ms, Memory: 167808 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -3,66 +3,72 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    // 나무의 수, 가져갈 나무 길이 입력, 절단기 높이
-    public static int treeCnt, getTreeLen, height;
-    // 결과
-    public static long result;
-    // 나무 길이 정보 배열
-    public static int treeLenArr[];
-
-    // 절단하기 함수
-    static void getCuttingTree(int start, int end) {
-        while(start<=end) {
-
-            // 절단기 중간 높이 지정
-            int mid = (start+end)/2;
-
-            // 나무 자르기
-            long getTreeSum = 0;
-            for(int t=0; t<treeLenArr.length; t++) {
-                long treeLen = treeLenArr[t] - mid;
-                if(treeLen < 0)
-                    treeLen = 0;
-
-                getTreeSum += treeLen;
-            }
-
-            // 자른 나무 확인
-            if(getTreeSum >= getTreeLen) {
-                start = mid+1;
-                result = mid;
-            }
-            else {
-                end = mid-1;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 나무의 수, 가져갈 나무 길이 입력
-        treeCnt = Integer.parseInt(st.nextToken());
-        getTreeLen = Integer.parseInt(st.nextToken());
-
-        // 각 나무의 길이
-        treeLenArr = new int[treeCnt];
-
-        // 나무 길이 입력
-        height = 0;
-        st = new StringTokenizer(br.readLine());
-        for(int idx=0; idx<treeCnt; idx++) {
-            treeLenArr[idx] = Integer.parseInt(st.nextToken());
-            height = Math.max(height, treeLenArr[idx]);
-        }
-
-        // 절단하기
-        result = 0;
-        getCuttingTree(0,height);
-
-        // 결과 확인
-        System.out.println(result);
-    }
+	
+	// 나무의 수, 가져갈 나무 길이, 결과
+	public static int treeCnt,getLen,result;
+	// 나무 길이 배열
+	public static int tree[];
+	// 절단기 높이 조절 범위
+	public static int left, right;
+	
+	// 나무 자르기 메서드
+	static void solve() {
+		
+		while(left<=right) {
+			
+			// 절단기 높이 설정
+			int mid = (left+right)/2;
+			
+			// 잘려 나온 나무 길이
+			long cutLen = 0;
+			
+			// 나무 자르기
+			for(int t: tree) {
+				
+				// 자른 후 양수인 경우만 더하기
+				if((t-mid)>=0) cutLen += t-mid;
+				
+			}
+			
+			// 자른 나무가 길이가 부족한 경우
+			if(cutLen<getLen) {
+				right = mid-1;
+			}
+			
+			// 자른 나무가 길이가 충부한 경우
+			else {
+				result = mid;
+				left = mid+1;
+			}		
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 나무의 수, 가져갈 나무 길이 입력
+		st = new StringTokenizer(br.readLine());
+		treeCnt = Integer.parseInt(st.nextToken());
+		getLen = Integer.parseInt(st.nextToken());
+		
+		// 나무 길이 배열 생성
+		tree = new int[treeCnt];
+		
+		// 나무 길이 정보 입력
+		left = 0;
+		right = -1;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<treeCnt; i++) {
+			tree[i] = Integer.parseInt(st.nextToken());
+			right = Math.max(right,tree[i]);
+		}
+		
+		// 나무 자르기
+		solve();
+		
+		// 결과 출력
+		System.out.println(result);
+	}
 }
