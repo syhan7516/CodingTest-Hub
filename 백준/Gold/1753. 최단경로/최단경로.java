ff485164d99ea119f344972fc467cd2840dc86a5
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-25 14:54:28 +0900

    [Gold IV] Title: 최단경로, Time: 1332 ms, Memory: 128128 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -7,115 +7,113 @@ import java.util.StringTokenizer;
 
 // 노드 클래스
 class Node implements Comparable<Node> {
-    private int node;
-    private int dist;
-
-    public Node(int node, int dist) {
-        this.node = node;
-        this.dist = dist;
-    }
-
-    public int getNode() {
-        return this.node;
-    }
-
-    public int getDist() {
-        return this.dist;
-    }
-
-    public int compareTo(Node other) {
-        if(this.dist < other.dist)
-            return -1;
-        return 1;
-    }
+	int node;
+	int value;
+	
+	public Node(int to, int value) {
+		this.node = to;
+		this.value = value;
+	}
+	
+	public int compareTo(Node other) {
+		return this.value-other.value;
+	}
 }
 
 public class Main {
-
-    // 경로 초기값
-    public static int MAX_PATH = Integer.MAX_VALUE;
-    // 정점의 개수, 간선의 개수, 시작 노드
-    public static int nodeCnt, edgeCnt, start;
+	
+	// 상수 설정
+	public static final int INF = (int)1e9;
+	// 정점의 개수, 간선의 개수, 시작점
+	public static int v,e,start;
+	// 그래프 리스트
+	public static ArrayList<ArrayList<Node>> graph;
+	// 최단 거리 배열
+	public static int path[];
     // 방문 여부 배열
     public static boolean visited[];
-    // 노드별 최단 경로
-    public static int path[];
-    // 노드 정보 리스트
-    public static ArrayList<ArrayList<Node>> nodes;
-    // 노드 정렬을 위한 우선순위 큐
-    public static PriorityQueue<Node> priQ;
-
-    // 다익스트라
-    static void djikstra(int node) {
-        // 시작노드 초기화 & 우선 순위 큐 삽입
-        path[node] = 0;
-        priQ = new PriorityQueue<>();
-        priQ.offer(new Node(node,path[node]));
-
-        // 모든 연결 노드 확인
-        while(!priQ.isEmpty()) {
-            Node nowNode = priQ.poll();
-            int nowN = nowNode.getNode();
-            int nowD = nowNode.getDist();
+	
+	// 최단 거리 구하기 메서드
+	static void dijkstra() {
+		
+		// 최단 간선 정렬을 위한 우선 순위 큐 생성
+		PriorityQueue<Node> pQueue = new PriorityQueue<>();
+		
+        // 방문 여부 배열 생성
+        visited = new boolean[v+1];
+        
+		// 시작점 처리
+		path[start] = 0;
+		pQueue.offer(new Node(start,path[start]));
+		
+		// 큐가 빌 때까지 반복 수행
+		while(!pQueue.isEmpty()) {
+			
+			// 확인 할 노드
+			Node curNode = pQueue.poll();
             
-            // 방문했을 경우
-            if(visited[nowN]==true)
-                continue;
+            // 이미 방문한 경우
+			if(visited[curNode.node]) continue;
             
-            // 연결된 노드 확인
-            for(int n=0; n<nodes.get(nowN).size(); n++) {
-                Node conNode = nodes.get(nowN).get(n);
-                int conN = conNode.getNode();
-                int conD = conNode.getDist();
-                // 더 짧은 거리 갱신
-                path[conN] = Math.min(path[conN],nowD+conD);
-                priQ.offer(new Node(conN,path[conN]));
-            }
-            visited[nowN] = true;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        // 정점, 간선 수 입력
-        st = new StringTokenizer(br.readLine());
-        nodeCnt = Integer.parseInt(st.nextToken());
-        edgeCnt = Integer.parseInt(st.nextToken());
-
-        // 시작 노드 입력
-        start = Integer.parseInt(br.readLine());
-
-        // 기본 셋팅
-        visited = new boolean[nodeCnt+1];
-        Arrays.fill(visited,false);
-
-        path = new int[nodeCnt+1];
-        Arrays.fill(path,MAX_PATH);
-
-        nodes = new ArrayList<>();
-        for(int idx=0; idx<=nodeCnt; idx++)
-            nodes.add(new ArrayList<>());
-
-        // 인접 노드 정보 입력
-        for(int idx=0; idx<edgeCnt; idx++) {
-            st = new StringTokenizer(br.readLine());
-            int startNode = Integer.parseInt(st.nextToken());
-            int endNode = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            nodes.get(startNode).add(new Node(endNode,value));
-        }
-
-        // 다익스트라
-        djikstra(start);
-
-        // 결과 출력
-        for(int idx=1; idx<=nodeCnt; idx++) {
-            if(path[idx]==MAX_PATH)
-                System.out.println("INF");
-            else
-                System.out.println(path[idx]);
-        }
-    }
+			// 해당 노드와 인접한 노드 확인
+			for(int i=0; i<graph.get(curNode.node).size(); i++) {
+				
+				// 연결  노드, 연결 정점, 연결 가중치
+				Node connectNode = graph.get(curNode.node).get(i);
+				
+				// 최단 경로 배열 갱신
+				if(path[connectNode.node]>curNode.value+connectNode.value) {
+					path[connectNode.node] = curNode.value+connectNode.value;
+				}	
+				
+				// 큐에 저장
+				pQueue.offer(new Node(connectNode.node,path[connectNode.node]));
+			}
+            
+            // 방문 처리
+            visited[curNode.node] = true;
+		}		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 정점, 간선 개수 입력
+		st = new StringTokenizer(br.readLine());
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		
+		// 시작점 입력
+		start = Integer.parseInt(br.readLine());
+		
+		// 그래프 리스트 생성
+		graph = new ArrayList<>();
+		
+		// 그래프 초기화
+		for(int i=0; i<=v; i++)
+			graph.add(new ArrayList<>());
+		
+		// 최단 경로 배열 초기화
+		path = new int[v+1];
+		Arrays.fill(path,INF);
+		
+		// 간선 정보 입력
+		for(int i=0; i<e; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			graph.get(from).add(new Node(to,value));
+		}
+		
+		// 최단 거리 구하기
+		dijkstra();
+		 
+		// 결과 출력
+		for(int i=1; i<=v; i++) {
+			if(path[i]==INF) System.out.println("INF");
+			else System.out.println(path[i]);
+		}
+	}
 }
