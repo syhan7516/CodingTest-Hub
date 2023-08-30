24476cba6f6b5eda1dec19c4d0c1b92d88dadb03
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-08-30 16:24:35 +0900

    [Silver V] Title: 다리 놓기, Time: 148 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,47 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+	
+	// 조합 배열
+	public static int DP[][];
+	
+	// 조합 구하기 메서드
+	static int solve(int n, int c) {
+		
+		// 1인 부분인 경우
+		if(n==c || c==0) return 1;
 
-    // 다리 건설 경우의 수
-    public static int DP[][] = new int[31][31];
-    
-    // 다리 짓기 함수
-    static int makeBrige(int n, int r) {
-        
-        // 이미 저장된 값일 경우
-        if(DP[n][r]>0)
-            return DP[n][r];
-
-        // 개수와 결과 수가 같거나 0일 경우
-        if (n == r || r == 0)
-            return DP[n][r] = 1;
-
-        // 다리 짓기
-        return DP[n][r] = makeBrige(n-1,r-1)+makeBrige(n-1,r);
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        // 테스트 케이스 수 입력
-        int caseNum = Integer.parseInt(br.readLine());
-
-        // 테스트 케이스 수만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
-
-            // 사이트 수 입력
-            st = new StringTokenizer(br.readLine());
-            int firSite = Integer.parseInt(st.nextToken());
-            int secSite = Integer.parseInt(st.nextToken());
-
-            // 다리 짓기
-            int result = makeBrige(secSite,firSite);
-            
-            // 결과 출력
-            System.out.println(result);
-        }
-    }
+		// 이미 구한 경우
+		if(DP[n][c]!=0) return DP[n][c];
+		
+		// 구하지 못했던 경우
+		return DP[n][c] = solve(n-1,c-1) + solve(n-1,c);
+	}
+	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		
+		// 테스트 케이스 수 입력
+		int caseNum = sc.nextInt();
+		
+		// 케이스 수 만큼 수행
+		for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+			
+			// 동쪽, 서쪽 사이트 수 입력
+			int west = sc.nextInt();
+			int east = sc.nextInt();
+			
+			// 조합 배열 생성
+			DP = new int[east+1][west+1];
+			
+			// 조합 구하기
+			int result = solve(east,west);			
+			
+			// 결과 저장
+			sb.append(result).append("\n");
+		}
+		
+		// 결과 출력
+		System.out.println(sb.toString());
+	}
 }
