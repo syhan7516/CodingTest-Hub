9c02de0cf431a44a86fff1c8180efaf80385a676
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-10-31 21:27:55 +0900

    [Silver I] Title: 그림, Time: 388 ms, Memory: 45792 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,108 +1,110 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class PaperNode {
-    private int y;
-    private int x;
+// 노드 클래스
+class Node {
+    int y;
+    int x;
 
-    public PaperNode(int y, int x) {
+    public Node(int y, int x) {
         this.y = y;
         this.x = x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public int getX() {
-        return this.x;
     }
 }
 
 public class Main {
 
-    // 가로, 세로
-    public static int rowLen, colLen;
+    // 도화지 크기, 결과, 각 그림 크기, 그림 개수
+    public static int row, col, answer, imgSize, imgCnt;
+
     // 도화지
     public static int paper[][];
-    // 가장 넓은 그림 넓이, 그림 개수
-    public static int result, cnt;
-    // 방문 여부 배열
-    public static boolean visited[][];
 
     // 방향 벡터
-    public static int dx[] = {1,0,-1,0};
-    public static int dy[] = {0,1,0,-1};
+    public static int dy[] = {1,0,-1,0};
+    public static int dx[] = {0,-1,0,1};
 
-    // bfs 함수 정의
-    static void bfs(int row, int col) {
-        int area = 0;
-        Queue<PaperNode> nodes = new LinkedList<>();
-        nodes.offer(new PaperNode(row,col));
-        visited[row][col] = true;
+    // 그림 크기 확인 메서드
+    static void solve(int a, int b) {
 
-        // 빌 때까지 수행
-        while(!nodes.isEmpty()) {
-            PaperNode curNode = nodes.poll();
-            area += 1;
-            int curY = curNode.getY();
-            int curX = curNode.getX();
-            for(int dir=0; dir<4; dir++) {
-                int nextY = curY+dy[dir];
-                int nextX = curX+dx[dir];
+        // 큐 생성
+        Queue<Node> queue = new LinkedList<>();
 
-                // 이동 방향 제한
-                if(nextY<0 || nextY>rowLen-1 || nextX<0 || nextX>colLen-1 || paper[nextY][nextX]!=1 || visited[nextY][nextX])
+        // 첫 노드 처리
+        queue.offer(new Node(a,b));
+        paper[a][b] = 0;
+        imgSize++;
+
+        // 탐색 수행
+        while(!queue.isEmpty()) {
+
+            // 현재 탐색할 노드
+            Node curNode = queue.poll();
+
+            // 네 방향 확인
+            for(int d=0; d<4; d++) {
+                int ny = curNode.y+dy[d];
+                int nx = curNode.x+dx[d];
+
+                // 범위 밖인 경우
+                if(ny<0 || ny>row-1 || nx<0 || nx>col-1)
                     continue;
 
-                // 탐색 가능한 경우
-                nodes.offer(new PaperNode(nextY,nextX));
-                visited[nextY][nextX] = true;
+                // 그림이 아닌 경우
+                if(paper[ny][nx]!=1)
+                    continue;
+
+                // 그림인 경우
+                queue.offer(new Node(ny,nx));
+                paper[ny][nx] = 0;
+                imgSize++;
             }
         }
-
-        // 가장 넓은 그림 저장
-        result = Math.max(result,area);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        // 가로, 세로 입력
-        rowLen = Integer.parseInt(st.nextToken());
-        colLen = Integer.parseInt(st.nextToken());
+        // 도화지 크기 입력
+        st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
 
         // 도화지 생성
-        paper = new int[rowLen][colLen];
-        visited = new boolean[rowLen][colLen];
+        paper = new int[row][col];
 
-        // 그림 정보 입력
-        for(int a=0; a<rowLen; a++) {
+        // 도화지 정보 입력
+        for(int i=0; i<row; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int b=0; b<colLen; b++) {
-                paper[a][b] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<col; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 그림 크기 확인
-        cnt = 0;
-        result = 0;
-        for(int a=0; a<rowLen; a++) {
-            for(int b=0; b<colLen; b++) {
-                // 그림인 부분 확인
-                if(paper[a][b]==1 && !visited[a][b]) {
-                    bfs(a,b);
-                    cnt += 1;
+        // 도화지 탐색
+        answer = 0;
+        imgSize = 0;
+        imgCnt = 0;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+
+                // 그림 부분인 경우
+                if(paper[i][j]==1) {
+                    imgSize = 0;
+                    imgCnt++;
+                    solve(i,j);
+                    answer = Math.max(answer,imgSize);
                 }
             }
         }
 
         // 결과 출력
-        System.out.println(cnt);
-        System.out.println(result);
+        System.out.println(imgCnt+"\n"+answer);
     }
 }
