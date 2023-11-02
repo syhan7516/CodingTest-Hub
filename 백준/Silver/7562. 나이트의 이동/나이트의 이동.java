667cf436519cba425a81a16a3bcf86f36d867b7c
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-11-02 20:11:16 +0900

    [Silver I] Title: 나이트의 이동, Time: 268 ms, Memory: 68252 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -6,104 +7,108 @@ import java.util.StringTokenizer;
 
 // 노드 클래스
 class Node {
-    private int y;
-    private int x;
+    int y;
+    int x;
+    int cnt;
 
-    public Node(int y, int x) {
+    public Node(int y, int x, int cnt) {
         this.y = y;
         this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-    public int getX() {
-        return x;
+        this.cnt = cnt;
     }
 }
 
 public class Main {
 
-    // 체스판 크기
-    public static int boardSize;
+    // 체스판의 길이, 결과
+    public static int boardSize, answer;
+
+    // 목표 위치
+    public static int targetY, targetX;
+
     // 체스판
     public static int board[][];
-    // 출발점 위치
-    public static int startY;
-    public static int startX;
-    // 도착점 위치
-    public static int destY;
-    public static int destX;
+
     // 방향 벡터
-    public static int dy[] = {2,1,-1,-2,-2,-1,1,2};
-    public static int dx[] = {1,2,2,1,-1,-2,-2,-1};
+    public static int dy[] = {-2,-2,-1,1,2,2,1,-1};
+    public static int dx[] = {-1,1,2,2,1,-1,-2,-2};
 
-    // 탐색 수행 함수
-    static void bfs() {
-        Queue<Node> nodes = new LinkedList<>();
-        nodes.offer(new Node(startY,startX));
-        board[startY][startX] = 1;
+    // 말 위치 노드 큐
+    public static Queue<Node> queue;
 
-        while(!nodes.isEmpty()) {
-            Node curNode = nodes.poll();
-            int curY = curNode.getY();
-            int curX = curNode.getX();
+    // 말 이동하기 메서드
+    static void solve() {
 
-            // 주위 방향 확인
-            for(int dir=0; dir<8; dir++) {
-                int nextY = curY+dy[dir];
-                int nextX = curX+dx[dir];
+        // 말 이동하기
+        while(!queue.isEmpty()) {
+
+            // 현재 위치
+            Node curNode = queue.poll();
+
+            // 목표 위치에 도착한 경우
+            if(curNode.y==targetY && curNode.x==targetX) {
+                answer = curNode.cnt;
+                return;
+            }
+
+            // 여덟 방향 확인
+            for(int d=0; d<8; d++) {
+                int ny = curNode.y+dy[d];
+                int nx = curNode.x+dx[d];
 
                 // 범위 확인
-                if(nextY<0 || nextY>boardSize-1 || nextX<0 || nextX>boardSize-1)
+                if(ny<0 || ny>boardSize-1 || nx<0 || nx>boardSize-1)
                     continue;
 
-                // 방문 여부
-                if(board[nextY][nextX]!=0)
+                // 이미 방문한 경우
+                if(board[ny][nx]==-1)
                     continue;
 
-                // 움직임 횟수 저장
-                board[nextY][nextX] = board[curY][curX]+1;
-
-                // 도착점 위치일 경우
-                if(nextY==destY && nextX==destX) return;
-
-                // 경로 추가
-                nodes.offer(new Node(nextY,nextX));
+                // 이동
+                queue.offer(new Node(ny,nx, curNode.cnt+1));
+                board[ny][nx] = -1;
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         // 테스트 케이스 수 입력
         int caseNum = Integer.parseInt(br.readLine());
 
-        // 케이스 수만큼 수행
+        // 케이스 수 만큼 수행
         for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
 
-            // 체스판의 크기 입력
-            boardSize = Integer.parseInt(br.readLine());
-            // 체스판 만들기
-            board = new int[boardSize][boardSize];
-            // 출발 위치 입력
-            st = new StringTokenizer(br.readLine());
-            startX = Integer.parseInt(st.nextToken());
-            startY = Integer.parseInt(st.nextToken());
-            // 목적치 위치 입력
-            st = new StringTokenizer(br.readLine());
-            destX = Integer.parseInt(st.nextToken());
-            destY = Integer.parseInt(st.nextToken());
+            // 큐 생성
+            queue = new LinkedList<>();
 
-            // 탐색 수행
-            if(startY==destY && startX==destX)
-                System.out.println(0);
-            else {
-                bfs();
-                System.out.println(board[destY][destX]-1);
-            }
+            // 체스판 크기 입력
+            boardSize = Integer.parseInt(br.readLine());
+
+            // 체스판 생성
+            board = new int[boardSize][boardSize];
+
+            // 나이트 위치 입력
+            st = new StringTokenizer(br.readLine());
+            int startY = Integer.parseInt(st.nextToken());
+            int startX = Integer.parseInt(st.nextToken());
+
+            queue. offer(new Node(startY, startX,0));
+            board[startY][startX] = -1;
+
+            // 목표 위치 입력
+            st = new StringTokenizer(br.readLine());
+            targetY = Integer.parseInt(st.nextToken());
+            targetX = Integer.parseInt(st.nextToken());
+
+            // 말 이동하기
+            answer = 0;
+            solve();
+
+            // 결과 출력
+            System.out.println(answer);
         }
     }
 }
