c03e0c67f721524e1dca79d750ff009bbe87c807
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-11-05 17:26:48 +0900

    [Silver V] Title: 배열 합치기, Time: 1452 ms, Memory: 337308 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,37 +1,72 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         // 배열 크기 입력
         st = new StringTokenizer(br.readLine());
-        int firSize = Integer.parseInt(st.nextToken());
-        int secSize = Integer.parseInt(st.nextToken());
+        int size1 = Integer.parseInt(st.nextToken());
+        int size2 = Integer.parseInt(st.nextToken());
 
         // 배열 생성
-        int result[] = new int[firSize+secSize];
+        int [] arr1 = new int[size1];
+        int [] arr2 = new int[size2];
 
         // 배열 정보 입력
         st = new StringTokenizer(br.readLine());
-        for(int idx=0; idx<firSize; idx++)
-            result[idx] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<size1; i++)
+            arr1[i] = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
-        for(int idx=firSize; idx<firSize+secSize; idx++)
-            result[idx] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<size2; i++)
+            arr2[i] = Integer.parseInt(st.nextToken());
 
-        // 정렬
-        Arrays.sort(result);
+        // 배열 합치기
+        int point1 = 0;
+        int point2 = 0;
 
-        // 결과 저장
-        for(int data: result) sb.append(data+" ");
+        while(point1<size1 && point2<size2) {
+
+            if(arr1[point1]>arr2[point2]) {
+                sb.append(arr2[point2]).append(" ");
+                point2++;
+            }
+
+            else if(arr1[point1]<arr2[point2]) {
+                sb.append(arr1[point1]).append(" ");
+                point1++;
+            }
+
+            else {
+                sb.append(arr1[point1]).append(" ");
+                sb.append(arr2[point2]).append(" ");
+                point1++;
+                point2++;
+            }
+        }
+
+        // 남은 경우
+        if(point1<size1) {
+            while(point1<size1) {
+                sb.append(arr1[point1]).append(" ");
+                point1++;
+            }
+        }
+
+        else {
+            while(point2<size2) {
+                sb.append(arr2[point2]).append(" ");
+                point2++;
+            }
+        }
 
         // 결과 출력
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
