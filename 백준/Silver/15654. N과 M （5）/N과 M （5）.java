fb04930a43aa03bd91bf11b95600ee8dbf7d363e
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-11-08 21:23:26 +0900

    [Silver III] Title: N과 M (5), Time: 332 ms, Memory: 68812 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,74 +1,79 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    // 총 개수, 선택 개수
-    public static int totalCnt, selectCnt;
-    // 숫자 배열
-    public static int nums[];
-    // 결과 배열
-    public static int result[];
-    // 방문 배열
+    // 결과 저장
+    public static StringBuilder sb = new StringBuilder();
+
+    // 숫자 개수, 선택할 수의 개수
+    public static int numCnt, selectCnt;
+
+    // 주어진 수 정보 배열, 선택된 수 정보 배열
+    public static int [] nums, select;
+
+    // 수 사용 여부 배열
     public static boolean visited[];
 
-    // 숫자 선택하기 함수
+    // 수열 구하기 메서드
     static void solve(int depth) {
 
-        // 종료 조건
+        // 숫자가 다 선택된 경우
         if(depth==selectCnt) {
-            for(int p: result)
-                System.out.print(p+" ");
-            System.out.println();
+            for(int i=0; i<selectCnt; i++) {
+                sb.append(select[i]+" ");
+            }
+
+            sb.append("\n");
             return;
         }
 
-        // 탐색 진행
-        for(int n=0; n<totalCnt; n++) {
-            if(!visited[n]) {
-                visited[n] = true;
-                result[depth] = nums[n];
+        // 숫자가 덜 선택된 경우
+        for(int i=0; i<numCnt; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                select[depth] = nums[i];
                 solve(depth+1);
-                visited[n] = false;
+                visited[i] = false;
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 총 개수, 선택 개수 입력
+        // 숫자 개수, 선택할 수의 개수 입력
         st = new StringTokenizer(br.readLine());
-        totalCnt = Integer.parseInt(st.nextToken());
+        numCnt = Integer.parseInt(st.nextToken());
         selectCnt = Integer.parseInt(st.nextToken());
 
-        // 요소 정보 입력
-        nums = new int[totalCnt];
+        // 배열 생성
+        nums = new int[numCnt];
+        select = new int[selectCnt];
+        visited = new boolean[numCnt];
 
+        // 배열 정보 입력
         st = new StringTokenizer(br.readLine());
-        for(int n=0; n<totalCnt; n++) {
-            nums[n] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<numCnt; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 결과 배열
-        result = new int[selectCnt];
-
-        // 숫자 정렬
+        // 정렬
         Arrays.sort(nums);
 
-        // 숫자 선택하기
-        int depth=0;
-        visited = new boolean[totalCnt];
-        for(int n=0; n<totalCnt; n++) {
-            if(!visited[n]) {
-                visited[n] = true;
-                result[depth] = nums[n];
-                solve(depth+1);
-                visited[n] = false;
-            }
+        // 수열 구하기
+        for(int i=0; i<numCnt; i++) {
+            visited[i] = true;
+            select[0] = nums[i];
+            solve(1);
+            visited[i] = false;
         }
+
+        // 결과 출력
+        System.out.println(sb.toString());
     }
 }
