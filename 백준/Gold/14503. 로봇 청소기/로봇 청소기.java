030b17c3af28484bf92fb4ca474b792464d8962e
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-11-15 21:24:17 +0900

    [Gold V] Title: 로봇 청소기, Time: 84 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,78 +1,114 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    // 방 가로, 세로, 청소 칸
-    public static int width, height, cleanCnt;
-    // 방향 배열
-    public static int dy[] = {-1,0,1,0};
-    public static int dx[] = {0,1,0,-1};
-    // 방 정보 배열
+    // 방 크기, 로봇 위치, 바라보는 방향, 결과
+    public static int rowSize, colSize, robotRow, robotCol, robotDir, clean;
+
+    // 방
     public static int room[][];
 
-    // 청소 함수
-    static void clean(int curR, int curC, int curDir) {
-        // 청소안되어있는 경우
-        if(room[curR][curC]==0) {
-            room[curR][curC] = 2;
-            cleanCnt += 1;
-        }
+    // 방향 벡터
+    public static int dy[] = {-1,0,1,0};
+    public static int dx[] = {0,1,0,-1};
 
-        // 주위 방향 확인
-        for(int dir=0; dir<4; dir++) {
-            curDir -= 1;
-            if(curDir==-1) curDir=3;
+    // 뒷 방향 방향 벡터
+    public static int back[] = {2,3,0,1};
 
-            int ny = dy[curDir]+curR;
-            int nx = dx[curDir]+curC;
+    // 청소하기 메서드
+    static void solve() {
 
-            // 탐색 확인
-            if(ny>=0 && ny<height && nx>=0 && nx<width && room[ny][nx]==0) {
-                clean(ny,nx,curDir);
-                return;
+        // 청소 여부
+        boolean isClean = false;
+
+        while(true) {
+
+            // 현재 칸 확인
+            if(room[robotRow][robotCol]==0) {
+                room[robotRow][robotCol]=2;
+                clean++;
+            }
+
+            // 청소 여부 갱신
+            isClean = false;
+
+            // 현재 칸 기준 네 방향 확인
+            for(int d=1; d<=4; d++) {
+
+                // 확인 방향
+                int move = robotDir-d;
+                if(move<0) move += 4;
+
+                int ny = robotRow+dy[move];
+                int nx = robotCol+dx[move];
+
+                // 청소 X
+                if(room[ny][nx]==0) {
+
+                    // 방향 전환
+                    robotDir = move;
+
+                    // 로봇 이동
+                    isClean = true;
+                    robotRow = ny;
+                    robotCol = nx;
+                    break;
+                }
+            }
+
+            // 네 방향 모두 청소된 경우
+            if(!isClean) {
+
+                // 후진 방향 좌표
+                int rowBack = robotRow+dy[back[robotDir]];
+                int colBack = robotCol+dx[back[robotDir]];
+
+                // 벽이 없는 경우 후진
+                if(room[rowBack][colBack]!=1) {
+                    robotRow = rowBack;
+                    robotCol = colBack;
+                }
+
+                // 벽이 있는 경우 정지
+                else return;
             }
         }
-
-        // 청소가된 경우
-        int back = (curDir+2)%4;
-        int by = curR+dy[back];
-        int bx = curC+dx[back];
-
-        // 탐색 확인
-        if(by>=0 && by<height && bx>=0 && bx<width && room[by][bx]!=1)
-            clean(by, bx, curDir);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 가로, 세로 입력
+        // 방의 크기 입력
         st = new StringTokenizer(br.readLine());
-        height = Integer.parseInt(st.nextToken());
-        width = Integer.parseInt(st.nextToken());
-        room = new int[height][width];
+        rowSize = Integer.parseInt(st.nextToken());
+        colSize = Integer.parseInt(st.nextToken());
 
-        // 로봇 청소기 정보
+        // 로봇 위치, 방향 입력
         st = new StringTokenizer(br.readLine());
-        int curR = Integer.parseInt(st.nextToken());
-        int curC = Integer.parseInt(st.nextToken());
-        int curDir = Integer.parseInt(st.nextToken());
+        robotRow = Integer.parseInt(st.nextToken());
+        robotCol = Integer.parseInt(st.nextToken());
+        robotDir = Integer.parseInt(st.nextToken());
+
+        // 방 생성
+        room = new int[rowSize][colSize];
 
         // 방 정보 입력
-        for(int row=0; row<height; row++) {
+        for(int i=0; i<rowSize; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int col=0; col<width; col++) {
-                room[row][col] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<colSize; j++) {
+                room[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 청소 시작
-        clean(curR,curC,curDir);
+        // 청소하기
+        clean = 0;
+        solve();
 
         // 결과 출력
-        System.out.println(cleanCnt);
+        System.out.println(clean);
     }
 }
