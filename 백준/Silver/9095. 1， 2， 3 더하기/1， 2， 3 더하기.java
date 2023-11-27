1f2a41fbfeca90f7b498ef30a563a88fad2eb1e5
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-11-27 19:08:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 76 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,35 +1,53 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    // 정수, 만들 수있는 가능한 방법 수
+    public static int number, cnt;
+
+    // 정수 만들기
+    static void solve(int num) {
+
+        // 정수와 같거나 큰 경우
+        if(num>=number) {
+
+            // 같은 경우
+            if(num==number) {
+                cnt++;
+                return;
+            }
+
+            // 다른 경우
+            else return;
+        }
+
+        // 1~3 더해보기
+        for(int i=1; i<=3; i++) {
+            solve(num+i);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        // DP 배열 생성
-        int DP[] = new int[11];
-
-        // 기본 셋팅
-        DP[1] = 1;
-        DP[2] = 2;
-        DP[3] = 4;
-
-        // 방법 수 찾기
-        for(int n=4; n<11; n++) {
-            DP[n] = DP[n-3]+DP[n-2]+DP[n-1];
-        }
 
         // 테스트 케이스 수 입력
         int caseNum = Integer.parseInt(br.readLine());
 
-        // 각 케이스 수행
+        // 케이스 수 만큼 수행
         for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
 
-            // 숫자 입력
-            int num = Integer.parseInt(br.readLine());
+            // 정수 입력
+            number = Integer.parseInt(br.readLine());
+
+            // 정수 만들기
+            cnt = 0;
+            solve(0);
 
             // 결과 저장
-            sb.append(DP[num]).append("\n");
+            sb.append(cnt).append("\n");
         }
 
         // 결과 출력
