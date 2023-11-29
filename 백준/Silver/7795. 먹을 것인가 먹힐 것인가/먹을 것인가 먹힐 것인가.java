2f1dfc4a7c7679e1421693676457d60bf63266f0
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-11-29 19:31:29 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 348 ms, Memory: 39136 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,76 +1,84 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    // A, B 개수, 결과
-    public static int A, B, result;
-    // A, B 배열
-    public static int[] Aarr, Barr;
+    // 쌍 구하기
+    static int solve(int number, int B[]) {
 
-    // 탐색
-    static void search(int target) {
+        // 범위 설정
         int start = 0;
-        int end = Barr.length-1;
+        int end = B.length-1;
         int idx = -1;
 
-        // 마지막 인덱스가 첫 인덱스를 넘지 않을 때까지 반복
         while(start<=end) {
-            // 중간 인덱스 구하기
+
             int mid = (start+end)/2;
 
-            // A의 원소가 더 클 경우
-            if(target>Barr[mid]) {
+            if(number<=B[mid]) {
+                end = mid-1;
+            }
+
+            else {
                 idx = mid;
                 start = mid+1;
             }
-            // A의 원소와 같거나,더 작을 경우
-            else {
-                end = mid-1;
-            }
         }
 
-        // 근접한 인덱스를 기준으로 개수 더하기
-        result += idx+1;
+        return idx;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         // 테스트 케이스 수 입력
         int caseNum = Integer.parseInt(br.readLine());
 
-        // 케이스 수만큼 수행
+        // 케이스 수 만큼 수행
         for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
-            st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
 
-            // A, B 정보 입력
-            Aarr = new int[A];
+            // A, B 배열 크기 입력
             st = new StringTokenizer(br.readLine());
-            for(int a=0; a<A; a++)
-                Aarr[a] = Integer.parseInt(st.nextToken());
-            Barr = new int[B];
+            int Acnt = Integer.parseInt(st.nextToken());
+            int Bcnt = Integer.parseInt(st.nextToken());
+
+            // 배열 생성
+            int A[] = new int[Acnt];
+            int B[] = new int[Bcnt];
+
+            // 배열 정보 입력
             st = new StringTokenizer(br.readLine());
-            for(int b=0; b<B; b++)
-                Barr[b] = Integer.parseInt(st.nextToken());
-
-            // A, B 정렬
-            Arrays.sort(Aarr);
-            Arrays.sort(Barr);
-
-            // 범위 탐색
-            result = 0;
-            for(int e=0; e<Aarr.length; e++) {
-                search(Aarr[e]);
+            for(int i=0; i<Acnt; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<Bcnt; i++) {
+                B[i] = Integer.parseInt(st.nextToken());
             }
 
-            // 결과 출력
-            System.out.println(result);
+            // 정렬
+            Arrays.sort(B);
+
+            // 쌍 확인
+            int cnt = 0;
+            for(int i=0; i<Acnt; i++) {
+                int current = solve(A[i],B);
+
+                if(current>-1) {
+                    cnt += (current+1);
+                }
+            }
+
+            // 결과 저장
+            sb.append(cnt).append("\n");
         }
+
+        // 결과 출력
+        System.out.println(sb.toString());
     }
 }
