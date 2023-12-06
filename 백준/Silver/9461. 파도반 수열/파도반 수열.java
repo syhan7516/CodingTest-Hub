4d889071b20c00db7ac1a19c91da7f879a174109
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-12-06 23:47:40 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,29 +1,40 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
+        // 수열 정보 배열
+        long arr[] = new long[101];
+
+        // 수열 초기 값
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 2;
+        arr[5] = 2;
+        arr[6] = 3;
+
+        // 배열 정보 채우기
+        for(int i=7; i<101; i++) {
+            arr[i] = arr[i-1]+arr[i-5];
+        }
 
         // 테스트 케이스 수 입력
         int caseNum = Integer.parseInt(br.readLine());
 
-        // 케이스 수만큼 수행
+        // 케이스 수 만큼 수행
         for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
 
-            // DP 테이블 생성
-            long DP[] = new long[101];
-            DP[0] = DP[1] = DP[2] = DP[3] = 1;
-            for(int d=4; d<=100; d++) {
-                DP[d] = DP[d-2] + DP[d-3];
-            }
+            // 정수 입력
+            int number = Integer.parseInt(br.readLine());
 
-            // N 입력
-            int N = Integer.parseInt(br.readLine());
-
-            // N 값 저장
-            sb.append(DP[N]+"\n");
+            // 결과 저장
+            sb.append(arr[number]).append("\n");
         }
 
         // 결과 출력
