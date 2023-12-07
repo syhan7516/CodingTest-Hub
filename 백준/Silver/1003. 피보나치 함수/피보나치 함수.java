8f5b8f9d477e273d85c343760e0d14d5bd539063
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-12-07 17:40:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 76 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,41 +1,47 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    // 정수
+    public static int number;
+
+    // 0,1 개수 정보 배열
+    public static long DP[][];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
-        // 케이스 수 입력
-        int caseNum = Integer.parseInt(br.readLine());
-        
-        // 수 만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
-            
-            // 수 입력
-            int fibo = Integer.parseInt(br.readLine());
-            
-            
-            // DP 배열
-            int[] firFibo = new int[41];
-            int[] secFibo = new int[41];
-            
-            // 기본 셋팅
-            firFibo[0] = 1;
-            firFibo[1] = 0;
-            secFibo[0] = 0;
-            secFibo[1] = 1;
-            
-            // 수까지 확인
-            for(int n=2; n<=fibo; n++) {
-                firFibo[n] = firFibo[n-1]+firFibo[n-2];
-                secFibo[n] = secFibo[n-1]+secFibo[n-2];
-            }
-            
-            // 결과 저장
-            sb.append(firFibo[fibo] + " " + secFibo[fibo]+"\n");
+
+        // 0,1 개수 정보 배열 생성
+        DP = new long[41][2];
+
+        // 배열 초기 값
+        DP[0][0] = 1;
+        DP[1][1] = 1;
+        DP[2][0] = 1;
+        DP[2][1] = 1;
+
+        // 0,1 개수 정보 구하기
+        for(int i=3; i<=40; i++) {
+            DP[i][0] = DP[i-1][0] + DP[i-2][0];
+            DP[i][1] = DP[i-1][1] + DP[i-2][1];
         }
-        
+
+        // 테스트 케이스 수 입력
+        int caseNum = Integer.parseInt(br.readLine());
+
+        // 케이스 수 만큼 수행
+        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+
+            // 정수 입력
+            number = Integer.parseInt(br.readLine());
+
+            // 결과 저장
+            sb.append(DP[number][0]).append(" ").append(DP[number][1]).append("\n");
+        }
+
         // 결과 출력
         System.out.println(sb.toString());
     }
