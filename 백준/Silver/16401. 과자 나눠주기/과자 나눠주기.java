55a7561637a78c0835e026ff6070cd032017ffef
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-12-22 14:10:34 +0900

    [Silver II] Title: 과자 나눠주기, Time: 624 ms, Memory: 162552 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -7,14 +7,14 @@ import java.util.StringTokenizer;
 public class Main {
 
     // 과자 나누기 메서드
-    static int solve(int arr[], int cnt) {
+    static int solve(int arr[], int cnt, int maxLen) {
 
         // 결과
         int answer = 0;
 
         // 초기 시작과 끝
         int start = 1;
-        int end = arr[arr.length-1];
+        int end = maxLen;
 
         while(start<=end) {
 
@@ -31,6 +31,9 @@ public class Main {
                 if(mid<=arr[i]) {
                     current += (arr[i]/mid);
                 }
+
+                // 과자를 인원 수 만큼 자른 경우
+                if(current>=cnt) break;
             }
 
             // 사람 만큼 과자가 잘린 경우
@@ -60,16 +63,18 @@ public class Main {
         // 과자 길이 정보 배열 생성
         int snackLen[] = new int[snackCnt];
 
+        // 가장 긴 과자 길이
+        int maxLen = 0;
+
         // 과자 길이 정보 입력
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<snackCnt; i++)
+        for(int i=0; i<snackCnt; i++) {
             snackLen[i] = Integer.parseInt(st.nextToken());
-
-        // 과자 길이 정렬
-        Arrays.sort(snackLen);
+            maxLen = Math.max(maxLen,snackLen[i]);
+        }
 
         // 과자 나누기
-        int answer = solve(snackLen,saramCnt);
+        int answer = solve(snackLen,saramCnt,maxLen);
 
         // 결과 출력
         System.out.println(answer);
