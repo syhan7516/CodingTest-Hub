061c7545507ed3cb12c1c0965fc46212494edf66
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-12-22 14:25:56 +0900

    [Gold V] Title: 용액, Time: 276 ms, Memory: 31988 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,67 +1,57 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    // 용액의 수
-    public static int fluidCnt;
-    // 용액 정보 배열
-    public static int fluidArr[];
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 용액의 수 입력
-        fluidCnt = Integer.parseInt(br.readLine());
+        // 용액 개수 입력
+        int cnt = Integer.parseInt(br.readLine());
+
+        // 용액 정보 배열 생성
+        int material[] = new int[cnt];
 
         // 용액 정보 입력
-        fluidArr = new int[fluidCnt];
         st = new StringTokenizer(br.readLine());
-        for(int idx=0; idx<fluidCnt; idx++) {
-            fluidArr[idx] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<cnt; i++) {
+            material[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 기본 셋팅
-        long result = Long.MAX_VALUE;
-        int values[] = new int[2];
+        // 용액 정렬
+        Arrays.sort(material);
 
-        // 투 포인터 위치 설정
-        int firPoint = 0;
-        int secPoint = fluidArr.length-1;
+        // 결과
+        int answer = Integer.MAX_VALUE;
 
-        // 포인터 범위 제한
-        while(firPoint<secPoint) {
+        // 0에 가까운 두 용액 찾기
+        int left = 0;
+        int right = cnt-1;
+        int first = -1;
+        int second = -1;
 
-            // 현재 가르키는 두 용액의 값
-            long curSum = fluidArr[firPoint] + fluidArr[secPoint];
+        while(left<right) {
 
-            // 기존보다 0에 더 가까운 경우
-            if(Math.abs(curSum)<result) {
-                result = Math.abs(curSum);
-                values[0] = fluidArr[firPoint];
-                values[1] = fluidArr[secPoint];
+            // 용액의 합
+            int sum = material[left]+material[right];
+            int mix = Math.abs(sum);
+
+            // 결과 갱신
+            if(Math.abs(answer)>=mix) {
+                answer = sum;
+                first = material[left];
+                second = material[right];
             }
 
-            // 합이 0일 경우
-            if(curSum==0)
-                break;
-            
-            // 합이 0보다 클 경우
-            else if(curSum>0) {
-                secPoint--;
-            }
-            
-            // 합이 0보다 작을 경우
-            else 
-                firPoint++;
+            // 위치 이동
+            if(sum>0) right--;
+            else left++;
         }
-
 
         // 결과 출력
-        Arrays.sort(values);
-        System.out.println(values[0]+" "+values[1]);
+        System.out.println(first+" "+second);
     }
 }
