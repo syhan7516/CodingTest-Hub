2e6436ed8eb449aa5473cd4199f625160a469753
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2023-12-26 22:58:10 +0900

    [Silver IV] Title: 수들의 합 2, Time: 116 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,43 +1,50 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 개수, 목표 수 입력
+        // 숫자 개수, 목표 수 입력
         st = new StringTokenizer(br.readLine());
         int numCnt = Integer.parseInt(st.nextToken());
-        int goal = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
 
-        // 개수 정보 입력
+        // 숫자 정보 배열 생성
         int nums[] = new int[numCnt];
 
+        // 숫자 정보 입력
         st = new StringTokenizer(br.readLine());
-        for(int n=0; n<numCnt; n++) {
-            nums[n] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<numCnt; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 합 찾기
-        int result = 0;
+        // 결과
+        int answer = 0;
+
+        // 합 구하기
         int point = 0;
-        int sumTotal = 0;
+        long sum = 0;
 
         for(int start=0; start<numCnt; start++) {
-            while(sumTotal<goal && point<numCnt) {
-                sumTotal += nums[point];
+            while(point<numCnt && sum<target) {
+                sum += nums[point];
                 point++;
             }
 
-            // 목표와 합이 같은 경우
-            if(sumTotal==goal) result++;
+            // 경우의 수를 구한 경우
+            if(sum==target) {
+                answer++;
+            }
 
-            sumTotal -= nums[start];
+            // 맨 앞 수 빼기
+            sum -= nums[start];
         }
 
         // 결과 출력
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
