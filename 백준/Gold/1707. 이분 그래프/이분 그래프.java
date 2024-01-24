5807aa01b2f186e4d0254a433a8110d51c0b38d3
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-01-24 21:41:02 +0900

    [Gold IV] Title: 이분 그래프, Time: 936 ms, Memory: 286048 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,5 +1,5 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -8,39 +8,49 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // 인접 리스트
-    public static ArrayList<ArrayList<Integer>> nodes;
-    // 정점, 간선 수
-    public static int nodeCnt, edgeCnt;
-    // 분류 집합 배열
-    public static int setArr[];
-    // 분류 집합
-    public static int firSet, secSet;
+    // 정점 수, 간선 수
+    public static int vCnt, eCnt;
 
-    // 탐색 수행 함수
-    static boolean bfs(int node) {
+    // 연결 정보 리스트
+    public static ArrayList<ArrayList<Integer>> list;
+
+    // 소속 그룹 정보 배열
+    public static int group[];
+
+    // 이분 그래프 확인 메서드
+    static boolean solve(int node) {
+
+        // 인접 정보 저장 큐
         Queue<Integer> queue = new LinkedList<>();
-        setArr[node] = firSet;
+
+        // 시작 노드 처리
+        group[node] = 1;
         queue.offer(node);
 
+        // 모든 노드 확인
         while(!queue.isEmpty()) {
-            // 현재 노드
-            int curNode = queue.poll();
+
+            // 기준 노드
+            int current = queue.poll();
+
             // 인접한 노드 확인
-            for(int n=0; n<nodes.get(curNode).size(); n++) {
-            // 확인할 노드
-            int connectNode = nodes.get(curNode).get(n);
-                // 같은 그룹일 경우
-                if(setArr[curNode]==setArr[connectNode]) return false;
+            for(int i=0; i<list.get(current).size(); i++) {
 
-                // 아닐 경우
+                // 확인 노드
+                int n = list.get(current).get(i);
 
-                // 미방문 노드
-                if(setArr[connectNode]==0) {
-                    // 반대 그룹
-                    setArr[connectNode] = setArr[curNode]*(-1);
-                    // 큐에 저장
-                    queue.offer(connectNode);
+                // 미방문 노드인 경우
+                if(group[n]==0) {
+                    group[n] = group[current]*(-1);
+                    queue.offer(n);
+                }
+
+                // 방문된 노드인 경우
+                else {
+
+                    // 같은 집합인 경우
+                    if(group[n]==group[current])
+                        return false;
                 }
             }
         }
@@ -48,53 +58,55 @@ public class Main {
         return true;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        // 테스트 케이스 수 입력
+        // 테스트 케이스 수 입려
         int caseNum = Integer.parseInt(br.readLine());
 
-        // 케이스 수만큼 수행
+        // 케이스 수 만큼 수행
         for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
 
-            // 정점의 수, 간선의 수 입력
+            // 정점, 간선 수 입력
             st = new StringTokenizer(br.readLine());
-            nodeCnt = Integer.parseInt(st.nextToken());
-            edgeCnt = Integer.parseInt(st.nextToken());
+            vCnt = Integer.parseInt(st.nextToken());
+            eCnt = Integer.parseInt(st.nextToken());
 
-            // 인접 리스트 초기화
-            nodes = new ArrayList<>();
-            for(int n=0; n<=nodeCnt; n++)
-                nodes.add(new ArrayList<>());
+            // 연결 정보 리스트 생성
+            list = new ArrayList<>();
+            for(int i=0; i<=vCnt; i++)
+                list.add(new ArrayList<>());
 
             // 연결 정보 입력
-            for(int e=0; e<edgeCnt; e++) {
+            for(int i=0; i<eCnt; i++) {
                 st = new StringTokenizer(br.readLine());
-                int from = Integer.parseInt(st.nextToken());
+                int from= Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
-                nodes.get(from).add(to);
-                nodes.get(to).add(from);
+                list.get(from).add(to);
+                list.get(to).add(from);
             }
 
-            // 탐색 수행
-            String result = "YES";
-            setArr = new int[nodeCnt+1];
-            firSet = 1;
-            secSet = -1;
-            for(int n=1; n<=nodeCnt; n++) {
-                // 미탐색인 경우
-                if(setArr[n]==0) {
-                    // 이분 그래프가 아닌 경우
-                    if(bfs(n)==false) {
-                        result = "NO";
-                        break;
-                    }
+            // 이분 그래프 확인
+            boolean res = true;
+            group = new int[vCnt+1];
+            for(int i=1; i<=vCnt; i++) {
+
+                // 미방문 노드인 경우
+                if(group[i]==0) {
+                    res = solve(i);
+                    if(!res) break;
                 }
             }
 
-            // 결과 출력
-            System.out.println(result);
+            // 결과 확인
+            if(res) sb.append("YES");
+            else sb.append("NO");
+            sb.append("\n");
         }
+
+        // 결과 출력
+        System.out.println(sb.toString());
     }
 }
