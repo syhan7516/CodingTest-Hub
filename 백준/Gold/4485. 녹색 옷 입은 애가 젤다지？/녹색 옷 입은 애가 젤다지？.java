dfcf6b2fa5258d2d8469579d933cbd4c5ea4f527
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-02-21 13:36:09 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 184 ms, Memory: 17228 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,125 +1,124 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-// 동굴 노드 클래스
-class caveNode implements Comparable<caveNode> {
-    private int y;
-    private int x;
-    private int money;
+// 노드 클래스
+class Node implements Comparable<Node> {
+    int y;
+    int x;
+    int money;
 
-    public caveNode(int y, int x, int money) {
+    public Node(int y, int x, int money) {
         this.y = y;
         this.x = x;
         this.money = money;
     }
 
-    public int getY() {
-        return this.y;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public int getMoney() {
-        return this.money;
-    }
-
-    public int compareTo(caveNode other) {
-        if(this.money < other.money)
-            return -1;
-        return 1;
+    public int compareTo(Node other) {
+        return this.money-other.money;
     }
 }
 
 public class Main {
 
-    // 초기 루피값
-    public static int MAX_MONEY = Integer.MAX_VALUE;
-    // 동굴 크기, 결과
-    public static int caveSize;
-    // 동굴
-    public static int cave[][];
-    // 주머니 배열
-    public static int bag[][];
-    // 방향 벡터
-    public static int dx[] = {1,0,-1,0};
-    public static int dy[] = {0,1,0,-1};
+    // 루피 무한 설정
+    public static final int MAX = (int)1e9;
 
-    // 다익스트라
-    static void dijistra(int row, int col) {
-        // 우선 순위 큐
-        PriorityQueue<caveNode> nodes = new PriorityQueue<>();
-        // 시작 지점 주머니
-        bag[row][col] = cave[row][col];
-        nodes.offer(new caveNode(row,col,bag[row][col]));
-        
-        while(!nodes.isEmpty()) {
-            caveNode curNode = nodes.poll();
-            int curY = curNode.getY();
-            int curX = curNode.getX();
-            int curMoney = curNode.getMoney();
+    // 동굴의 크기
+    public static int size;
+
+    // 동굴
+    public static int map[][];
+
+    // 네 방향 벡터
+    public static int dy[] = {0,1,0,-1};
+    public static int dx[] = {1,0,-1,0};
+
+    // 최소 비용 배열
+    public static int path[][];
+
+    // 동굴 탐색 메서드
+    static void solve() {
+
+        // 노드 저장 우선 순위 큐
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+
+        // 첫 노드 처리
+        path[0][0] = map[0][0];
+        queue.offer(new Node(0,0,path[0][0]));
+
+        // 탐색 수행
+        while(!queue.isEmpty()) {
+
+            // 현재 노드
+            Node current = queue.poll();
 
             // 네 방향 확인
             for(int d=0; d<4; d++) {
-                int nextY = curY + dy[d];
-                int nextX = curX + dx[d];
-                // 방향 제한
-                if(nextY<0 || nextY>caveSize-1 || nextX<0 || nextX>caveSize-1)
-                    continue;
+                int ny = current.y + dy[d];
+                int nx = current.x + dx[d];
 
-                // 다음 이동할 위치의 기존 잃었던 돈의 값 보다 
-                // 현재 잃은 돈 + 다음 위치에서 잃을 돈이 더 작은 경우
-                if(bag[nextY][nextX]>cave[nextY][nextX]+curMoney) {
-                    bag[nextY][nextX] = cave[nextY][nextX]+curMoney;
-                    // 갱신된 위치에서 잃은 돈 및 위치 큐에 저장
-                    nodes.offer(new caveNode(nextY,nextX,bag[nextY][nextX]));
+                // 범위 확인
+                if(ny<0 || ny>size-1 || nx<0 || nx>size-1) continue;
+
+                // 이미 방문한 경우
+                if(path[ny][nx]!=MAX) continue;
+
+                // 탐색 할 노드로 추가
+                if(path[ny][nx]>path[current.y][current.x]+map[ny][nx]) {
+                    path[ny][nx] = path[current.y][current.x]+map[ny][nx];
+                    queue.offer(new Node(ny,nx,path[ny][nx]));
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        // 동굴 크기 초기값
-        caveSize = -1;
+        StringBuilder sb = new StringBuilder();
 
         // 케이스 수
-        int caseIdx= 0;
+        int caseNum = 1;
 
-        // 반복 수행
+        // 케이스 반복 수행
         while(true) {
 
             // 동굴 크기 입력
-            caveSize = Integer.parseInt(br.readLine());
-            cave = new int[caveSize][caveSize];
-            bag = new int[caveSize][caveSize];
+            size = Integer.parseInt(br.readLine());
 
-            // 종료 조건
-            if(caveSize ==0)
-                break;
+            // 크기가 0인 경우
+            if(size==0) break;
 
-            // 케이스 증가
-            caseIdx += 1;
+            // 동굴 생성
+            map = new int[size][size];
+
+            // 최단 경로 배열 생성
+            path = new int[size][size];
 
             // 동굴 정보 입력
-            for(int row = 0; row< caveSize; row++) {
+            for(int i=0; i<size;i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int col = 0; col< caveSize; col++) {
-                    cave[row][col] = Integer.parseInt(st.nextToken());
-                    bag[row][col] = MAX_MONEY;
+                for(int j=0; j<size; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    path[i][j] = MAX;
                 }
             }
 
-            // 동굴 탈출
-            dijistra(0,0);
+            // 동굴 탐색
+            solve();
 
-            // 결과 출력
-            System.out.println("Problem "+(caseIdx)+": "+bag[caveSize-1][caveSize-1]);
+            // 결과 저장
+            sb.append("Problem ").append(caseNum).append(": ")
+                    .append(path[size-1][size-1]).append("\n");
+
+            // 케이스 수 증가
+            caseNum++;
         }
+
+        // 결과 출력
+        System.out.println(sb.toString());
     }
 }
