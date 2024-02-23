6d1bc5be64502243ca531c8e0b1bea18088c1ef4
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-02-23 12:45:44 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 264 ms, Memory: 28708 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -1,62 +1,70 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    // 노드의 수, 루트 노드, 결과
-    public static int nodeCnt, rootNode, result;
-    // 트리
-    public static ArrayList<Integer>[] tree;
-    // 부모, 깊이 배열
-    public static int [] parents, depths;
-    // 공통 조상 확인할 두 노드
-    public static int firNode, secNode;
-    // 방문 배열
-    public static boolean visited[];
+    // 노드 수, 관계 수, 결과, 찾을 두 노드
+    public static int nodeCnt, relCnt, answer, findA, findB;
 
-    // 깊이 및 부모 노드 저장 함수
-    static void dfs(int current, int depth, int parent) {
-        // 해당 노드 깊이 저장
-        depths[current] = depth;
-        // 해당 노드 부모 저장
-        parents[current] = parent;
+    // 연결 관계 리스트
+    public static ArrayList<ArrayList<Integer>> relation;
 
-        for(int nextNode : tree[current]) {
-            dfs(nextNode,depth+1,current);
+    // 조상을 담을 리스트
+    public static ArrayList<Integer> aList, bList;
+
+    // 공통 조상 리스트 조회 메서드
+    static void find(int node, ArrayList<Integer> list) {
+
+        // 자기 자신 추가
+        list.add(node);
+
+        // 부모가 있는 경우
+        if(!relation.get(node).isEmpty())
+            find(relation.get(node).get(0),list);
+    }
+
+    // 공통 조상 찾기 메서드
+    static void solve() {
+
+        // 조상을 담을 리스트 생성
+        aList = new ArrayList<>();
+        bList = new ArrayList<>();
+        
+        // 조상 찾기
+        find(findA,aList);
+        find(findB,bList);
+        
+        // 더 높은 조상을 앞으로 배치
+        Collections.reverse(aList);
+        Collections.reverse(bList);
+
+        // 조상 탐색
+        int index = 0;
+
+        while(index<aList.size() && index<bList.size()) {
+
+            // 조상 가져오기
+            int a = aList.get(index);
+            int b = bList.get(index);
+
+            // 같은 경우
+            if(a==b)
+                answer = aList.get(index);
+
+            // 다른 경우
+            else return;
+
+            // 인덱스 증가
+            index++;
         }
     }
 
-    // LCA 함수
-    static void LCA(int firNode, int secNode) {
-
-        // 각 노드 깊이 확인
-        int firDepth = depths[firNode];
-        int secDepth = depths[secNode];
-
-        // 깊이 맞춰주기
-        while(firDepth>secDepth) {
-            firNode = parents[firNode];
-            firDepth--;
-        }
-        while(secDepth>firDepth) {
-            secNode = parents[secNode];
-            secDepth--;
-        }
-
-        // 공통 조상 찾을 때까지 계층 오르기
-        while(firNode!=secNode) {
-            firNode = parents[firNode];
-            secNode = parents[secNode];
-        }
-
-        // 결과 저장
-        result = firNode;
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
@@ -64,53 +72,39 @@ public class Main {
         // 테스트 케이스 수 입력
         int caseNum = Integer.parseInt(br.readLine());
 
-        // 테스트 케이스 수만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+        // 케이스 수 만큼 수행
+        for(int caseIdx=1; caseIdx<=caseNum; caseIdx++) {
 
-            // 노드의 수 입력
+            // 노드 수 입력
             nodeCnt = Integer.parseInt(br.readLine());
 
-            // 트리 만들기
-            tree = new ArrayList[nodeCnt+1];
-            for(int t=1; t<=nodeCnt; t++)
-                tree[t] = new ArrayList<>();
+            // 관계 리스트 생성
+            relation = new ArrayList<>();
+            for (int i = 0; i <= nodeCnt; i++)
+                relation.add(new ArrayList<>());
 
-            // 부모, 깊이 배열 만들기
-            parents = new int[nodeCnt+1];
-            depths = new int[nodeCnt+1];
-            visited = new boolean[nodeCnt+1];
-            Arrays.fill(depths,-1);
-            Arrays.fill(visited,true);
+            // 관계 수 입력
+            relCnt = nodeCnt-1;
 
-            // 트리 정보 입력
-            for(int e=0; e<nodeCnt-1; e++) {
+            // 관계 정보 입력
+            for (int i = 0; i <relCnt; i++) {
                 st = new StringTokenizer(br.readLine());
-                int p = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
-                tree[p].add(c);
-                visited[c] = false;
+                int parent = Integer.parseInt(st.nextToken());
+                int child = Integer.parseInt(st.nextToken());
+                relation.get(child).add(parent);
             }
 
-            // 루트 노드 찾기
-            rootNode = 0;
-            for(int n=1; n<=nodeCnt; n++) {
-                if(visited[n]) rootNode = n;
-            }
-
-            // 깊이 및 부모 노드 저장
-            dfs(rootNode,1,0);
-
-            // 공통 조상 확인할 두 노드 입력
+            // 찾을 두 노드 입력
             st = new StringTokenizer(br.readLine());
-            firNode = Integer.parseInt(st.nextToken());
-            secNode = Integer.parseInt(st.nextToken());
+            findA = Integer.parseInt(st.nextToken());
+            findB = Integer.parseInt(st.nextToken());
 
             // 공통 조상 찾기
-            result = 0;
-            LCA(firNode,secNode);
+            answer = -1;
+            solve();
 
             // 결과 저장
-            sb.append(result+"\n");
+            sb.append(answer).append("\n");
         }
 
         // 결과 출력
