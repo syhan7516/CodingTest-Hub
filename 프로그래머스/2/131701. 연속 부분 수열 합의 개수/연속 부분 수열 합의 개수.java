3d0b35a23696da86af5f50fb9bed668d528c3ca4
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-06-21 00:22:55 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 168.69 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,31 +1,52 @@
 import java.util.*;
 
 class Solution {
+    
+    // 합 저장 셋
+    static HashSet<Integer> set;
+    
+    // 각 개수마다 연속 부분 수열 합 구하기 메서드
+    static void solve(int cnt, int[] elements) {
+        
+        // 합
+        int sum = 0;
+        
+        // 초기 합 구하기
+        for(int i=0; i<cnt; i++)
+            sum += elements[i];
+        set.add(sum);
+        
+        // 이후 합 구하기
+        for(int i=0; i<elements.length-1; i++) {
+            
+            // 가장 앞 부분 빼기
+            sum -= elements[i];
+            
+            // 인덱스가 범위를 넘은 경우
+            if(cnt>=elements.length) 
+                cnt -= elements.length; 
+            
+            // 다음 수 더하기
+            sum += elements[cnt];
+            set.add(sum);
+            
+            // 인덱스 증가
+            cnt++;
+        }
+    }
+    
     public int solution(int[] elements) {
         
+        // 합 저장 셋 생성
+        set = new HashSet<>();
+        
+        // 각 개수마다 연속 부분 수열 합 구하기
+        for(int i=1; i<=elements.length; i++)
+            solve(i,elements);
+        
         // 결과
-        int answer = 0;
+        int answer = set.size();
         
-        // 원소 개수
-        int cnt = elements.length;
-    
-        // 부분 수열 합 셋
-        HashSet<Integer> set = new HashSet<>();
-        
-        // 부분 수열 합 구하기
-        for(int i=0; i<cnt; i++) {
-            int current = elements[i];
-            set.add(current);
-            for(int j=1; j<cnt; j++) {
-                int idx = i+j;
-                if(idx>cnt-1) idx -= cnt;
-                current += elements[idx];
-                set.add(current);
-            }
-        }
-        
-        // 결과 반환
-        answer = set.size();
         return answer;
     }
 }
