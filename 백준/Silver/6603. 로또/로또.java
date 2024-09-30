810494f52b4da8b3564f7f081c6878df6d427d01
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-09-30 11:04:09 +0900

    [Silver II] Title: 로또, Time: 72 ms, Memory: 11796 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -5,27 +5,40 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // 결과 저장
-    public static StringBuilder sb = new StringBuilder();
+    // 로또 번호 개수
+    public static final int LOTTO_COUNT = 6;
 
     // 숫자 개수
     public static int numCnt;
 
-    // 숫자 정보 배열
+    // 숫자 배열
     public static int nums[];
 
-    // 로또 번호 뽑기 메서드
-    static void solve(int cnt, int idx, String answer) {
+    // 방문 여부 배열
+    public static boolean visited[];
+
+    // 빌더
+    public static StringBuilder sb = new StringBuilder();
+
+    // 로또 만들기 메서드
+    public static void solve(int idx, int cnt) {
 
         // 숫자를 다 고른 경우
-        if(cnt==6) {
-            sb.append(answer).append("\n");
+        if(cnt==LOTTO_COUNT) {
+
+            // 선택된 숫자 저장
+            for(int i=0; i<numCnt; i++)
+                if(visited[i]) sb.append(nums[i]).append(" ");
+            sb.append("\n");
+
             return;
         }
 
-        // 덜 고른 경우
+        // 숫자 고르기
         for(int i=idx; i<numCnt; i++) {
-            solve(cnt+1,i+1,answer+nums[i]+" ");
+            visited[i] = true;
+            solve(i+1,cnt+1);
+            visited[i] = false;
         }
     }
 
@@ -33,26 +46,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 테스트 반복
         while(true) {
 
             // 숫자 개수 입력
             st = new StringTokenizer(br.readLine());
             numCnt = Integer.parseInt(st.nextToken());
 
-            // 0인 경우
+            // 숫자 개수가 0개인 경우
             if(numCnt==0) break;
 
-            // 0이 아닌 경우
+            // 숫자 배열 생성
             nums = new int[numCnt];
+
+            // 방문 여부 배열 생성
+            visited = new boolean[numCnt];
+
+            // 숫자 입력
             for(int i=0; i<numCnt; i++)
                 nums[i] = Integer.parseInt(st.nextToken());
 
-            // 숫자 뽑기
-            for(int i=0; i<numCnt; i++) {
-                solve(1,i+1,nums[i]+" ");
-            }
-
+            // 로또 만들기
+            solve(0,0);
             sb.append("\n");
         }
 
