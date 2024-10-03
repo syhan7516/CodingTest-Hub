5e79b63a545e73b67cd277692c70c809476083e4
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-10-03 17:49:49 +0900

    [Silver V] Title: 색종이, Time: 68 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,48 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		// 색종이 수 입력
-		int paperCnt = Integer.parseInt(br.readLine());
-		
-		// 도화지 만들기
-		int BigPaper[][] = new int[101][101];
-		
-		// 결과
-		int sum = 0;
-		
-		// 색종이 정보 입력
-		for(int i=0; i<paperCnt; i++) {
-			
-			st = new StringTokenizer(br.readLine());
-			
-			// 좌표 입력
-			int start = 100-Integer.parseInt(st.nextToken())-10;
-			int end = Integer.parseInt(st.nextToken());
-			
-			// 탐색 마지막 범위 설정
-			int startEnd = start+10;
-			int endEnd = end+10;
-			
-			// 종이 붙이기
-			for(int j=start; j<startEnd; j++) {
-				for(int k=end; k<endEnd; k++) {
-					
-					// 0인 경우 갱신
-					if(BigPaper[j][k]==0) {
-						BigPaper[j][k] = 1;
-						sum++;
-					} 
-				}
-			}
-		}
-		
-		// 결과 출력
-		System.out.println(sum);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 결과
+        int answer = 0;
+
+        // 도화지 배열 생성
+        boolean drawingPaper[][] = new boolean[101][101];
+
+        // 색종이 수 입력
+        int colorPaperCnt = Integer.parseInt(br.readLine());
+
+        // 색종이 정보 입력
+        for(int i=0; i<colorPaperCnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int startY = Integer.parseInt(st.nextToken());
+            int startX = Integer.parseInt(st.nextToken());
+            int endY = startY+10;
+            int endX = startX+10;
+            
+            for(int j=startY; j<endY; j++) {
+                for(int k=startX; k<endX; k++) {
+                    if(!drawingPaper[j][k]) {
+                        drawingPaper[j][k] = true;
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        // 결과
+        System.out.println(answer);
+    }
 }
