cae52a793f4f617d3f035e046a674724c67ab120
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-10-04 18:07:55 +0900

    [Bronze I] Title: 색종이, Time: 92 ms, Memory: 17208 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.java
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -1,46 +1,59 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        // 색종이 수 입력
+        // 평면 배열 생성
+        int visited[][] = new int[1002][1002];
+
+        // 색종이 개수
         int paperCnt = Integer.parseInt(br.readLine());
 
-        // 각 색종이 넓이 배열
-        int paper[] = new int[paperCnt+1];
+        // 결과 해시 생성
+        HashMap<Integer,Integer> area = new HashMap<>();
 
-        // 큰 종이
-        int board[][] = new int[1001][1001];
-
-        // 종이 정보 입력
-        for(int idx=1; idx<=paperCnt; idx++) {
+        // 위치 정보 입력
+        for(int i=1; i<=paperCnt; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int width = Integer.parseInt(st.nextToken());
-            int height = Integer.parseInt(st.nextToken());
-            
-            // 큰 종이에 종이 붙이기
-            for(int a=x; a<width+x; a++) {
-                for(int b=y; b<height+y; b++) {
-                    board[a][b] = idx;
+            int startY = Integer.parseInt(st.nextToken());
+            int startX = Integer.parseInt(st.nextToken());
+            int endY = startY+Integer.parseInt(st.nextToken());
+            int endX = startX+Integer.parseInt(st.nextToken());
+
+
+            // 색종이 넓이
+            int current = 0;
+
+            // 종이 올리기
+            for(int j=startY; j<endY; j++) {
+                for(int k=startX; k<endX; k++) {
+
+                    // 아무것도 없는 경우
+                    if(visited[j][k]!=0)
+                        area.put(visited[j][k], area.get(visited[j][k])-1);
+
+                    // 방문 처리
+                    visited[j][k] = i;
+                    current++;
                 }
             }
+
+            // 결과 저장
+            area.put(i,current);
         }
 
-        // 각 종이 넓이 확인
-        for(int a=0; a<1001; a++) {
-            for(int b=0; b<1001; b++) {
-                paper[board[a][b]] += 1;
-            }
+        // 결과 출력
+        for(int i=1; i<=paperCnt; i++) {
+            int result = area.get(i);
+            if(result<0) System.out.println(0);
+            else System.out.println(result);
         }
-
-        // 결과 확인
-        for(int idx=1; idx<=paperCnt; idx++)
-            System.out.println(paper[idx]);
     }
 }
