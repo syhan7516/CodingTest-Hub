f59b95d374133b403d57af1c916e1cb46da0b602
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-10-04 18:30:40 +0900

    [Bronze I] Title: 색종이, Time: 460 ms, Memory: 18432 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.java
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -8,28 +8,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
         // 평면 배열 생성
-        int visited[][] = new int[1002][1002];
+        int visited[][] = new int[1001][1001];
 
         // 색종이 개수
         int paperCnt = Integer.parseInt(br.readLine());
 
-        // 결과 해시 생성
-        HashMap<Integer,Integer> area = new HashMap<>();
+        // 색종이 넓이 배열 생성
+        int area[] = new int[paperCnt+1];
 
         // 위치 정보 입력
         for(int i=1; i<=paperCnt; i++) {
             st = new StringTokenizer(br.readLine());
-            int startY = Integer.parseInt(st.nextToken());
             int startX = Integer.parseInt(st.nextToken());
-            int endY = startY+Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
             int endX = startX+Integer.parseInt(st.nextToken());
-
-
-            // 색종이 넓이
-            int current = 0;
+            int endY = startY+Integer.parseInt(st.nextToken());
 
             // 종이 올리기
             for(int j=startY; j<endY; j++) {
@@ -37,23 +32,19 @@ public class Main {
 
                     // 아무것도 없는 경우
                     if(visited[j][k]!=0)
-                        area.put(visited[j][k], area.get(visited[j][k])-1);
+                        area[visited[j][k]]--;
 
                     // 방문 처리
                     visited[j][k] = i;
-                    current++;
+                    area[i]++;
                 }
             }
-
-            // 결과 저장
-            area.put(i,current);
         }
 
         // 결과 출력
         for(int i=1; i<=paperCnt; i++) {
-            int result = area.get(i);
-            if(result<0) System.out.println(0);
-            else System.out.println(result);
+            if(area[i]<0) System.out.println(0);
+            else System.out.println(area[i]);
         }
     }
 }
