4db532de1f71a07dc1512ab8eec33e6dd9acfe3e
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-11-08 00:18:19 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,59 +1,57 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	// 재료의 개수
-	public static int ingredientCnt;
-	// 결과
-	public static long result;
-	// 재료 맛 정도 배열
-	public static int taste[][];
-	// 재료 방문 여부 배열
-	public static boolean v[];
-	
-	// 요리하기 메서드
-	static void solve(int cnt, long sour, long bitter, int selectCnt) {
-		
-		// 종료 조건 : 재료를 다 골랐을 경우
-		if(cnt==ingredientCnt) {
-			
-			// 재료가 하나도 없는 경우
-			if(selectCnt==0) return;
-			
-			// 아닌 경우
-			result = Math.min(result, Math.abs(bitter-sour));
-			return;
-		}
-		
-		// 재료 고르기
-		solve(cnt+1, sour*taste[cnt][0], bitter+taste[cnt][1], selectCnt+1);
-		solve(cnt+1, sour, bitter, selectCnt);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		// 재료의 개수 입력
-		ingredientCnt = Integer.parseInt(br.readLine());
-		
-		// 재료 맛 정도 배열
-		taste = new int[ingredientCnt][2];
-		
-		// 재료 정보 입력
-		for(int i=0; i<ingredientCnt; i++) {		
-			st = new StringTokenizer(br.readLine());
-			taste[i][0] = Integer.parseInt(st.nextToken());
-			taste[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 요리하기
-		result = Long.MAX_VALUE;
-		solve(0,1,0,0);
-		
-		// 결과 출력
-		System.out.println(result);
-	}
+
+    // 결과, 재료 개수
+    public static int answer, ingredientCount;
+
+    // 재료 배열
+    public static int ingredients[][];
+
+    // 재료 선택하기 메서드
+    public static void solve(int index, int selectedCount, int s, int ss) {
+
+        // 재료 선택이 완료된 경우
+        if(index==ingredientCount) {
+
+            // 재료 선택이 하나도 안된 경우
+            if(selectedCount==0) return;
+
+            // 결과 갱신
+            answer = Math.min(answer,Math.abs(s-ss));
+            return;
+        }
+
+        // 재료 선택, 미선택
+        solve(index+1,selectedCount+1,s*ingredients[index][0],ss+ingredients[index][1]);
+        solve(index+1,selectedCount,s,ss);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 재료 개수 입력
+        ingredientCount = Integer.parseInt(br.readLine());
+
+        // 재료 배열 생성
+        ingredients = new int[ingredientCount][2];
+
+        // 재료 정보 입력
+        for(int ingredient=0; ingredient<ingredientCount; ingredient++) {
+            st = new StringTokenizer(br.readLine());
+            ingredients[ingredient][0] = Integer.parseInt(st.nextToken());
+            ingredients[ingredient][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 재료 선택하기
+        answer = (int)1e9;
+        solve(0,0,1,0);
+
+        // 결과 출력
+        System.out.println(answer);
+    }
 }
