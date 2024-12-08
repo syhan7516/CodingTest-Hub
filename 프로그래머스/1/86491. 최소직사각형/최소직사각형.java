c5ca38b7eaa5d4c2e33b597aae0b727877c2e6f6
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-12-08 12:42:09 +0900

    [level 1] Title: 최소직사각형, Time: 2.18 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,26 @@
 class Solution {
     public int solution(int[][] sizes) {
         
-        // 이전 가로, 세로 크기
-        int prevMax = 0;
-        int prevMin = 0;
-
-        // 다른 명함 확인
-        for (int i=0; i<sizes.length; i++) {
-            int curMax = Math.max(sizes[i][0], sizes[i][1]);
-            int curMin = Math.min(sizes[i][0], sizes[i][1]);
-
-            prevMax = Math.max(curMax, prevMax);
-            prevMin = Math.max(curMin, prevMin);
+        // 가로, 세로 가장 큰 크기
+        int maxRow = 0;
+        int maxCol = 0;
+        
+        // 크기 순회
+        for(int card=0; card<sizes.length; card++) {
+            
+            // 가로보다 세로가 더 큰 경우
+            if(sizes[card][0]<sizes[card][1]) {
+                int mock = sizes[card][0];
+                sizes[card][0] = sizes[card][1];
+                sizes[card][1] = mock;
+            }
+            
+            // 더 큰지 확인
+            maxRow = Math.max(maxRow,sizes[card][0]);
+            maxCol = Math.max(maxCol,sizes[card][1]);
+            
         }
 
-        // 결과
-        int answer = prevMax * prevMin;
-        
-        return answer;
+        return maxRow * maxCol;
     }
 }
