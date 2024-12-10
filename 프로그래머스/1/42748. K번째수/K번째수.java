6ee01f5fe42c651a57375edfecf539dae17206f6
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-12-10 12:03:57 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,27 @@
 import java.util.*;
 
 class Solution {
-    
     public int[] solution(int[] array, int[][] commands) {
         
-        // 결과 배열 생성
-        int[] answer = new int[commands.length];
+        // 결과
+        int answer[] = new int[commands.length];
         
-        // 각 케이스 확인
-        for(int t=0; t<commands.length; t++) {
+        // 명령 순회
+        for(int command=0; command<commands.length; command++) {
             
-            // 확인할 배열
-            int curArr[] = Arrays.copyOfRange(array, commands[t][0]-1, commands[t][1]);
+            // 명령 정보
+            int start = commands[command][0]-1;
+            int end = commands[command][1];
+            int point = commands[command][2]-1;
             
-            // 배열 정렬
-            Arrays.sort(curArr);
+            // 자르기
+            int arr[] = Arrays.copyOfRange(array,start,end);
             
-            // K번째 뽑기
-            answer[t] = curArr[commands[t][2]-1];
+            // 정렬
+            Arrays.sort(arr);
             
+            // 값 저장
+            answer[command] = arr[point];        
         }
         
         return answer;
