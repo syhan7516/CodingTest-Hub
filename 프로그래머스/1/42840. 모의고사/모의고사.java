d394f4024f1480cb31477754b4a391c9d1bd961e
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-12-22 19:35:55 +0900

    [level 1] Title: 모의고사, Time: 2.47 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,43 +1,58 @@
 import java.util.*;
 
 class Solution {
+    
+    // 정답 개수
+    public static int maxCount, firstIndex, secondIndex, thirdIndex;
+    
+    // 제출 배열
+    public static int orms[][] = {{1,2,3,4,5},
+                                 {2,1,2,3,2,4,2,5},
+                                 {3,3,1,1,2,2,4,4,5,5}};
+    
+    // 정답 개수 배열
+    public static int answerCount[];
+    
+    // 답 확인 메서드
+    public static boolean isSolve(int order, int index, int answer) {
+        
+        // 수포자 답
+        int point = index>=orms[order].length ? index%orms[order].length : index;
+        
+        // 답 확인
+        if(orms[order][point]==answer)
+            return true;
+        
+        return false;
+    }
+    
     public int[] solution(int[] answers) {
         
-        // 1번
-        int one[] = {1,2,3,4,5};
-        // 2번
-        int two[] = {2,1,2,3,2,4,2,5};
-        // 3번
-        int three[] = {3,3,1,1,2,2,4,4,5,5};
-        
-        // 정답 수
-        int aOne = 0;
-        int aTwo = 0;
-        int aThree = 0; 
 
-        // 결과
-        ArrayList<Integer> res = new ArrayList<>();
+        // 정답 개수 배열 생성
+        answerCount = new int[3];
         
-        // 채점
-        for(int i=0; i<answers.length; i++) {
-            if(one[i%5]==answers[i]) aOne++;
-            if(two[i%8]==answers[i]) aTwo++;
-            if(three[i%10]==answers[i]) aThree++;
+        // 답 확인
+        for(int index=0; index<answers.length; index++) {
+            for(int orm=0; orm<orms.length; orm++) {
+                if(isSolve(orm,index,answers[index])) {
+                    answerCount[orm]++;
+                    maxCount = Math.max(maxCount,answerCount[orm]);
+                }
+            }
         }
         
-        // 가장 높은 점수 확인
-        int best = Math.max(Math.max(aOne,aTwo),aThree);
-        
-        // 세 명 점수 확인
-        if(aOne==best) res.add(1);
-        if(aTwo==best) res.add(2);
-        if(aThree==best) res.add(3);
-        
-        // 결과 저장
-        int[] answer = new int[res.size()];
-        for(int i=0; i<res.size(); i++)
-            answer[i] = res.get(i);
-        
+        // 확인
+        ArrayList<Integer> saram = new ArrayList<>();
+        for(int index=0; index<answerCount.length; index++) {            
+            if(answerCount[index]==maxCount)
+                saram.add(index+1);
+        }
+                
+        // 결과
+        int[] answer = new int[saram.size()];
+        for(int index=0; index<answer.length; index++)
+            answer[index] = saram.get(index);
         return answer;
     }
 }
