646180288a7c8f41917c91a11faa5875e8ba6a37
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-12-31 10:55:28 +0900

    [Gold IV] Title: 이분 그래프, Time: 868 ms, Memory: 285952 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -8,48 +8,48 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // 정점 수, 간선 수
-    public static int vCnt, eCnt;
+    // 정점 개수, 간선 개수
+    public static int nodeCount, edgeCount;
 
-    // 연결 정보 리스트
-    public static ArrayList<ArrayList<Integer>> list;
+    // 집합 배열
+    public static int []group;
 
-    // 소속 그룹 정보 배열
-    public static int group[];
+    // 연결 관계 리스트
+    public static ArrayList<ArrayList<Integer>> relations;
 
     // 이분 그래프 확인 메서드
-    static boolean solve(int node) {
+    public static boolean solve(int node) {
 
-        // 인접 정보 저장 큐
+        // 인접 정보 저장 큐 생성
         Queue<Integer> queue = new LinkedList<>();
 
         // 시작 노드 처리
         group[node] = 1;
         queue.offer(node);
 
-        // 모든 노드 확인
+        // 노드 확인
         while(!queue.isEmpty()) {
 
             // 기준 노드
-            int current = queue.poll();
+            int currentNode = queue.poll();
 
-            // 인접한 노드 확인
-            for(int i=0; i<list.get(current).size(); i++) {
+            // 인접 노드 확인
+            for(int index=0; index<relations.get(currentNode).size(); index++) {
 
-                // 확인 노드
-                int n = list.get(current).get(i);
+                // 인접 노드
+                int connectNode = relations.get(currentNode).get(index);
 
                 // 미방문 노드인 경우
-                if(group[n]==0) {
-                    group[n] = group[current]*(-1);
-                    queue.offer(n);
+                if(group[connectNode]==0) {
+                    group[connectNode] = group[currentNode]*(-1);
+                    queue.offer(connectNode);
                 }
 
-                // 방문된 노드인 경우
+                // 방문한 경우
                 else {
 
                     // 같은 집합인 경우
-                    if(group[n]==group[current])
+                    if(group[currentNode]==group[connectNode])
                         return false;
                 }
             }
@@ -63,47 +63,50 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        // 테스트 케이스 수 입려
-        int caseNum = Integer.parseInt(br.readLine());
+        // 테스트 케이스 수 입력
+        int caseCount = Integer.parseInt(br.readLine());
 
-        // 케이스 수 만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+        // 테스트 케이스 수행
+        for(int caseIndex=0; caseIndex<caseCount; caseIndex++) {
 
             // 정점, 간선 수 입력
             st = new StringTokenizer(br.readLine());
-            vCnt = Integer.parseInt(st.nextToken());
-            eCnt = Integer.parseInt(st.nextToken());
+            nodeCount = Integer.parseInt(st.nextToken());
+            edgeCount = Integer.parseInt(st.nextToken());
 
-            // 연결 정보 리스트 생성
-            list = new ArrayList<>();
-            for(int i=0; i<=vCnt; i++)
-                list.add(new ArrayList<>());
+            // 집합 배열 생성
+            group = new int[nodeCount+1];
+
+            // 연결 관계 리스트 생성, 초기화
+            relations = new ArrayList<>();
+            for(int index=0; index<=nodeCount; index++)
+                relations.add(new ArrayList<>());
 
             // 연결 정보 입력
-            for(int i=0; i<eCnt; i++) {
+            for(int index=0; index<edgeCount; index++) {
                 st = new StringTokenizer(br.readLine());
-                int from= Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                list.get(from).add(to);
-                list.get(to).add(from);
+                int node1 = Integer.parseInt(st.nextToken());
+                int node2 = Integer.parseInt(st.nextToken());
+                relations.get(node1).add(node2);
+                relations.get(node2).add(node1);
             }
 
             // 이분 그래프 확인
-            boolean res = true;
-            group = new int[vCnt+1];
-            for(int i=1; i<=vCnt; i++) {
+            boolean flag = true;
+            for(int index=1; index<=nodeCount; index++) {
 
-                // 미방문 노드인 경우
-                if(group[i]==0) {
-                    res = solve(i);
-                    if(!res) break;
-                }
+                // 방문 여부 확인
+                if(group[index]==0)
+                    flag = solve(index);
+
+                // 실패한 경우
+                if(!flag) break;
             }
 
-            // 결과 확인
-            if(res) sb.append("YES");
-            else sb.append("NO");
-            sb.append("\n");
+            // 결과 저장
+            if(flag) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
+
         }
 
         // 결과 출력
