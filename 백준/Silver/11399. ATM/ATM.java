6fcf1017194faa0a540cba7f573b5860815a3843
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-01 16:44:01 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11832 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,36 +1,40 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-		
-		// 사람 수 입력
-		int human = Integer.parseInt(br.readLine());
 
-		// 인출 시간 정보 입력
-		int time[] = new int[human];
-		st = new StringTokenizer(br.readLine());
-		for(int t=0; t<human; t++) {
-			time[t] = Integer.parseInt(st.nextToken());
-		}
-
-		// 인출 시간 오름 차순 정렬
-		Arrays.sort(time);
-
-		// 시간 구하기
-		int result = 0;
+        // 결과
         int answer = 0;
-		for(int r=0; r<human; r++) {
-			result += time[r];
-			time[r] = result;
-            answer += result;
-		}
 
-		// 결과 출력
-		System.out.println(answer);
-	}
+        // 사람 수 입력
+        int saramCount = Integer.parseInt(br.readLine());
+
+        // 시간 정보 배열 생성
+        int times[] = new int[saramCount];
+
+        // 시간 정보 입력
+        st = new StringTokenizer(br.readLine());
+        for(int time=0; time<saramCount; time++) {
+            times[time] = Integer.parseInt(st.nextToken());
+        }
+
+        // 정렬
+        Arrays.sort(times);
+
+        // 시간 확인
+        int waitTime = 0;
+        for(int time=0; time<saramCount; time++) {
+            waitTime += times[time];
+            answer += waitTime;
+        }
+
+        // 결과 출력
+        System.out.println(answer);
+    }
 }
