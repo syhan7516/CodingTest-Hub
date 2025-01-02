77f9e104fe976846cb18484c75b107e95b7f5de8
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-02 17:10:52 +0900

    [Gold IV] Title: 직사각형으로 나누기, Time: 72 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Gold/1451. 직사각형으로 나누기/직사각형으로 나누기.java
+++ 백준/Gold/1451. 직사각형으로 나누기/직사각형으로 나누기.java
@@ -1,117 +1,153 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    // 가로, 세로
-    public static int rowSize, colSize;
     // 결과
-    public static long result;
-    // 직사각형
-    public static long square[][];
-    // 연산을 위한 배열
-    static long squareSum[][];
+    public static long answer;
 
-    // 연산 함수
-    static long getOperation(int a, int b, int c, int d) {
-        long operation = squareSum[c][d]-squareSum[c][b-1]-squareSum[a-1][d]+squareSum[a-1][b -1];
-        return operation;
+    // 가로, 세로 크기
+    public static int rowSize, colSize;
+
+    // 사각형 배열
+    public static int[][] square;
+
+    // 누적합 배열
+    public static long[][] prefixSum;
+
+    // 임시 배열
+    public static long[] mock;
+    public static int[] divide;
+
+    public static long findSum(int x1, int y1, int x2, int y2) {
+        
+        if(x1==0 && y1==0) {
+            return prefixSum[y2][x2];
+        } 
+        else if(x1==0) {
+            return prefixSum[y2][x2]-prefixSum[y1-1][x2];
+        } 
+        else if(y1==0) {
+            return prefixSum[y2][x2]-prefixSum[y2][x1-1];
+        }
+
+        return prefixSum[y2][x2]-prefixSum[y2][x1-1]-prefixSum[y1-1][x2]+prefixSum[y1-1][x1-1];
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void divideRow(int count, int index) {
+        if(count>=2) {
+            mock[0] = findSum(0,0,colSize-1,divide[0]-1);
+            mock[1] = findSum(0,divide[0],colSize-1,divide[1]-1);
+            mock[2] = findSum(0,divide[1],colSize-1,rowSize-1);
+            answer = Math.max(answer,mock[0]*mock[1]*mock[2]);
+            return;
+        }
+
+        for (int point=index; point+1<rowSize; point++) {
+            divide[count] = point+1;
+            divideRow(count+1,point+1);
+        }
+    }
+
+    public static void divideCol(int count, int index) {
+        if(count>=2) {
+            mock[0] = findSum(0,0,divide[0]-1,rowSize-1);
+            mock[1] = findSum(divide[0],0,divide[1]-1,rowSize-1);
+            mock[2] = findSum(divide[1],0,colSize-1,rowSize-1);
+            answer = Math.max(answer,mock[0]*mock[1]*mock[2]);
+            return;
+        }
+
+        for (int point=index; point+1<colSize; point++) {
+            divide[count] = point+1;
+            divideCol(count+1,point+1);
+        }
+    }
+
+    public static void dividePoint() {
+        for (int rowIndex=1; rowIndex<=rowSize-1; rowIndex++) {
+            for (int colIndex=1; colIndex<=colSize-1; colIndex++) {
+                
+                // ㅗ
+                mock[0] = findSum(0,0,colIndex-1,rowIndex-1);
+                mock[1] = findSum(colIndex,0,colSize-1,rowIndex-1);
+                mock[2] = findSum(0,rowIndex,colSize-1,rowSize-1);
+                answer = Math.max(answer,mock[0]*mock[1]*mock[2]);
+
+                // ㅜ
+                mock[0] = findSum(0,0,colSize-1,rowIndex-1);
+                mock[1] = findSum(0,rowIndex,colIndex-1,rowSize-1);
+                mock[2] = findSum(colIndex,rowIndex,colSize-1,rowSize-1);
+                answer = Math.max(answer,mock[0]*mock[1]*mock[2]);
+
+                // ㅏ
+                mock[0] = findSum(0,0,colIndex-1,rowSize-1);
+                mock[1] = findSum(colIndex,0,colSize-1,rowIndex-1);
+                mock[2] = findSum(colIndex,rowIndex,colSize-1,rowSize-1);
+                answer = Math.max(answer,mock[0]*mock[1]*mock[2]);
+
+                // ㅓ
+                mock[0] = findSum(0,0,colIndex-1,rowIndex-1);
+                mock[1] = findSum(0,rowIndex,colIndex-1,rowSize-1);
+                mock[2] = findSum(colIndex,0,colSize-1,rowSize-1);
+                answer = Math.max(answer,mock[0]*mock[1]*mock[2]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 가로, 세로 입력
+        // 가로, 세로 크기 입력
         st = new StringTokenizer(br.readLine());
         rowSize = Integer.parseInt(st.nextToken());
         colSize = Integer.parseInt(st.nextToken());
 
-        // 직사각형
-        square = new long[rowSize+1][colSize+1];
+        // 배열 생성
+        square = new int[rowSize][colSize];
+        prefixSum = new long[rowSize][colSize];
+        mock = new long[3];
 
-        // 직사각형 정보 입력
-        for (int a=1; a<=rowSize; a++) {
-            String line = " "+br.readLine();
-            for (int b=1; b<=colSize; b++) {
-                square[a][b] = line.charAt(b)-'0';
+        // 사각형 정보 입력
+        for(int rowIndex=0; rowIndex<rowSize; rowIndex++) {
+            String line = br.readLine();
+            for(int colIndex=0; colIndex<colSize; colIndex++) {
+                square[rowIndex][colIndex] = line.charAt(colIndex)-'0';
+
+                //
+                if (rowIndex==0 && colIndex==0) {
+                    prefixSum[0][0] = square[0][0];
+                }
+                else if(rowIndex==0) {
+                    prefixSum[0][colIndex] = prefixSum[0][colIndex-1]+square[rowIndex][colIndex];
+                }
+                else if(colIndex==0) {
+                    prefixSum[rowIndex][0] = prefixSum[rowIndex-1][0]+square[rowIndex][colIndex];
+                }
             }
         }
 
-        // 연산 직사각형 배열
-        squareSum = new long[rowSize+1][colSize+1];
-        for (int a=1; a<=rowSize; a++) {
-            for (int b=1; b<=colSize; b++) {
-                squareSum[a][b] = squareSum[a-1][b]+squareSum[a][b-1]-squareSum[a-1][b-1]+(long)square[a][b];
+        // 누적합 구하기
+        for (int rowIndex=1; rowIndex<rowSize; rowIndex++) {
+            for (int colIndex=1; colIndex<colSize; colIndex++) {
+                prefixSum[rowIndex][colIndex] 
+                        = prefixSum[rowIndex][colIndex-1]
+                        +prefixSum[rowIndex-1][colIndex]
+                        -prefixSum[rowIndex-1][colIndex-1]
+                        +square[rowIndex][colIndex];
             }
         }
-
-        // 결과
-        result = 0;
-
-        // 1번 연산
-        for (int a=1; a<=colSize-2; a++) {
-            for (int b=a+1; b<=colSize-1 ; b++) {
-                long first = getOperation(1,1,rowSize,a);
-                long second = getOperation(1,a+1,rowSize,b);
-                long third = getOperation(1,b+1,rowSize,colSize);
-
-                // 최대값 갱신
-                result = Math.max(result,first*second*third);
-            }
-        }
-
-        // 2번 연산
-        for (int a=1; a<=rowSize-2; a++) {
-            for (int b=a+1; b<=rowSize-1; b++) {
-                long first = getOperation(1,1,a,colSize);
-                long second = getOperation(a+1,1,b,colSize);
-                long third = getOperation(b+1,1,rowSize,colSize);
-
-                // 최대값 갱신
-                result = Math.max(result,first*second*third);
-            }
-        }
-
-        // 3번, 4번, 5번, 6번 연산
-        for (int a=1; a<=rowSize-1; a++) {
-            for (int b=1; b<=colSize-1; b++) {
-
-                // 3번
-                long first = getOperation(1,1,rowSize,b);
-                long second = getOperation(1,b+1,a,colSize);
-                long third = getOperation(a+1,b+1,rowSize,colSize);
-
-                // 최대값 갱신
-                result = Math.max(result,first*second*third);
-
-                // 4번
-                first = getOperation(1,1,a,b);
-                second = getOperation(a+1,1,rowSize,b);
-                third = getOperation(1,b+1,rowSize,colSize);
-
-                // 최대값 갱신
-                result = Math.max(result,first*second*third);
-
-                // 5번
-                first = getOperation(1,1,a,colSize);
-                second = getOperation(a+1,1,rowSize,b);
-                third = getOperation(a+1,b+1,rowSize,colSize);
-
-                // 최대값 갱신
-                result = Math.max(result,first*second*third);
-
-                // 6번
-                first = getOperation(1,1,a,b);
-                second = getOperation(1,b+1,a,colSize);
-                third = getOperation(a+1,1,rowSize,colSize);
-
-                // 최대값 갱신
-                result = Math.max(result,first*second*third);
-            }
-        }
+        
+        answer = 0;
+        divide = new int[2];
+        divideRow(0, 0);
+        divideCol(0, 0);
+        dividePoint();
 
         // 결과 출력
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
