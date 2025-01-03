86e1d870f8f5d11d7e22f21aaa5657070692fbca
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-03 17:13:27 +0900

    [Gold IV] Title: 가르침, Time: 264 ms, Memory: 13128 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,101 +1,108 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    // 존재 단어 수, 학습 단어 수, 결과
-    public static int existCnt, studyCnt, result;
-    // 단어 종류 배열
-    public static String word[];
-    // 알파벳 존재 여부 배열
+    // 결과, 단어 개수, 학습 글자 수
+    public static int answer, wordCount, possibleCount;
+
+    // 알파벳 학습 여부 배열
     public static boolean visited[];
 
-    // 학습 단어 확인 함수
-    static void getStudy(int idx, int depth) {
+    // 단어 배열
+    public static String words[];
 
-        // 단어를 다 찾은 경우
-        if(depth==studyCnt-5) {
-            // 해당 알파벳으로 학습 가능한 단어 수
+    // 필수 학습 알파벳
+    public static char required[] = {'a','n','t','i','c'};
+
+    // 단어 확인 메서드
+    public static void solve(int index, int selectedCount) {
+
+        // 선택이 완료된 경우
+        if(selectedCount==possibleCount) {
+
+            // 학습 가능한 단어 수
             int count = 0;
-            // 단어 가져오기
-            for(int a=0; a<existCnt; a++) {
-                // 단어 학습 가능 여부
-                boolean check = true;
-                // 단어 알파벳 가져오기
-                for(int b = 0; b< word[a].length(); b++) {
-                    // 해당 알파벳이 존재하지 않은 경우
-                    if(!visited[word[a].charAt(b)-'a']) {
-                        check = false;
+
+            // 단어 확인
+            for(int word=0; word<wordCount; word++) {
+
+                // 통과 여부
+                boolean flag = true;
+
+                for(int alpha=0; alpha<words[word].length(); alpha++) {
+
+                    // 확인 문자
+                    char letter = words[word].charAt(alpha);
+
+                    // 문자가 없는 경우
+                    if(!visited[letter-'a']) {
+                        flag = false;
                         break;
                     }
                 }
 
-                // 학습 가능한 경우
-                if(check) count++;
+                // 통과한 경우
+                if(flag) count++;
             }
 
-            // 학습 가능한 최대 단어 수 갱신
-            result = Math.max(result,count);
+            // 결과 갱신
+            answer = Math.max(answer,count);
             return;
         }
 
-        // 단어 찾기
-        for(int v=idx; v<26; v++) {
-            // 미방문 알파벳인 경우
-            if(!visited[v]) {
-                visited[v] = true;
-                getStudy(v+1,depth+1);
-                visited[v] = false;
+        // 선택하기
+        for(int alpha=index; alpha<26; alpha++) {
+
+            // 미학습한 경우
+            if(!visited[alpha]) {
+                visited[alpha] = true;
+                solve(alpha+1,selectedCount+1);
+                visited[alpha] = false;
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 존재 단어 수, 학습 단어 수 입력
+        // 단어 개수, 학습 글자 수 입력
         st = new StringTokenizer(br.readLine());
-        existCnt = Integer.parseInt(st.nextToken());
-        studyCnt = Integer.parseInt(st.nextToken());
+        wordCount = Integer.parseInt(st.nextToken());
+        possibleCount = Integer.parseInt(st.nextToken());
 
-        // 단어 입력
-        word = new String[existCnt];
-        for(int v=0; v<existCnt; v++) {
-            String reStr = br.readLine();
-            reStr = reStr.replace("anta","");
-            reStr = reStr.replace("tica","");
-            word[v] = reStr;
+        // 단어 배열 생성
+        words = new String[wordCount];
+
+        // 단어 저장
+        for(int word=0; word<wordCount; word++) {
+            st = new StringTokenizer(br.readLine());
+            String letter = st.nextToken();
+            words[word] = letter.substring(4,letter.length()-4);
         }
 
-        // 기본 단어 학습 불가인 경우
-        if(studyCnt<5) {
-            System.out.println(0);
-        }
+        // 알파벳 학습 여부 배열 생성 및 초기화
+        visited = new boolean[26];
+        for(int index=0; index<required.length; index++)
+            visited[required[index]-'a'] = true;
 
-        // 모든 알파벳을 학습이 가능한 경우
-        else if(studyCnt==26) {
-            System.out.println(existCnt);
-        }
+        // 결과
+        answer = 0;
 
-        // 그 외
+        // 알파벳을 5개 미만으로 배운 경우
+        if(possibleCount<5) System.out.println(0);
+
+        // 알파벳을 모두 배운 경우
+        else if(possibleCount==26) System.out.println(wordCount);
+
+        // 아닌 경우
         else {
-            // 기본 셋팅
-            result = 0;
-            visited = new boolean[26];
-            visited['a'-'a'] = true;
-            visited['n'-'a'] = true;
-            visited['t'-'a'] = true;
-            visited['c'-'a'] = true;
-            visited['i'-'a'] = true;
-
-            // 학습 단어 확인
-            getStudy(0,0);
-
-            // 결과 출력
-            System.out.println(result);
+            possibleCount -= 5;
+            solve(0,0);
+            System.out.println(answer);
         }
     }
 }
