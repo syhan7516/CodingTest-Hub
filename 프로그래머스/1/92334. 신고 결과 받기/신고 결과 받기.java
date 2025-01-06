3e14ae44342f9126314e61c64c495eaeae011993
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-06 12:35:12 +0900

    [level 1] Title: 신고 결과 받기, Time: 201.26 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,62 +1,49 @@
 import java.util.*;
 
-// 같은 사람이 같은 사람 -> 1번 처리
-
 class Solution {
+    
+    // 이름 저장 집합 배열
+    public static HashSet<String>[] set;
+    
+    // 인덱스 저장 해시
+    public static HashMap<String,Integer> map;
+    
     public int[] solution(String[] id_list, String[] report, int k) {
         
         StringTokenizer st;
         
-        // 신고 수신, 발신 정보 해시
-        HashMap<String,Integer> map = new HashMap<>();
+        // 결과
+        int answer[] = new int[id_list.length];
         
-        // 신고한 사람 리스트 
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        // 이름 저장 집합 배열 생성
+        set = new HashSet[id_list.length];
+        for(int index=0; index<id_list.length; index++)
+            set[index] = new HashSet<>();
         
-        // 메일 수신 정보 배열
-        int mail[] = new int[id_list.length];
+        // 인덱스 저장 해시 생성
+        map = new HashMap<>();
         
+        // 이름 순회
+        for(int index=0; index<id_list.length; index++)
+            map.put(id_list[index],index);
         
-        // 초기화
-        for(int i=0; i<id_list.length; i++) {
-            list.add(new ArrayList<>());
-            map.put(id_list[i],i);
-        }
-        
-        // 신고받은 횟수 배열
-        int arr[] = new int[id_list.length];
-        
-        // 신고 정보 확인
-        for(int i=0; i<report.length; i++) {
+        // 신고 확인
+        for(int index=0; index<report.length; index++) {
+            st = new StringTokenizer(report[index]);
+            String from = st.nextToken();
+            String to = st.nextToken();
             
-            // 신고 수신, 발신자 
-            st = new StringTokenizer(report[i]);
-            int from = map.get(st.nextToken());
-            int to = map.get(st.nextToken());
-            
-            // 재신고 확인
-            if(list.get(to).contains(from)) continue;
-            
-            // 수신자 : 발신자 추가
-            list.get(to).add(from);
-            
-            // 수신자 : 신고 횟수 증가
-            arr[to]++;
-        }
-        
-        // 신고 횟수 확인
-        for(int i=0; i<arr.length; i++) {
-            
-            // 신고 횟수가 기준 이상인 경우
-            if(arr[i]>=k) {
-                for(int user: list.get(i))
-                    mail[user]++;
+            // 신고 대상 사람이 명단에 있는 경우
+            if(map.containsKey(to)) {
+                set[map.get(to)].add(from);
             }
         }
         
+        // 메일 확인
+        Arrays.stream(set).filter(group -> group.size()>=k)
+            .forEach(group -> group.forEach(member -> answer[map.get(member)]++));
+        
         // 결과
-        int[] answer = mail;
-    
         return answer;
     }
 }
