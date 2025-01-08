e7a000d7ebc094fd3f7a0a7a041a2bf111009d2f
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-08 15:18:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution
 {
     public int solution(int n, int a, int b)
@@ -7,17 +5,12 @@ class Solution
         // 결과
         int answer = 0;
         
-        // 확인
-        while(true) {
+        // 경기 수행
+        while(a!=b) {
             
-            // 종료 조건
-            if(a==b) break;
+            a = a%2==0 ? a/2 : (a+1)/2;
+            b = b%2==0 ? b/2 : (b+1)/2;
             
-            // 2로 나누기
-            a = a%2==0? a/2 : (a+1)/2;
-            b = b%2==0? b/2 : (b+1)/2;
-            
-            // 라운드 증가
             answer++;
         }
         
