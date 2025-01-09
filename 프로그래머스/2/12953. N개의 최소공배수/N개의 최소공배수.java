cb907ad004cec519a763591bf85c00554b48aa2c
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-09 10:59:14 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,33 +1,33 @@
-import java.util.*;
-
 class Solution {
     
-    // 최소 공배수 메서드
-    public static int gcd(int a, int b) {
+    // 최소공배수 구하기 메서드
+    public static int getGCB(int a, int b) {
         
+        // 큰 수, 작은 수 설정
         if(a<b) {
-            int c = a;
+            int mock = a;
             a = b;
-            b = c;
+            b = mock;
         }
         
-        if(a%b==0) return b;
+        // 작은 값이 0인 경우
+        if(b==0) return a;
         
-        return gcd(b,a%b);
+        // 최대공약수 구하기
+        return getGCB(b,a%b);
     }
     
     public int solution(int[] arr) {
         
-        // 결과
-        int answer = 1;
+        // 초기 값
+        int current = 1;
         
-        // 배열 요소 확인
-        for(int i=0; i<arr.length; i++) {
-            
-            // 최소 공배수 구하기
-            answer = answer*arr[i]/gcd(answer,arr[i]);
+        // 두 수씩 최대공약수 구한 후 최소공배수 구하기
+        for(int index=0; index<arr.length; index++) {
+            int gcb = getGCB(current,arr[index]);
+            current = current*arr[index]/gcb;
         }
         
-        return answer;
+        return current;
     }
 }
