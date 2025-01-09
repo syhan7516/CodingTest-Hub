2846c03187abd08ada79d66459991fdd3a11702b
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-09 11:09:43 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,17 @@
 class Solution {
     public long solution(int n) {
-    
-        // 가지 수 저장 배열
-        long DP[] = new long[2001];
         
-        // 초기 값
-        DP[0] = 1;
+        // 이동 가능 수 배열
+        long DP[] = new long[n+1];
+        
+        // 초기 0칸, 1칸 설정
         DP[1] = 1;
+        DP[0] = 1;
         
-        // 멀리 뛰기
-        for(int i=2; i<=2000; i++) {
-            DP[i] = (DP[i-1]+DP[i-2]) % 1234567;
-        }
+        // 2칸부터 확인
+        for(int index=2; index<=n; index++)
+            DP[index] = (DP[index-1]+DP[index-2])%1234567;
         
-        // 결과
-        long answer = DP[n];
-        
-        return answer;
+        return DP[n];
     }
 }
