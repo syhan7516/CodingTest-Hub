dc50274516661aee9b1db36e509408bdf1a1717d
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-13 20:44:15 +0900

    [level 2] Title: 할인 행사, Time: 22.59 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,64 +1,97 @@
 import java.util.*;
 
 class Solution {
+    
+    // 인덱스 매칭 해시 맵
+    public static HashMap<String,Integer> names;
+    
+    // 행사 그룹 해시 셋
+    public static HashSet<String> foods;
+    
+    // 개수 배열
+    public static String count[];
+    
     public int solution(String[] want, int[] number, String[] discount) {
         
         // 결과
         int answer = 0;
         
-        // 원하는 물건 개수 목록 해시 생성
-        HashMap<String,Integer> map = new HashMap<>();
+        // 개수 정보 해시 맵 생성
+        names = new HashMap<>();
         
-        // 원하는 물품 현재 상태 리스트 생성
-        ArrayList<String> status = new ArrayList<>();
+        // 행사 그룹 해시 셋 생성
+        foods = new HashSet<>();
         
-        // 품목, 개수 저장
-        for(int i=0; i<want.length; i++) {
-            map.put(want[i],number[i]);
-            status.add(want[i]);
-        }
+        // 개수 배열 생성
+        count = new String[want.length];
         
-        // 초기 할인 일정 확인
-        for(int i=0; i<10; i++) {
+        // 인덱스 정보 입력
+        for(int index=0; index<want.length; index++)
+            names.put(want[index],index);
+        
+        // 10개 물건 처리
+        for(int index=0; index<10; index++) {
             
-            // 원하는 품목이 있는 경우
-            if(map.containsKey(discount[i])) {
+            // 등록된 상품인 경우
+            if(names.containsKey(discount[index])) {
+                
+                // 인덱스 확인
+                int point = names.get(discount[index]);
                 
                 // 개수 감소
-                map.put(discount[i],map.get(discount[i])-1);
+                number[point]--;
                 
-                // 개수가 0인 경우 품목에서 제거
-                if(map.get(discount[i])<=0 && status.contains(discount[i]))
-                    status.remove(discount[i]);
+                // 개수가 0인 경우
+                if(number[point]==0) {
+                    
+                    // 상품 추가
+                    foods.add(discount[index]);
+                }
+                
+                // 상품이 모두 모인 경우
+                if(foods.size()==want.length)
+                    answer++;
             }
         }
-        
-        // 다 산 경우
-        if(status.size()==0) answer++;
-        
-        // 나머지 할인 일정 확인
-        for(int i=10; i<discount.length; i++) {
             
-            // 제거 항목이 원하는 품목인 경우
-            if(map.containsKey(discount[i-10])) {
-                map.put(discount[i-10], map.get(discount[i-10])+1);
-                if(map.get(discount[i-10])>0 && !status.contains(discount[i-10])) 
-                    status.add(discount[i-10]);
-            }
+        // 이 외 물건 처리
+        for(int index=10; index<discount.length; index++) {
+
+            // 최근 날짜 상품이 등록된 상품인 경우
+            if(names.containsKey(discount[index])) {
                 
-            // 원하는 품목이 있는 경우
-            if(map.containsKey(discount[i])) {
+                // 인덱스 위치 확인
+                int point = names.get(discount[index]);
                 
                 // 개수 감소
-                map.put(discount[i],map.get(discount[i])-1);
+                number[point]--;
                 
-                // 개수가 0이하인 경우
-                if(map.get(discount[i])<=0 && status.contains(discount[i]))
-                    status.remove(discount[i]);
+                // 개수가 0인 경우
+                if(number[point]==0)
+                    
+                    // 상품 추가
+                    foods.add(discount[index]);
             }
-            
-            // 다 산 경우
-            if(status.size()==0) answer++;
+
+            // 가장 오래된 날짜 상품이 등록된 상품인 경우
+            if(names.containsKey(discount[index-10])) {
+                
+                // 인덱스 위치 확인
+                int point = names.get(discount[index-10]);
+                
+                // 개수 증가
+                number[point]++;
+                
+                // 개수가 0을 넘는 경우
+                if(number[point]>0) 
+                    
+                    // 상품 목록에서 제거
+                    foods.remove(discount[index-10]);
+            }
+
+            // 상품이 모두 모인 경우
+            if(foods.size()==want.length)
+                answer++;
         }
         
         return answer;
