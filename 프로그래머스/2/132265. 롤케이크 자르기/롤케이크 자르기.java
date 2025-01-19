f7f86269a3695dc51bf877adfd13bffb11eac8ce
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-19 14:56:30 +0900

    [level 2] Title: 롤케이크 자르기, Time: 138.92 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,56 +1,84 @@
 import java.util.*;
 
 class Solution {
+    
+    // 결과
+    public static int answer;
+    
+    // 개수 배열
+    public static int[] right;
+    
+    // 종류 셋
+    public static HashSet<Integer> leftSet, rightSet;
+    
+    // 동일 여부 확인 메서드
+    public static void isEquals() {
+        answer = leftSet.size()==rightSet.size() ? answer+1 : answer;
+    }
+    
+    // 초기 설정 메서드
+    public static void init(int[] topping) {
+        
+        // 왼쪽
+        leftSet.add(topping[0]);
+        
+        // 오른쪽
+        for(int index=1; index<topping.length; index++) {
+            right[topping[index]]++;
+            rightSet.add(topping[index]);
+        }
+        
+        // 종류 개수 확인
+        isEquals();
+    }
+    
+    // 종류 개수 확인 메서드
+    public static void isZero(int kind) {
+        if(right[kind]==0) rightSet.remove(kind);
+    }
+    
+    // 케이크 자르기 메서드
+    public static void solve(int[] topping) {
+        
+        // 자르는 위치
+        int point = 1;
+        
+        // 토핑 확인
+        while(point<topping.length) {
+            
+            // 왼쪽 토핑 추가
+            int kind = topping[point];
+            leftSet.add(kind);
+            
+            // 오른쪽 토핑 제거
+            right[kind]--;
+            isZero(kind);
+            
+            // 양쪽 토핑 개수 확인
+            isEquals();
+            
+            // 위치 이동
+            point++;
+        }
+    }
+    
     public int solution(int[] topping) {
         
         // 결과
-        int answer = 0;
+        answer = 0;
         
-        // 개수 저장 배열
-        int leftTopping[] = new int[10001];
-        int rightTopping[] = new int[10001];
+        // 배열 생성
+        right = new int[1000001];
         
-        // 토핑 종류 저장 셋
-        HashSet<Integer> leftSet = new HashSet<>();
-        HashSet<Integer> rightSet = new HashSet<>();
+        // 종류 셋 생성
+        leftSet = new HashSet<>();
+        rightSet = new HashSet<>();
         
-        // 초기 시작 토핑
-        leftTopping[topping[0]]++;
-        leftSet.add(topping[0]);
+        // 시작 설정
+        init(topping);
         
-        for(int i=1; i<topping.length; i++) {
-            rightTopping[topping[i]]++;
-            rightSet.add(topping[i]);
-        }
-        
-        // 종류가 동일한 경우
-        if(leftSet.size()==rightSet.size())
-            answer++;
-        
-        // 케이크 자르기 수행
-        int idx = 1;
-        while(true) {
-            
-            // 종료 조건
-            if(idx==topping.length-1) 
-                break;
-            
-            // 왼쪽 토핑 추가
-            leftTopping[topping[idx]]++;
-            leftSet.add(topping[idx]);
-            
-            // 오른쪽 토핑 제거
-            rightTopping[topping[idx]]--;
-            if(rightTopping[topping[idx]]==0)
-                rightSet.remove(topping[idx]);
-            
-            // 개수가 동일한 경우
-            if(leftSet.size()==rightSet.size())
-            answer++;
-            
-            // 왼쪽 크기 증가
-            idx++;
-        }
+        // 케이크 자르기
+        solve(topping);
         
         return answer;
     }
