4a0cb12915c3c990c3f2ac27010ddf8c5e711b6f
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-01-20 13:51:18 +0900

    [Gold IV] Title: 작업, Time: 636 ms, Memory: 81904 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -1,102 +1,101 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    // 수행할 작업 수
-    public static int workCnt;
-    // 결과
-    public static long result;
-    // 작업 정보 리스트
-    public static ArrayList<ArrayList<Integer>> work;
-    // 선 작업 배열
-    public static int degree[];
-    // 작업 시간 정보 배열
-    public static int workTime[];
-    // 방문 배열
-    public static boolean visited[];
-    // 총 작업 시간 DP
-    public static long DP[];
+    // 결과, 작업 개수
+    public static int answer, jobCount;
+
+    // 진입 차수, 시간 배열
+    public static int[] degree, time;
+
+    // 선행 정보 리스트
+    public static ArrayList<ArrayList<Integer>> relations;
+
     // 작업 큐
     public static Queue<Integer> queue;
 
-    // 작업 수행 함수
-    static void startWork() {
+    // 작업 완료 시간
+    public static int completeTime[];
 
-        // 모든 작업 마칠 때까지 수행
+    // 작업 수행 메서드
+    public static void solve() {
+
         while(!queue.isEmpty()) {
 
             // 현재 작업
-            int curWork = queue.poll();
+            int currentJob = queue.poll();
 
-            // 현재 작업을 선 작업으로 하는 작업 처리
-            for(int w=0; w<work.get(curWork).size(); w++) {
-                int nextWork = work.get(curWork).get(w);
-                degree[nextWork] -= 1;
-                
-                // 작업이 오래 걸리는 경우로 갱신
-                DP[nextWork] = Math.max(DP[nextWork],DP[curWork]+workTime[nextWork]);
+            // 연관된 작업 확인
+            for(int job=0; job<relations.get(currentJob).size(); job++) {
 
-                // 선 작업이 모두 이루어진 경우
-                if(degree[nextWork]==0) {
-                    queue.offer(nextWork);
-                }
+                // 후속 작업
+                int afterJob = relations.get(currentJob).get(job);
+
+                // 후속 작업 처리
+                degree[afterJob]--;
+                completeTime[afterJob] = Math.max(completeTime[afterJob],completeTime[currentJob]+time[afterJob]);
+
+                // 선행 작업이 완료된 경우
+                if(degree[afterJob]==0)
+                    queue.offer(afterJob);
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 작업 수 입력
-        workCnt = Integer.parseInt(br.readLine());
+        // 작업 개수 입력
+        jobCount = Integer.parseInt(br.readLine());
 
-        // 생성 및 초기화
-        work = new ArrayList<>();
-        for(int w=0; w<=workCnt; w++)
-            work.add(new ArrayList<>());
+        // 진입 차수, 시간 배열 생성
+        degree = new int[jobCount+1];
+        time = new int[jobCount+1];
 
-        degree = new int[workCnt+1];
-        workTime = new int[workCnt+1];
-        visited = new boolean[workCnt+1];
-        DP = new long[workCnt+1];
+        // 선행 정보 리스트 생성, 초기화
+        relations = new ArrayList<>();
+        for(int job=0; job<=jobCount; job++)
+            relations.add(new ArrayList<>());
 
-        // 작업 정보 입력
-        for(int w=1; w<=workCnt; w++) {
+        // 각 작업 정보 입력
+        for(int job=1; job<=jobCount; job++) {
             st = new StringTokenizer(br.readLine());
-            int curTime = Integer.parseInt(st.nextToken());
-            int preWorkCnt = Integer.parseInt(st.nextToken());
+            time[job] = Integer.parseInt(st.nextToken());
+            int preJobCount = Integer.parseInt(st.nextToken());
+            degree[job] = preJobCount;
 
-            // 선 작업 정보 입력
-            for(int p=0; p<preWorkCnt; p++) {
-                int preWork = Integer.parseInt(st.nextToken());
-                work.get(preWork).add(w);
-                visited[w] = true;
-                degree[w] += 1;
+            // 선행 작업 정보 입력
+            for(int preJob=0; preJob<preJobCount; preJob++) {
+                int pre = Integer.parseInt(st.nextToken());
+                relations.get(pre).add(job);
             }
-            workTime[w] = curTime;
         }
 
-        // 가장 첫 작업 찾기
+        // 작업 규 생성
         queue = new LinkedList<>();
-        for(int w=1; w<=workCnt; w++) {
-            if(visited[w]==false) {
-                queue.offer(w);
-                DP[w] = workTime[w];
+
+        // 작업 완료 시간 배열 생성
+        completeTime = new int[jobCount+1];
+
+        // 선행 조건이 없는 작업 확인
+        for(int index=1; index<=jobCount; index++) {
+            if(degree[index]==0) {
+                queue.offer(index);
+                completeTime[index] = time[index];
             }
         }
 
         // 작업 수행
-        startWork();
-
-        // 결과 저장
-        result = 0;
-        for(long element: DP)
-            result = Math.max(result,element);
+        answer = 0;
+        solve();
 
         // 결과 출력
-        System.out.println(result);
+        for(int result: completeTime)
+            answer = Math.max(answer,result);
+        System.out.println(answer);
     }
 }
