04d894ffc65043c2ae595b372dd3ebe6c7472792
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-06 15:33:55 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,4 @@
-select i.name, i.datetime
-from animal_ins i left join animal_outs o
-    on i.animal_id = o.animal_id
-where o.datetime is null
-order by i.datetime limit 3;
+SELECT I.NAME, I.DATETIME
+FROM ANIMAL_INS I LEFT JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.ANIMAL_ID IS NULL
+ORDER BY I.DATETIME ASC LIMIT 3
