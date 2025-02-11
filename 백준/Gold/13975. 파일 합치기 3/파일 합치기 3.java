3835a3269414f8d23b034bae2d0afd81e3dd1a83
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-11 17:44:17 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2460 ms, Memory: 332008 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -5,6 +5,30 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    // 결과
+    public static long answer;
+
+    // 파일 개수
+    public static int fileCnt;
+
+    // 파일 크기 우선 순위 큐
+    public static PriorityQueue<Long> queue;
+
+    // 파일 합치기 메서드
+    public static void solve() {
+
+        // 파일 크기
+        answer = 0;
+
+        // 파일 합치기
+        while(queue.size()>1) {
+            long size = queue.poll()+queue.poll();
+            answer += size;
+            queue.offer(size);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -17,26 +41,18 @@ public class Main {
         for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
 
             // 우선 순위 큐 생성
-            PriorityQueue<Long> queue = new PriorityQueue<>();
+            queue = new PriorityQueue<>();
 
             // 파일 개수 입력
-            int fileCnt = Integer.parseInt(br.readLine());
+            fileCnt = Integer.parseInt(br.readLine());
 
             // 파일 정보 입력
             st = new StringTokenizer(br.readLine());
-            for(int i=0; i<fileCnt; i++)
+            for(int index=0; index<fileCnt; index++)
                 queue.offer(Long.parseLong(st.nextToken()));
 
-            // 파일 크기
-            long answer = 0;
-
             // 파일 합치기
-            while(queue.size()>1) {
-
-                long size = queue.poll()+queue.poll();
-                answer += size;
-                queue.offer(size);
-            }
+            solve();
 
             // 결과 저장
             sb.append(answer).append("\n");
