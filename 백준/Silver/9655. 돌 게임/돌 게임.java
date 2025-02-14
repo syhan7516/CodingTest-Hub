89b5a2a98565e8dd5e7629da65e6d86daef4bfcf
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-14 20:02:37 +0900

    [Silver V] Title: 돌 게임, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,17 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 돌의 개수 입력
-        int stoneCnt = Integer.parseInt(br.readLine());
-
-        // 게임 진행
-        if(stoneCnt%2!=0)
-            System.out.println("SK");
-        else
-            System.out.println("CY");
+        
+        // 돌 개수 입력
+        int stoneCount = Integer.parseInt(br.readLine());
+        
+        // 승리자 저장 배열 생성
+        int winner[] = new int[1001];
+        
+        // 초기 설정
+        winner[1] = 1;
+        winner[2] = -1;
+        winner[3] = 1;
+        
+        // 승리자 확인
+        for(int index=4; index<1001; index++) {
+            winner[index] = winner[index-1] * -1;
+        }
+        
+        // 결과 출력
+        if(winner[stoneCount]==1) System.out.println("SK");
+        else System.out.println("CY");
     }
 }
