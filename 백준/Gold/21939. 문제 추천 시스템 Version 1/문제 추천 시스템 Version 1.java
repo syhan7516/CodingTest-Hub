6e06d4ba263870dd659c723c882f8c1cabc99f0b
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-15 19:05:38 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 464 ms, Memory: 52704 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -1,92 +1,101 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
+// 문제 클래스
 class Problem implements Comparable<Problem> {
     int number;
-    int level;
+    int grade;
 
-    public Problem(int number, int level) {
+    public Problem(int number, int grade) {
         this.number = number;
-        this.level = level;
+        this.grade = grade;
     }
 
+    @Override
     public int compareTo(Problem other) {
-        if(this.level==other.level)
-            return this.number-other.number;
-
-        else
-            return this.level-other.level;
+        if(this.grade == other.grade)
+            return this.number - other.number;
+        return this.grade - other.grade;
     }
 }
 
 public class Main {
 
-    // 문제 개수
-    public static int problemCnt;
+    // 문제 개수, 명령어 개수
+    public static int problemCount, commandCount;
 
-    // 최대, 최소 트리 셋
+    // 문제 저장 트리셋
     public static TreeSet<Problem> set;
 
-    // 문제 정보 해시 셋
-    public static HashMap<Integer,Integer> problems;
+    // 문제 저장 해시
+    public static HashMap<Integer,Integer> map;
+
+    // 문제 추가 메서드
+    public static void add(int problemNum, int problemGrade) {
+        set.add(new Problem(problemNum, problemGrade));
+        map.put(problemNum,problemGrade);
+    }
+
+    // 문제 추천 메서드
+    public static int recommend(int value) {
+
+        if(value==-1)
+            return set.first().number;
+
+        else
+            return set.last().number;
+    }
+
+    // 문제 제거 메서드
+    public static void solved(int problemNum) {
+        set.remove(new Problem(problemNum,map.get(problemNum)));
+        map.remove(problemNum);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        // 트리 셋, 해시 생성
-        set = new TreeSet<>();
-        problems = new HashMap<>();
-
         // 문제 개수 입력
-        problemCnt = Integer.parseInt(br.readLine());
+        problemCount = Integer.parseInt(br.readLine());
 
-        // 문제 번호, 난이도 입력
-        for(int i=0; i<problemCnt; i++) {
+        // 문제 저장 트리셋 생성
+        set = new TreeSet<>();
+
+        // 문제 저장 해시 생성
+        map = new HashMap<>();
+
+        // 문제 정보 입력
+        for(int index=0; index<problemCount; index++) {
             st = new StringTokenizer(br.readLine());
             int number = Integer.parseInt(st.nextToken());
-            int level = Integer.parseInt(st.nextToken());
-            set.add(new Problem(number,level));
-            problems.put(number,level);
+            int grade = Integer.parseInt(st.nextToken());
+            set.add(new Problem(number,grade));
+            map.put(number,grade);
         }
 
         // 명령어 개수 입력
-        int orderCnt = Integer.parseInt(br.readLine());
-
-        // 명령어 입력
-        for(int i=0; i<orderCnt; i++) {
+        commandCount = Integer.parseInt(br.readLine());
+        for(int index=0; index<commandCount; index++) {
             st = new StringTokenizer(br.readLine());
-            String order = st.nextToken();
-            int first = Integer.parseInt(st.nextToken());
+            String command = st.nextToken();
+            int inputValue = Integer.parseInt(st.nextToken());
 
-            // add
-            if(order.equals("add")) {
-                int second = Integer.parseInt(st.nextToken());
-                set.add(new Problem(first,second));
-                problems.put(first,second);
+            if(command.equals("add")) {
+                add(inputValue, Integer.parseInt(st.nextToken()));
             }
 
-            // recommend
-            else if(order.equals("recommend")) {
-
-                // 1
-                if(first==1) {
-                    sb.append(set.last().number).append("\n");
-                }
-
-                // -1
-                else {
-                    sb.append(set.first().number).append("\n");
-                }
+            else if(command.equals("recommend")) {
+                sb.append(recommend(inputValue)).append("\n");
             }
 
-            // solved
             else {
-                set.remove(new Problem(first,problems.get(first)));
-                problems.remove(first);
+                solved(inputValue);
             }
         }
 
