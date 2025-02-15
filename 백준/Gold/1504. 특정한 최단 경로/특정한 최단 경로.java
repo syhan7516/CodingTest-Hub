3685266fcd31e8886d67037f5e4b797ec0d7e4ac
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-15 18:31:06 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 552 ms, Memory: 67036 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -8,14 +8,15 @@ import java.util.StringTokenizer;
 
 // 간선 클래스
 class Edge implements Comparable<Edge> {
-    int toNode;
+    int node;
     int value;
 
-    public Edge(int toNode, int value) {
-        this.toNode = toNode;
+    public Edge(int node, int value) {
+        this.node = node;
         this.value = value;
     }
 
+    @Override
     public int compareTo(Edge other) {
         return this.value - other.value;
     }
@@ -23,114 +24,117 @@ class Edge implements Comparable<Edge> {
 
 public class Main {
 
-    // 최단 거리 비용 초기화 값
-    public static final int MAX_VALUE = 10000000;
+    // 최대 비용
+    public static final int MAX = (int)1e9;
 
-    // 결과, 정점 개수, 간선 개수, 거쳐야하는 정점
-    public static int answer, nodeCount, edgeCount, passNode1, passNode2;
-
-    // 정점 관계 정보 리스트
-    public static ArrayList<ArrayList<Edge>> relations;
+    // 결과, 노드, 간선 개수, 경로 포함 노드
+    public static int answer, nodeCount, edgeCount, pathNode1, pathNode2;
 
     // 최단 거리 배열
     public static int path[];
 
-    // 방문 배열
-    public static boolean visited[];
+    // 관계 정보 리스트
+    public static ArrayList<ArrayList<Edge>> relations;
 
-    // 최단 거리 구하기 메서드
-    private static int getNodeShortestPath(int startNode, int endNode) {
+    // 최단 경로 구하기 메서드
+    public static void findShortestPath(int startNode) {
 
-        // 최단 거리 배열, 방문 배열 초기화
-        Arrays.fill(path,MAX_VALUE);
-        Arrays.fill(visited,false);
+        // 최단 거리 배열 생성 및 초기화
+        path = new int[nodeCount+1];
+        Arrays.fill(path,MAX);
 
-        // 우선 순위 큐 생성
+        // 경로 저장 우선 순위 큐 생성
         PriorityQueue<Edge> queue = new PriorityQueue<>();
 
-        // 시작 노드 처리
+        // 시작점 처리
         path[startNode] = 0;
         queue.offer(new Edge(startNode,path[startNode]));
 
-        // 간선 확인
+        // 경로 탐색
         while(!queue.isEmpty()) {
 
-            // 확인 간선 꺼내기
-            Edge currentEdge = queue.poll();
-            int currentNode = currentEdge.toNode;
+            // 현재 위치
+            Edge edge = queue.poll();
 
-            // 이미 방문한 노드인 경우
-            if(visited[currentNode]) continue;
+            // 연결된 경로 확인
+            for(int node=0; node<relations.get(edge.node).size(); node++) {
 
-            // 방문 처리
-            visited[currentNode] = true;
+                // 연결 노드
+                Edge connect = relations.get(edge.node).get(node);
 
-            // 연결 정점 확인
-            for(int node=0; node<relations.get(currentNode).size(); node++) {
-                
-                // 연결 간선 정보
-                Edge connectEdge = relations.get(currentNode).get(node);
-                int connectNode = connectEdge.toNode;
-                int connectValue = connectEdge.value;
-
-                // 최단 거리인 경우
-                if(path[connectNode]>path[currentNode]+connectValue) {
-                    path[connectNode] = path[currentNode]+connectValue;
-                    queue.offer(new Edge(connectNode,path[connectNode]));
+                // 거리 비교
+                if(path[connect.node]>path[edge.node]+connect.value) {
+                    path[connect.node] = path[edge.node]+connect.value;
+                    queue.offer(new Edge(connect.node,path[connect.node]));
                 }
             }
         }
-        
-        return path[endNode];
     }
 
+    // 특정한 최단 경로 구하기 메서드
+    public static void solve() {
+
+        // 경로 거리 값
+        int path1 = MAX;
+        int path2 = MAX;
+
+        // 시작점에서 최단 경로 구하기
+        findShortestPath(1);
+        int startToPathNode1 = path[pathNode1];
+        int startToPathNode2 = path[pathNode2];
+
+        // 포함 경로에서 최단 경로 구하기
+        findShortestPath(pathNode1);
+        int pathNode1ToPathNode2 = path[pathNode2];
+        int pathNode1ToDestination = path[nodeCount];
+
+        findShortestPath(pathNode2);
+        int pathNode2ToPathNode1 = path[pathNode1];
+        int pathNode2ToDestination = path[nodeCount];
+
+        if(startToPathNode1!=MAX && pathNode1ToPathNode2!=MAX && pathNode2ToDestination!=MAX)
+            path1 = startToPathNode1+pathNode1ToPathNode2+pathNode2ToDestination;
+
+        if(startToPathNode2!=MAX && pathNode2ToPathNode1!=MAX && pathNode1ToDestination!=MAX)
+            path2 = startToPathNode2+pathNode2ToPathNode1+pathNode1ToDestination;
+
+        answer = Math.min(path1,path2);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 정점, 간선 개수 입력
+        // 노드, 간선 개수 입력
         st = new StringTokenizer(br.readLine());
         nodeCount = Integer.parseInt(st.nextToken());
         edgeCount = Integer.parseInt(st.nextToken());
 
-        // 관계 정보 리스트 생성, 초기화
+        // 관계 정보 리스트 생성 및 초기화
         relations = new ArrayList<>();
         for(int node=0; node<=nodeCount; node++)
             relations.add(new ArrayList<>());
 
-        // 간선 정보 입력
+        // 관계 정보 입력
         for(int edge=0; edge<edgeCount; edge++) {
             st = new StringTokenizer(br.readLine());
-            int fromNode = Integer.parseInt(st.nextToken());
-            int toNode = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
-            relations.get(toNode).add(new Edge(fromNode, value));
-            relations.get(fromNode).add(new Edge(toNode, value));
+            relations.get(from).add(new Edge(to, value));
+            relations.get(to).add(new Edge(from, value));
         }
 
-        // 거쳐야하는 정점 정보 입력
+        // 경로 포함 두 노드 입력
         st = new StringTokenizer(br.readLine());
-        passNode1 = Integer.parseInt(st.nextToken());
-        passNode2 = Integer.parseInt(st.nextToken());
+        pathNode1 = Integer.parseInt(st.nextToken());
+        pathNode2 = Integer.parseInt(st.nextToken());
 
-        // 최단 거리 배열, 방문 배열 생성
-        path = new int[nodeCount+1];
-        visited = new boolean[nodeCount+1];
-
-        // 최단 거리 구하기
-        int firstPath = getNodeShortestPath(1,passNode1);
-        firstPath += getNodeShortestPath(passNode1,passNode2);
-        firstPath += getNodeShortestPath(passNode2,nodeCount);
-
-        int secondPath = getNodeShortestPath(1,passNode2);
-        secondPath += getNodeShortestPath(passNode2,passNode1);
-        secondPath += getNodeShortestPath(passNode1,nodeCount);
-
-        // 결과 저장
-        answer = (firstPath>=MAX_VALUE && secondPath>=MAX_VALUE) ? -1 : Math.min(firstPath,secondPath);
+        // 특정한 최단 경로 구하기
+        solve();
 
         // 결과 출력
+        answer = answer==MAX ? -1 : answer;
         System.out.println(answer);
     }
 }
