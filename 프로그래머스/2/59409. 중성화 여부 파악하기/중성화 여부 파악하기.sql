b5baf7a53878eddfa12ea066872ed6353ceb4443
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-16 22:31:43 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,7 +1,8 @@
-select animal_id, name, (
-    case
-        when sex_upon_intake like 'Neutered%' then 'O'
-        when sex_upon_intake like 'Spayed%' then 'O'
-    else 'X'
-    end) as 중성화
-from animal_ins
+SELECT ANIMAL_ID, NAME, 
+    (CASE
+        WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+        WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+        ELSE 'X'
+    END) AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC
