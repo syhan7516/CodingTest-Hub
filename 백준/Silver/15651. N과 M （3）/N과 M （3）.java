40d729afbda49a7accd96718631f2b66ad0971d1
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-17 17:12:43 +0900

    [Silver III] Title: N과 M (3), Time: 352 ms, Memory: 112072 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,54 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    // 결과 저장
-    public static StringBuilder sb = new StringBuilder();
+    // 길이, 선택 개수
+    public static int len, selectedCount;
 
-    // 중복 확인 해시
-    public static HashMap<String,Boolean> visited;
+    // 결과 저장 빌더
+    public static StringBuilder sb;
 
-    // 수의 개수, 선택할 수
-    public static int numCnt, selectCnt;
+    // 선택된 수 저장 배열
+    public static int arr[];
 
-    // 중복 순열
-    static void solve(int cnt, String nums) {
+    // 수열 구하기 메서드
+    public static void solve(int count) {
 
-        // 수를 다 고른 경우
-        if(cnt==selectCnt) {
+        // 모두 선택된 경우
+        if(count==selectedCount) {
 
-            // 중복이 아닌 경우
-            if(!visited.containsKey(nums)) {
-                sb.append(nums).append("\n");
-                visited.put(nums,true);
-            }
+            // 저장
+            for(int index=0; index<arr.length; index++)
+                sb.append(arr[index]).append(" ");
+            sb.append("\n");
             return;
         }
 
-        // 수를 덜 고른 경우
-        for(int i=1; i<=numCnt; i++)
-            solve(cnt+1, nums+i+" ");
+        // 선택하기
+        for(int num=1; num<=len; num++) {
+            arr[count] = num;
+            solve(count+1);
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 수의 개수, 선택할 수 입력
+        // 길이, 선택 개수 입력
         st = new StringTokenizer(br.readLine());
-        numCnt = Integer.parseInt(st.nextToken());
-        selectCnt = Integer.parseInt(st.nextToken());
+        len = Integer.parseInt(st.nextToken());
+        selectedCount = Integer.parseInt(st.nextToken());
 
-        // 중복 확인 해쉬
-        visited = new HashMap<>();
+        // 선택된 수 저장 배열 생성
+        arr = new int[selectedCount];
 
-        // 중복 순열
-        for(int i=1; i<=numCnt; i++)
-            solve(1,i+" ");
+        // 수열 구하기
+        sb = new StringBuilder();
+        solve(0);
 
         // 결과 출력
         System.out.println(sb.toString());
