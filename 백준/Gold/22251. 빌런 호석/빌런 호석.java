9f4d00fea2c414fb6a024c481bf2b3528e90c919
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-18 14:51:48 +0900

    [Gold V] Title: 빌런 호석, Time: 364 ms, Memory: 123804 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.java
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -1,18 +1,14 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    // 건물 층, 자리 수, 반전 수, 현재 층 (정수)
-    public static int buildingFloor, numLen, changeCnt, current;
+    // 결과, 총 층수, 디스플레이 자리 수, 반전 개수, 현재 층수
+    public static int answer, totalFloor, displayDigit, reverseCount, currentFloor;
 
-    // 현재 층 (문자열)
-    public static String curFloor;
-
-    // 문자열 처리 빌더
-    public static StringBuilder sb;
-
-    // 층 디스플레이
+    // 디스플레이 배열 (0~9)
     public static boolean display[][] = {
             {true,true,true,false,true,true,true},
             {false,false,true,false,false,true,false},
@@ -23,80 +19,87 @@ public class Main {
             {true,true,false,true,true,true,true},
             {true,false,true,false,false,true,false},
             {true,true,true,true,true,true,true},
-            {true,true,true,true,false,true,true}
-    };
+            {true,true,true,true,false,true,true}};
 
-    // 층 문자열화 메서드
-    public static String numToLetter(int number) {
+    // 문자열 변환 메서드
+    public static String changeIntToString(int number) {
 
-        // 층 문자열화
-        sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         sb.append(number);
 
-        // 길이가 다른 경우
-        if(sb.length()!=numLen) {
+        // 0 필요 개수
+        int requiredZeroCount = displayDigit-sb.length();
 
-            // 붙여야하는 0 개수
-            int zeroCnt = numLen-sb.length();
-
-            // 앞에 부족한 만큼 0 추가
-            for(int i=0; i<zeroCnt; i++)
-                sb.insert(0,0);
-        }
+        // 0 추가
+        for(int count=0; count<requiredZeroCount; count++)
+            sb.insert(0, '0');
 
         return sb.toString();
     }
 
-    // 현재 층과 다른 층 반전 수 확인 메서드
-    public static int change(int next, String curFloor) {
+    // 자리 비교 메서드
+    public static int compareStringAndString(String floor1, String floor2) {
 
-        // 같은 층인 경우
-        if(next==current) return changeCnt+1;
+        // 디스플레이 차이 개수
+        int differenceDisplayCount = 0;
 
-        // 다른 층 문자열화
-        String nextFloor = numToLetter(next);
+        // 자리 확인
+        for(int index=0; index<displayDigit; index++) {
 
-        // 반전 수
-        int cnt = 0;
+            // 비교 자리 숫자 확인
+            int floor1Num = floor1.charAt(index)-'0';
+            int floor2Num = floor2.charAt(index)-'0';
 
-        // 비교
-        for(int i=0; i<numLen; i++) {
-            for(int j=0; j<7; j++) {
-                if(display[nextFloor.charAt(i)-'0'][j]!=display[curFloor.charAt(i)-'0'][j])
-                    cnt++;
+            // 자리 디스플레이 확인
+            for(int displayIndex=0; displayIndex<7; displayIndex++) {
+
+                // 켜진 곳이 다른 경우
+                if(display[floor1Num][displayIndex]!=display[floor2Num][displayIndex])
+                    differenceDisplayCount++;
             }
         }
 
-        return cnt;
+        return differenceDisplayCount;
+    }
+
+    // 반전 경우의 수 구하기 메서드
+    public static void solve() {
+
+        // 현재 층수 문자열 변환
+        String currentFloorToString = changeIntToString(currentFloor);
+
+        // 1층부터 확인
+        for(int num=1; num<=totalFloor; num++) {
+
+            // 자기 자신인 겨웅
+            if(num==currentFloor) continue;
+
+            // 비교 층
+            String compareFloor = changeIntToString(num);
+
+            // 자리 비교
+            int differenceDisplayCount = compareStringAndString(currentFloorToString,compareFloor);
+
+            // 결과 반영
+            if(differenceDisplayCount<=reverseCount)
+                answer++;
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 결과
-        int answer = 0;
-
-        // 건물 층, 자리 수, 반전 수, 현재 층 입력
+        // 총 층수, 디스플레이 자리 수, 반전 개수, 현재 층수 입력
         st = new StringTokenizer(br.readLine());
-        buildingFloor = Integer.parseInt(st.nextToken());
-        numLen = Integer.parseInt(st.nextToken());
-        changeCnt = Integer.parseInt(st.nextToken());
-        current = Integer.parseInt(st.nextToken());
+        totalFloor = Integer.parseInt(st.nextToken());
+        displayDigit = Integer.parseInt(st.nextToken());
+        reverseCount = Integer.parseInt(st.nextToken());
+        currentFloor = Integer.parseInt(st.nextToken());
 
-        // 현재 층 문자열화
-        String curFloor = numToLetter(current);
-
-        // 각 층 확인
-        for(int i=1; i<=buildingFloor; i++) {
-
-            // 현재 층과 다른 층 반전 수 확인
-            int cnt = change(i,curFloor);
-
-            // 반전 수 최대치를 넘지 않은 경우
-            if(cnt<=changeCnt)
-                answer++;
-        }
+        // 반전 경우의 수 구하기
+        answer = 0;
+        solve();
 
         // 결과 출력
         System.out.println(answer);
