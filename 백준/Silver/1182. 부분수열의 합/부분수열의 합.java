bd6eb2f8592718cf4578d93ae539997df7b54966
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-18 15:29:36 +0900

    [Silver II] Title: 부분수열의 합, Time: 72 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,56 +1,60 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	// 수열의 크기, 목표 합, 결과
-	public static int numSize, target, result;
-	// 수열 배열
-	public static int nums[];
-	
-	// 부분 수열 합 구하기 메서드
-	static void solve(int cnt, int sum, int curCnt) {
-		
-		// 종료 조건
-		if(cnt==numSize) {
-			
-			// 합이 원하는 합과 맞을 경우
-			if(curCnt>0 && sum==target) 
-				result++;
-			
-			return;
-		}
-	
-		// 부분 수열 포함 여부 적용
-		solve(cnt+1,sum,curCnt);
-		solve(cnt+1,sum+nums[cnt],curCnt+1);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		// 수열의 크기, 목표 값 입력
-		st = new StringTokenizer(br.readLine());
-		numSize = Integer.parseInt(st.nextToken());
-		target = Integer.parseInt(st.nextToken());
-		
-		// 수열 만들기
-		nums = new int[numSize];
-		
-		// 수열 정보 입력
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<numSize; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 부분 수열 합 구하기 
-		result = 0;
-		solve(0,0,0);
-		
-		// 결과 출력
-		System.out.println(result);
-	}
+
+    // 결과, 숫자 개수, 목표 수
+    public static int answer, numCount, target;
+
+    // 숫자 배열
+    public static int nums[];
+
+    // 부분 수열 합 구하기 메서드
+    public static void solve(int index, int sum, int selectedCount) {
+
+        // 숫자 선택이 끝난 경우
+        if(index==numCount) {
+
+            // 숫자가 하나도 선택이 안된 경우
+            if(selectedCount==0)
+                return;
+
+            // 합이 목표와 일치하는 경우
+            if(sum==target)
+                answer++;
+            return;
+        }
+
+        // 숫자 선택하기
+        solve(index+1,sum+nums[index],selectedCount+1);
+        solve(index+1, sum,selectedCount);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 숫자 개수, 목표 수 입력
+        st = new StringTokenizer(br.readLine());
+        numCount = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
+
+        // 숫자 배열 생성
+        nums = new int[numCount];
+
+        // 숫자 정보 입력
+        st = new StringTokenizer(br.readLine());
+        for(int index=0;index<numCount;index++) {
+            nums[index] = Integer.parseInt(st.nextToken());
+        }
+
+        // 부분 수열 합 구하기
+        answer = 0;
+        solve(0,0,0);
+
+        // 결과 출력
+        System.out.println(answer);
+    }
 }
