c97f21d77d90cfd9413b7354f89b2ce4859b430e
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-21 15:08:38 +0900

    [Silver III] Title: 01타일, Time: 88 ms, Memory: 15480 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,34 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    // MOD
+    public static final int MOD = 15746;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 숫자 입력
-        int num = Integer.parseInt(br.readLine());
 
-        // 1인 경우
-        if(num==1) System.out.println(1);
-        
-        // 아닌 경우
-        else {
-            
-            // DP
-            long DP[] = new long[num+1];
+        // 문자 길이 입력
+        int letterLen = Integer.parseInt(br.readLine());
 
-            // 기본 셋팅
-            DP[1] = 1;
-            DP[2] = 2;
+        // 가능한 경우의 수 저장 배열 생성
+        int DP[] = new int[1000001];
+        DP[1] = 1;
+        DP[2] = 2;
 
-            // 숫자까지 확인
-            int range = num+1;
-            for (int idx=3; idx<range; idx++) {
-                DP[idx] = (DP[idx-2]+DP[idx-1])%15746;
-            }
-            
-            // 결과 출력
-            System.out.println(DP[num]);
+        // 경우의 수 구하기 메서드
+        for(int len=3; len<=letterLen; len++) {
+            DP[len] = (DP[len-1]+DP[len-2]) % MOD;
         }
+
+        // 결과 출력
+        System.out.println(DP[letterLen]);
     }
 }
