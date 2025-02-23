a74bd596173d4a6d00aa6ff260f815f1dcff6b2c
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-02-23 12:11:37 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11816 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,50 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-
-    // 계단의 수
-    public static int stairCnt;
-
-    // 계단 점수 정보 배열
-    public static int score[];
-
-    // 계단 누적 점수 정보 배열
-    public static int DP[];
-
-    // 구하기
-    static void solve() {
-
-        // 배열 초기 값 셋팅
-        DP[0] = 0;
-        DP[1] = score[1];
-        DP[2] = score[2]+score[1];
-
-        // 나머지 값 구하기
-        for(int i=3; i<=stairCnt; i++) {
-            DP[i] = Math.max(DP[i-2],DP[i-3]+score[i-1])+score[i];
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 계단 수 입력
-        stairCnt = Integer.parseInt(br.readLine());
+        // 계단 개수 입력
+        int count = Integer.parseInt(br.readLine());
 
-        // 배열 생성
-        score = new int[10001];
-        DP = new int[10001];
+        // 계단 배열 생성
+        int stage[] = new int[count+1];
 
-        // 계단 점수 입력
-        for(int i=1; i<=stairCnt; i++)
-            score[i] = Integer.parseInt(br.readLine());
+        // 각 계단 점수 입력
+        for(int index=1; index<=count; index++) {
+            stage[index] = Integer.parseInt(br.readLine());
+        }
 
-        // 계단 오르기
-        solve();
+        if(count==1) {
 
-        // 결과 출력
-        System.out.println(DP[stairCnt]);
+            // 결과 출력
+            System.out.println(stage[1]);
+        }
+
+        else {
+
+            // 계단 점수 배열 생성
+            int DP[] = new int[301];
+            Arrays.fill(DP,-1);
+            DP[0] = 0;
+            DP[1] = stage[1];
+            DP[2] = DP[1] + stage[2];
+
+            // 계단 오르기
+            for(int index=3; index<=count; index++) {
+                DP[index] = Math.max(DP[index-3]+stage[index-1],DP[index-2]) + stage[index];
+            }
+
+            // 결과 출력
+            System.out.println(DP[count]);
+        }
     }
 }
