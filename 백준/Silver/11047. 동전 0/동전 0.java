597e5739a552626b274a258a6b8d8900e1cab274
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-03-03 16:43:04 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,39 +1,39 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 동전 종류 수, 구할 합 입력
+        // 동전 종류, 금액 입력
         st = new StringTokenizer(br.readLine());
-        int kinds = Integer.parseInt(st.nextToken());
+        int kindCount = Integer.parseInt(st.nextToken());
         int money = Integer.parseInt(st.nextToken());
 
-        // 동전 종류 수 입력
-        int coin[] = new int[kinds];
-        for(int c=0; c<kinds; c++) {
-            coin[c] = Integer.parseInt(br.readLine());
+        // 동전 종류 리스트 생성
+        ArrayList<Integer> kinds = new ArrayList<>();
+
+        // 동전 종류 입력
+        for(int index=0; index<kindCount; index++) {
+            kinds.add(Integer.parseInt(br.readLine()));
         }
 
-        // 동전 수 구하기
-        int result = 0;
-        int idx = coin.length-1;
-        
-        while(money>0) {
-            // 동전 종류보다 클 경우
-            if(coin[idx]>money)
-                idx--;
-            // 교환 가능한 경우
-            else {
-                result += money/coin[idx];
-                money %= coin[idx];
-            }
+        // 역정렬
+        Collections.reverse(kinds);
+
+        // 동전 거스르기
+        int answer = 0;
+        for(int index=0; index<kinds.size(); index++) {
+            answer += money/kinds.get(index);
+            money %= kinds.get(index);
         }
 
         // 결과 출력
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
