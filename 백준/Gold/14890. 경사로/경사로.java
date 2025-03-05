acfad3ae4b035e0d562a5748ed1f85e33977c612
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-03-05 11:42:32 +0900

    [Gold III] Title: 경사로, Time: 88 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -4,107 +4,123 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // 길의 크기, 경사로 길이, 결과
-    public static int roadSize, slipWayLen, result;
-    // 길
+    // 결과, 길의 크기, 경사로 길이
+    public static int answer, roadSize, roadLen;
+
+    // 길 배열
     public static int road[][];
+
     // 경사로 놓인 구간
     public static boolean visited[];
 
-    // 길 확인 함수
-    static void rowCheckRoad(int row, int col) {
+    // 길 확인 메서드
+    static void rowCheckRoad(int rowIndex, int colIndex) {
 
         // 경사로 위치
         visited = new boolean[roadSize];
 
-        for(int a=col; a<roadSize-1; a++) {
+        // 길 확인
+        for(int index=colIndex; index<roadSize-1; index++) {
 
             // 바로 앞 길이 높이가 다른 경우
-            if(road[row][a]!=road[row][a+1]) {
-                int curValue = road[row][a];
-                int nextValue = road[row][a+1];
+            if(road[rowIndex][index]!=road[rowIndex][index+1]) {
+
+                // 각 높이 저장
+                int currentValue = road[rowIndex][index];
+                int nextValue = road[rowIndex][index+1];
 
                 // 높이 차이가 1이 아닌 경우
-                if(Math.abs(curValue-nextValue)!=1)
+                if(Math.abs(currentValue-nextValue)!=1)
                     return;
 
                 // 앞의 길이 현재 높이보다 작을 경우
-                if(road[row][a]>road[row][a+1]) {
-                    for(int c=a+1; c<=a+slipWayLen; c++) {
+                if(road[rowIndex][index]>road[rowIndex][index+1]) {
+
+                    // 경사로 크기만큼 앞쪽 확인
+                    for(int point=index+1; point<=index+roadLen; point++) {
 
                         // 높이가 같지 않거나 길을 벗어난 경우
-                        if(c>roadSize-1 || road[row][c]!=nextValue || visited[c])
+                        if(point>roadSize-1 || road[rowIndex][point]!=nextValue || visited[point])
                             return;
 
                         // 경사로 놓기
-                        visited[c] = true;
+                        visited[point] = true;
                     }
                 }
 
                 // 앞의 길이 현재 높이보다 클 경우
                 else {
-                    for(int c=a; c>=a-slipWayLen+1; c--) {
+
+                    // 경사로 크기만큼 뒷쪽 확인
+                    for(int point=index; point>=index-roadLen+1; point--) {
 
                         // 높이가 같지 않거나 길을 벗어난 경우
-                        if(c<0 || road[row][c]!=curValue || visited[c])
+                        if(point<0 || road[rowIndex][point]!=currentValue || visited[point])
                             return;
 
                         // 경사로 놓기
-                        visited[c] = true;
+                        visited[point] = true;
                     }
                 }
             }
         }
 
         // 길 추가
-        result += 1;
+        answer++;
     }
 
-    static void colCheckRoad(int row, int col) {
+    static void colCheckRoad(int rowIndex, int colIndex) {
 
         // 경사로 위치
         visited = new boolean[roadSize];
 
-        for(int a=row; a<roadSize-1; a++) {
+        // 길 확인
+        for(int index=rowIndex; index<roadSize-1; index++) {
 
             // 바로 앞 길이 높이가 다른 경우
-            if(road[a][col]!=road[a+1][col]) {
-                int curValue = road[a][col];
-                int nextValue = road[a+1][col];
+            if(road[index][colIndex]!=road[index+1][colIndex]) {
+
+                // 각 높이 저장
+                int currentValue = road[index][colIndex];
+                int nextValue = road[index+1][colIndex];
 
                 // 높이 차이가 1이 아닌 경우
-                if(Math.abs(curValue-nextValue)!=1)
+                if(Math.abs(currentValue-nextValue)!=1)
                     return;
 
                 // 앞의 길이 현재 높이보다 작을 경우
-                if(road[a][col]>road[a+1][col]) {
-                    for(int r=a+1; r<=a+slipWayLen; r++) {
+                if(road[index][colIndex]>road[index+1][colIndex]) {
+
+                    // 경사로 크기만큼 앞쪽 확인
+                    for(int point=index+1; point<=index+roadLen; point++) {
 
                         // 높이가 같지 않거나 길을 벗어난 경우
-                        if(r>roadSize-1 || road[r][col]!=nextValue || visited[r])
+                        if(point>roadSize-1 || road[point][colIndex]!=nextValue || visited[point])
                             return;
 
                         // 경사로 놓기
-                        visited[r] = true;
+                        visited[point] = true;
                     }
                 }
 
                 // 앞의 길이 현재 높이보다 클 경우
                 else {
-                    for(int r=a; r>=a-slipWayLen+1; r--) {
+
+                    // 경사로 크기만큼 뒷쪽 확인
+                    for(int point=index; point>=index-roadLen+1; point--) {
                         // 높이가 같지 않거나 길을 벗어난 경우
-                        if(r<0 || road[r][col]!=curValue || visited[r])
+                        if(point<0 || road[point][colIndex]!=currentValue || visited[point])
                             return;
 
                         // 경사로 놓기
-                        visited[r] = true;
+                        visited[point] = true;
                     }
                 }
             }
         }
 
         // 길 추가
-        result += 1;
+        answer++;
     }
 
     public static void main(String[] args) throws Exception {
@@ -114,27 +130,27 @@ public class Main {
         // 길의 크기, 경사로 길이 입력
         st = new StringTokenizer(br.readLine());
         roadSize = Integer.parseInt(st.nextToken());
-        slipWayLen = Integer.parseInt(st.nextToken());
+        roadLen = Integer.parseInt(st.nextToken());
 
         // 길 만들기
         road = new int[roadSize][roadSize];
 
         // 길 정보 입력
-        for(int a=0; a<roadSize; a++) {
+        for(int rowIndex=0; rowIndex<roadSize; rowIndex++) {
             st = new StringTokenizer(br.readLine());
-            for(int b=0; b<roadSize; b++) {
-                road[a][b] = Integer.parseInt(st.nextToken());
+            for(int colIndex=0; colIndex<roadSize; colIndex++) {
+                road[rowIndex][colIndex] = Integer.parseInt(st.nextToken());
             }
         }
 
         // 길 확인
-        result = 0;
-        for(int idx=0; idx<roadSize; idx++) {
-            rowCheckRoad(idx,0);
-            colCheckRoad(0,idx);
+        answer = 0;
+        for(int index=0; index<roadSize; index++) {
+            rowCheckRoad(index,0);
+            colCheckRoad(0,index);
         }
 
         // 결과 출력
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
