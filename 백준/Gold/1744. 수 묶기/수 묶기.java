e22c5ea81517c0078c3f6e73a202c10709cbba81
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-03-05 11:58:40 +0900

    [Gold IV] Title: 수 묶기, Time: 68 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -8,55 +8,46 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 수열의 크기 입력
-        int size = Integer.parseInt(br.readLine());
-
-        // 수열 정보 우선 순위 큐 생성
-        PriorityQueue<Integer> plus = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minus = new PriorityQueue<>();
-
         // 결과
         int answer = 0;
 
-        // 수열 정보 입력
-        for(int i=0; i<size; i++) {
-            int num = Integer.parseInt(br.readLine());
+        // 숫자 개수 입력
+        int numCount = Integer.parseInt(br.readLine());
 
-            // 1, 양수, 0과 음수 따로 저장
-            if(num==1) answer += 1;
-            else if(num>0) plus.offer(num);
-            else minus.offer(num);
+        // 우선 순위 큐 생성
+        PriorityQueue<Integer> plusQueue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusQueue = new PriorityQueue<>();
+
+        // 숫자 입력
+        for(int index=0; index<numCount; index++) {
+            int number = Integer.parseInt(br.readLine());
+
+            // 1인 경우
+            if(number==1) answer++;
+
+            // 양수인 경우
+            else if(number>0) plusQueue.offer(number);
+
+            // 음수인 경우
+            else minusQueue.offer(number);
         }
-        
-        // 연산을 수행 할 두 수
-        int first = 0;
-        int second = 0;
 
-        // 양수 수열 확인
-        while(plus.size()>1) {
-
-            // 수 꺼내기
-            first = plus.poll();
-            second = plus.poll();
-
-            // 곱해서 더하기
+        // 숫자 확인
+        while(plusQueue.size()>1) {
+            int first = plusQueue.poll();
+            int second = plusQueue.poll();
             answer += (first*second);
         }
 
-        // 음수 수열 확인
-        while(minus.size()>1) {
-
-            // 수 꺼내기
-            first = minus.poll();
-            second = minus.poll();
-
-            // 곱해서 더하기
+        while(minusQueue.size()>1) {
+            int first = minusQueue.poll();
+            int second = minusQueue.poll();
             answer += (first*second);
         }
 
-        // 큐에 수가 남은 경우
-        if(!plus.isEmpty()) answer += plus.poll();
-        if(!minus.isEmpty()) answer += minus.poll();
+        // 나머지 수 다 더하기
+        if(!plusQueue.isEmpty()) answer += plusQueue.poll();
+        if(!minusQueue.isEmpty()) answer += minusQueue.poll();
 
         // 결과 출력
         System.out.println(answer);
