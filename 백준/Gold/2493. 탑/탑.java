d2b78e460610ff65a0692122c9949fa8b753bd02
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-03-07 13:06:32 +0900

    [Gold V] Title: 탑, Time: 628 ms, Memory: 115372 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,85 +1,68 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-// 타워 클래스
+// 탑 클래스
 class Tower {
-	private int height;
-	private int point;
-	
-	public Tower(int height, int point) {
-		this.height = height;
-		this.point = point;
-	}
-	
-	public int getHeight() {
-		return this.height;
-	}
-	
-	public int getPoint() {
-		return this.point;
-	}
+    int point;
+    int height;
+
+    public Tower(int point, int height) {
+        this.point = point;
+        this.height = height;
+    }
 }
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		// 탑의 개수
-		int towerCnt = Integer.parseInt(br.readLine());
-		
-		// 탑 송신 확인을 위한 스택 선언, 생성 및 할당
-		Stack<Tower> stack = new Stack<>();
-		
-		// 송신 결과 저장 배열
-		int result[] = new int[towerCnt];
-		
-		// 탑 정보 저장
-		st = new StringTokenizer(br.readLine());
-		
-		// 첫 타워 정보 삽입
-		int towerHeight = Integer.parseInt(st.nextToken());
-		stack.push(new Tower(towerHeight,1));
 
-		// 나머지 타워 확인
-		for(int t=2; t<=towerCnt; t++) {
-			towerHeight = Integer.parseInt(st.nextToken());
-			
-			// 기존 타워 확인
-			while(true) {
-				
-				// 스택이 비었을 경우
-				if(stack.isEmpty()) {
-					stack.push(new Tower(towerHeight,t));
-					result[t-1] = 0;
-					break;
-				}
-				
-				// 아닌 경우
-				else {
-					
-					// 비교 대상
-					Tower curTower = stack.peek();
-					
-					// 스택의 위의 값보다 작을 경우
-					if(towerHeight<curTower.getHeight()) {
-						result[t-1] = curTower.getPoint();
-						stack.push(new Tower(towerHeight,t));
-						break;
-					}
-					
-					// 아닌 경우
-					else {
-						stack.pop();
-					}
-				}
-			}
-		}
-		
-		// 결과 출력
-		for(int element: result)
-			System.out.print(element+" ");
-	}
+    // 탑 개수
+    public static int towerCount;
+
+    // 결과, 탑 높이 배열
+    public static int[] answer, towers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        // 탑 개수 입력
+        towerCount = Integer.parseInt(br.readLine());
+
+        // 탑 높이 배열 생성
+        towers = new int[towerCount];
+
+        // 탑 높이 정보 입력
+        st = new StringTokenizer(br.readLine());
+        for(int index=0; index<towerCount; index++) {
+            towers[index] = Integer.parseInt(st.nextToken());
+        }
+
+        // 높이 저장 스택 생성
+        answer = new int[towerCount];
+        Stack<Tower> stack = new Stack<>();
+
+        // 높이 확인
+        for(int index=towerCount-1; index>=0; index--) {
+
+            // 현재 높이
+            int currentHeight = towers[index];
+
+            // 이전 탑 확인
+            while(!stack.isEmpty() && stack.peek().height<currentHeight) {
+                Tower tower = stack.pop();
+                answer[tower.point] = index+1;
+            }
+
+            stack.push(new Tower(index, currentHeight));
+        }
+
+        // 결과 출력
+        for(int index=0; index<towerCount; index++) {
+            sb.append(answer[index]).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
 }
