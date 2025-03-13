1080c6b2522d40c620b6c2590f3c5cb3ce1be39f
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-03-13 12:18:31 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 92 ms, Memory: 12460 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -6,59 +6,88 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 위치 클래스
-class Node {
-    int x;
+class Point {
     int y;
+    int x;
 
-    public Node(int x, int y) {
-        this.x = x;
+    public Point(int y, int x) {
         this.y = y;
+        this.x = x;
     }
 }
 
 public class Main {
 
-    // 편의점 위치 배열
-    public static Node store[];
+    // 결과 값
+    public static final String SUCCESS_VALUE = "happy";
+    public static final String FAILURE_VALUE = "sad";
 
-    // 편의점 방문 여부 배열
-    public static boolean visited[];
+    // 이동 가능한 거리
+    public static final int POSSIBLE_MOVE = 1000;
 
-    // 맥주 마시면서 걸어가기 메서드
-    static String solve(int startX, int startY, int endX, int endY) {
+    // 가게 개수, 시작점, 도착점
+    public static int storeCount, startY, startX, endY, endX;
 
-        // 거리 탐색 큐
-        Queue<Node> queue = new LinkedList<>();
+    // 가게 위치 배열
+    public static Point[] points;
 
-        // 출발점 처리
-        queue.offer(new Node(startX,startY));
+    // 방문 여부 배열
+    public static boolean[] visited;
 
-        // 걸어가기
+    // 위치 저장 큐
+    public static Queue<Point> queue;
+
+    // 거리 차이 확인 메서드
+    public static int calculationDistance(int y1, int y2, int x1, int x2) {
+        return Math.abs(y1-y2)+Math.abs(x1-x2);
+    }
+
+    // 목표 지점에 도달 가능 여부 확인 메서드
+    public static boolean isMoveToDestination(int y, int x) {
+        return calculationDistance(endY,y,endX,x) <= POSSIBLE_MOVE;
+    }
+
+    // 탐색 메서드
+    public static String solve() {
+
+        // 위치 저장 큐 생성
+        queue = new LinkedList<>();
+
+        // 방문 여부 배열 생성
+        visited = new boolean[storeCount];
+
+        // 시작점 처리
+        queue.offer(new Point(startY,startX));
+
+        // 이동
         while(!queue.isEmpty()) {
 
-            // 현재 위치 좌표
-            Node current = queue.poll();
+            // 현재 위치
+            Point point = queue.poll();
 
-            // 페스티벌에 도착한 경우
-            if(1000>=(Math.abs(current.x-endX)+Math.abs(current.y-endY)))
-                return "happy";
+            // 목표 지점에 도달 가능한 경우
+            if(isMoveToDestination(point.y,point.x))
+                return SUCCESS_VALUE;
 
-            // 거리 확인
-            for(int i=0; i<store.length; i++) {
+            // 이동 가능한 가게 위치 확인
+            for(int index=0; index<storeCount; index++) {
 
-                // 이동 불가능한 경우
-                if(1000<(Math.abs(store[i].x- current.x)+Math.abs(store[i].y- current.y))) continue;
+                // 이미 방문한 경우
+                if(visited[index]) continue;
 
-                // 방문한 경우
-                if(visited[i]) continue;
+                // 거리 차이
+                int distance = calculationDistance(point.y,points[index].y,point.x,points[index].x);
 
-                // 이동 가능한 경우
-                queue.offer(new Node(store[i].x,store[i].y));
-                visited[i] = true;
+                // 1000m 넘는 경우
+                if(distance>POSSIBLE_MOVE) continue;
+
+                // 탐색 대상 추가
+                visited[index] = true;
+                queue.offer(new Point(points[index].y,points[index].x));
             }
         }
 
-        return "sad";
+        return FAILURE_VALUE;
     }
 
     public static void main(String[] args) throws IOException {
@@ -67,38 +96,37 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         // 테스트 케이스 수 입력
-        int caseNum = Integer.parseInt(br.readLine());
+        int caseCount = Integer.parseInt(br.readLine());
 
-        // 케이스 수 만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+        // 케이스 수행
+        for(int caseIndex=0; caseIndex<caseCount; caseIndex++) {
 
-            // 편의점 개수 입력
-            int storeCnt = Integer.parseInt(br.readLine());
+            // 가게 개수 입력
+            storeCount = Integer.parseInt(br.readLine());
 
-            // 편의점 위치 배열 생성
-            store = new Node[storeCnt];
+            // 가게 위치 정보 저장 배열 생성
+            points = new Point[storeCount];
 
-            // 상근이네 집 위치 입력
+            // 시작점 입력
             st = new StringTokenizer(br.readLine());
-            int homeX = Integer.parseInt(st.nextToken());
-            int homeY = Integer.parseInt(st.nextToken());
+            startY = Integer.parseInt(st.nextToken());
+            startX = Integer.parseInt(st.nextToken());
 
-            // 편의점 위치 입력
-            for(int i=0; i<storeCnt; i++) {
+            // 가게 위치 입력
+            for(int index=0; index<storeCount; index++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                store[i] = new Node(x,y);
+                int x = Integer.parseInt(st.nextToken());
+                points[index] = new Point(y, x);
             }
 
-            // 페스티벌 위치 입력
+            // 도착점 입력
             st = new StringTokenizer(br.readLine());
-            int festivalX = Integer.parseInt(st.nextToken());
-            int festivalY = Integer.parseInt(st.nextToken());
-            
-            // 맥주 마시면서 걸어가기
-            visited = new boolean[storeCnt];
-            sb.append(solve(homeX,homeY,festivalX,festivalY)).append("\n");
+            endY = Integer.parseInt(st.nextToken());
+            endX = Integer.parseInt(st.nextToken());
+
+            // 탐색
+            sb.append(solve()).append("\n");
         }
 
         // 결과 출력
