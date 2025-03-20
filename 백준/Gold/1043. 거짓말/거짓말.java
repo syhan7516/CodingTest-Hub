e59304b1c73d22996c1b62048dc87bf32c93d36b
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-03-20 19:26:25 +0900

    [Gold IV] Title: 거짓말, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -2,89 +2,173 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static ArrayList<Integer>[] party;
+
+    // 입력
+    public static StringTokenizer st;
+
+    // 결과, 사람 수, 파티 수, 진실을 아는 사람 수
+    public static int answer, saramCount, partyCount, truthCount;
+
+    // 파티 리스트
+    public static ArrayList<ArrayList<Integer>> parties;
+
+    // 집합 배열
     public static int[] parent;
 
-    public static void main(String[] args) throws IOException {
-        int n, m;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(stk.nextToken()); // 사람의 수
-        m = Integer.parseInt(stk.nextToken()); // 파티의 수
-        int k; // 진실을 아는 사람의 수
-        stk = new StringTokenizer(br.readLine());
-        k = Integer.parseInt(stk.nextToken());
-        int[] true_man = new int[k];
-        for (int i = 0; i < k; i++) {
-            true_man[i] = Integer.parseInt(stk.nextToken());
-        } // 진실을 아는 사람의 번호를 저장
+    // 파티 참여 여부
+    public static boolean[] visited;
 
-        parent = new int[n + 1];
-        party = new ArrayList[m];
-        for (int i = 0; i < m; i++) { // 파티의 수 만큼
-            party[i] = new ArrayList<>();
-            stk = new StringTokenizer(br.readLine());
-            int party_size = Integer.parseInt(stk.nextToken());
-            for (int j = 0; j < party_size; j++) {
-                party[i].add(Integer.parseInt(stk.nextToken()));
+    // find
+    public static int find(int saram) {
+
+        // 자기 자신인 경우
+        if(parent[saram]==saram)
+            return saram;
+
+        return parent[saram] = find(parent[saram]);
+    }
+
+    // union
+    public static void union(int saram1, int saram2) {
+
+        // find
+        saram1 = find(saram1);
+        saram2 = find(saram2);
+
+        // union
+        if(saram1<saram2) parent[saram2] = saram1;
+        else parent[saram1] = saram2;
+    }
+
+    // 집합 배열 생성, 초기화 메서드
+    public static void createAndInitParentArray() {
+        parent = new int[saramCount+1];
+        for(int index=1; index<=saramCount; index++) {
+            parent[index] = index;
+        }
+    }
+
+    // 파티 참여 가능 여부 배열 생성 메서드
+    public static void createVisitedArray() {
+        visited = new boolean[partyCount];
+    }
+
+    // 파티 리스트 생성 및 초기화 메서드
+    public static void createAndInitPartiesList() {
+        parties = new ArrayList<>();
+        for(int index=0; index<partyCount; index++) {
+            parties.add(new ArrayList<>());
+        }
+    }
+
+    // 진실을 아는 사람 수 입력 메서드
+    public static void inputTruthKnowingSaram(StringTokenizer st) {
+        truthCount = Integer.parseInt(st.nextToken());
+        for(int truth=0; truth<truthCount; truth++) {
+            int truthSaram = Integer.parseInt(st.nextToken());
+            union(0,truthSaram);
+        }
+    }
+
+    // 결과 확인 메서드
+    public static void checkVisitedResult() {
+        for(int index=0; index<partyCount; index++) {
+            if(!visited[index]) answer++;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 사람 수, 파티 수 입력
+        st = new StringTokenizer(br.readLine());
+        saramCount = Integer.parseInt(st.nextToken());
+        partyCount = Integer.parseInt(st.nextToken());
+
+        // 집합 배열 생성, 초기화
+        createAndInitParentArray();
+
+        // 파티 참여 가능 여부 배열 생성
+        createVisitedArray();
+
+        // 파티 리스트 생성 및 초기화
+        createAndInitPartiesList();
+
+        // 진실을 아는 사람 수 입력
+        inputTruthKnowingSaram(new StringTokenizer(br.readLine()));
+
+        // 파티 정보 입력
+        for(int party=0; party<partyCount; party++) {
+            st = new StringTokenizer(br.readLine());
+
+            // 파티 참여 수 입력
+            int truthSaramCount = Integer.parseInt(st.nextToken());
+
+            // 파티 참여 사람
+            int firstSaram = Integer.parseInt(st.nextToken());
+            if(find(firstSaram)==0) visited[party] = true;
+            parties.get(party).add(firstSaram);
+
+            // 파티 참여 인원 정보 입력
+            for(int saramIndex=1; saramIndex<truthSaramCount; saramIndex++) {
+
+                // 파티 참여 사람
+                int partySaram = Integer.parseInt(st.nextToken());
+
+                // 진실을 아는 사람인 경우
+                if(find(partySaram)==0) visited[party] = true;
+
+                // 합치기
+                union(firstSaram,partySaram);
+                parties.get(party).add(partySaram);
             }
         }
 
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-        for (int i = 0; i < m; i++) {
-            int first_man = party[i].get(0);
-            for (int j = 1; j < party[i].size(); j++) {
-                union(first_man, party[i].get(j));
-            } // 각각의 파티에 참석한 인원들은 같은 집합으로 분류함
-        }
-        // parent 배열을 순회하며 진실을 아는 사람들과 다른 집합으로 분류되어 있다면
-        // 해당 사람들의 개수를 더해서 출력하면 됨
-        int cnt = 0;
-        for (int i = 0; i < m; i++) {
-            int leader = party[i].get(0);
-            boolean flag = true;
-            for (int j = 0; j < k; j++) {
-                if (isitsame(leader, true_man[j])) {
-                    // 진실을 아는 사람과 같은 집합에 속한다면
-                    flag = false;
-                    break;
+        // 변경이 없을 때까지 반복
+        while(true) {
+
+            boolean flag = false;
+
+            // 파티 순회
+            for(int index=0; index<parties.size(); index++) {
+
+                // 파티 참여가 가능한 경우
+                if(!visited[index]) {
+
+                    // 파티 참여 사람
+                    int firstSaram = parties.get(index).get(0);
+                    if(find(firstSaram)==0) visited[index] = true;
+
+                    // 파티 참여 인원 정보 입력
+                    for(int saramIndex=1; saramIndex<parties.get(index).size(); saramIndex++) {
+
+                        // 파티 참여 사람
+                        int partySaram = parties.get(index).get(saramIndex);
+
+                        // 진실을 아는 사람인 경우
+                        if(find(partySaram)==0) {
+                            visited[index] = true;
+                            flag = true;
+                        }
+
+                        // 합치기
+                        union(firstSaram,partySaram);
+                    }
                 }
             }
-            if (flag) {
-                // 진실을 아는 사람과 같은 집합에 속하지 않는다면
-                // 해당 파티에서는 거짓말을 해도 들키지 않음
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
-    }
 
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            // a 와 b 가 다른 집합에 속한다면
-            parent[b] = a; // 같은 집합으로 묶어주기
+            // 변경이 없는 경우
+            if(!flag) break;
         }
-    }
 
-    public static int find(int a) {
-        if (parent[a] == a) {
-            // a 의 부모가 a 이면(root)
-            return a;
-        } else {
-            return parent[a] = find(parent[a]);
-        }
-    }
+        // 결과 확인
+        checkVisitedResult();
 
-    public static boolean isitsame(int a, int b) {
-        if (find(a) == find(b)) { // 같은 집합에 속한다면 true
-            return true;
-        } else return false;
+        // 결과 출력
+        System.out.println(answer);
     }
 }
