7ea3ee895c97d2124596730c95ae7b99eb1a7722
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-04-10 17:17:54 +0900

    [Gold V] Title: 동전 2, Time: 100 ms, Memory: 12308 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,39 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static final int MAX_VALUE = (int)1e9;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 동전 개수, 금액 입력
+        // 동전 종류, 목표 금액 입력
         st = new StringTokenizer(br.readLine());
-        int coinCnt = Integer.parseInt(st.nextToken());
-        int money = Integer.parseInt(st.nextToken());
+        int coinCount = Integer.parseInt(st.nextToken());
+        int targetMoney = Integer.parseInt(st.nextToken());
+
+        // 동전 종류 저장 리스트 생성
+        ArrayList<Integer> coins = new ArrayList<>();
 
         // 동전 종류 입력
-        int coin[] = new int[coinCnt];
-        for(int c=0; c<coinCnt; c++)
-            coin[c] = Integer.parseInt(br.readLine());
+        for(int coinIndex=0; coinIndex<coinCount; coinIndex++) {
+            int coin = Integer.parseInt(br.readLine());
+            if(!coins.contains(coin)) coins.add(coin);
+        }
 
-        // 지갑 만들기
-        int poket[] = new int[money+1];
-        Arrays.fill(poket,10001);
-        poket[0] = 0;
-        
-        // 동전 조합 만들기
-        for(int a=0; a<coinCnt; a++) {
-            for(int b=coin[a]; b<=money; b++) {
-                poket[b] = Math.min(poket[b],poket[b-coin[a]]+1);
+        // 동전 개수 저장 배열 생성
+        int[] DP = new int[targetMoney+1];
+
+        // 동전 개수 저장 배열 초기화
+        Arrays.fill(DP,MAX_VALUE);
+        DP[0] = 0;
+
+        // 동전 최소 개수 구하기
+        for(int money=1; money<=targetMoney; money++) {
+            for(int coinIndex=0; coinIndex<coins.size(); coinIndex++) {
+                if(money-coins.get(coinIndex)<0) continue;
+                DP[money] = Math.min(DP[money],DP[money-coins.get(coinIndex)]+1);
             }
         }
-        
-        // 결과 출력
-        if(poket[money]==10001)
-            System.out.println(-1);
-        else
-            System.out.println(poket[money]);
+
+        // 결과 저장
+        int answer = DP[targetMoney]==MAX_VALUE ? -1 : DP[targetMoney];
+        System.out.println(answer);
     }
 }
