af810c89c3b3d59be4e1512488511d6946fd8f8e
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-04-24 10:08:53 +0900

    [Gold IV] Title: 물통, Time: 72 ms, Memory: 20752 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -1,116 +1,138 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 물병 클래스
+// 물통 클래스
 class Bottle {
-    private int a;
-    private int b;
-    private int c;
+    int A;
+    int B;
+    int C;
 
-    public Bottle(int a,int b,int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public Bottle(int A, int B, int C) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
     }
 
-    public int getA() {
-        return this.a;
-    }
-
-    public int getB() {
-        return this.b;
-    }
-
-    public int getC() {
-        return this.c;
+    public boolean isEmpty() {
+        return A == 0;
     }
 }
 
 public class Main {
 
-    // 물병
-    public static int capacity[];
-    // 방문 배열
-    public static boolean visited[][][];
-    // 이동 경우의 수 배열
-    public static int start[] = {0,0,1,1,2,2};
-    public static int end[] = {1,2,0,2,0,1};
-    // 결과 리스트
-    public static ArrayList<Integer> result;
+    // 물통 개수
+    public static final int BOTTLE_COUNT = 3;
 
-    // 물 다 옮겨보기 함수
-    static void bfs() {
-        // 물병 탐색 큐
-        Queue<Bottle> bottles = new LinkedList<>();
-        // 첫 용량 넣기
-        bottles.offer(new Bottle(0,0,capacity[2]));
+    // 물 최대 용량
+    public static final int MAX_LITER = 200;
 
-        // 탐색 수행
-        while(!bottles.isEmpty()) {
+    // 물통 채우는 경우의 수
+    public static final int CASE_COUNT = 6;
 
-            // 현재 물 용량 확인
-            Bottle curWater = bottles.poll();
+    // 물통 용량
+    public static int[] maxBottle;
 
-            // 옮겨 담기
-            for(int m=0; m<6; m++) {
-                int curWaters[] = {curWater.getA(),curWater.getB(),curWater.getC()};
-                int from = start[m];
-                int to = end[m];
+    // C용량 저장 리스트 생성
+    public static ArrayList<Integer> capacities;
 
-                // 물 담기
-                curWaters[to] += curWaters[from];
-                curWaters[from] = 0;
+    // 큐
+    public static Queue<Bottle> queue;
 
-                // 물이 넘치는지 확인
-                if(curWaters[to]>capacity[to]) {
-                    int extra = curWaters[to]-capacity[to];
-                    curWaters[to] = capacity[to];
-                    curWaters[from] += extra;
+    // 방문 여부 배열
+    public static boolean[][][] visited;
+
+    // 물통 채우는 경우
+    public static int[] from = {0,0,1,1,2,2};
+    public static int[] to = {1,2,0,2,0,1};
+
+
+    // 물통 채우기 메서드
+    public static void solve() {
+
+        // 물통 처음 상태 처리
+        visited[0][0][maxBottle[2]] = true;
+        queue.offer(new Bottle(0,0,maxBottle[2]));
+        capacities.add(maxBottle[2]);
+
+        // 물통 채우기
+        while(!queue.isEmpty()) {
+
+            // 현재 물통 상태
+            Bottle currentBottle = queue.poll();
+
+            // A가 0인 경우
+            if(currentBottle.isEmpty() && !capacities.contains(currentBottle.C)) {
+                capacities.add(currentBottle.C);
+            }
+
+            // 채울 수 있는 경우 확인
+            for(int dir=0; dir<CASE_COUNT; dir++) {
+
+                // 물 채울 준비
+                int[] ABC = {currentBottle.A,currentBottle.B,currentBottle.C};
+                int fromBottle = from[dir];
+                int toBottle = to[dir];
+
+                // 물 채우기
+                ABC[toBottle] += ABC[fromBottle];
+                ABC[fromBottle] = 0;
+
+                // 물이 넘치는 경우
+                if(ABC[toBottle]>maxBottle[toBottle]) {
+                    ABC[fromBottle] = ABC[toBottle]-maxBottle[toBottle];
+                    ABC[toBottle] = maxBottle[toBottle];
                 }
 
-                // 방문 처리
-                int nextA = curWaters[0];
-                int nextB = curWaters[1];
-                int nextC = curWaters[2];
+                // 방문 여부 확인
+                if(visited[ABC[0]][ABC[1]][ABC[2]]) continue;
 
-                // 방문했던 경우
-                if(visited[nextA][nextB][nextC])
-                    continue;
-
-                // 방문하지 않은 경우
-                else {
-                    visited[nextA][nextB][nextC] = true;
-                    bottles.offer(new Bottle(nextA,nextB,nextC));
-
-                    // 결과 조건에 맞을 경우
-                    if(nextA==0) result.add(nextC);
-                }
+                // 탐색 대상 추가
+                visited[ABC[0]][ABC[1]][ABC[2]] = true;
+                queue.offer(new Bottle(ABC[0],ABC[1],ABC[2]));
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    // 결과 저장 메서드
+    public static String save() {
+        StringBuilder sb = new StringBuilder();
+        Collections.sort(capacities);
+
+        for(int index=0; index<capacities.size(); index++) {
+            sb.append(capacities.get(index)).append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 물병 만들기
-        capacity = new int[3];
+        // 물통 용량 배열 생성
+        maxBottle = new int[BOTTLE_COUNT];
 
-        // 물병 용량 입력
+        // 물통 정보 입력
         st = new StringTokenizer(br.readLine());
-        for(int b=0; b<3; b++) {
-            capacity[b] = Integer.parseInt(st.nextToken());
-        }
+        maxBottle[0] = Integer.parseInt(st.nextToken());
+        maxBottle[1] = Integer.parseInt(st.nextToken());
+        maxBottle[2] = Integer.parseInt(st.nextToken());
 
-        // 물 다 옮겨보기
-        visited = new boolean[201][201][201];
-        result = new ArrayList<>();
-        bfs();
+        // 방문 여부 배열 생성
+        visited = new boolean[MAX_LITER+1][MAX_LITER+1][MAX_LITER+1];
+
+        // 큐 생성
+        queue = new LinkedList<>();
+
+        // C용량 저장 리스트 생성
+        capacities = new ArrayList<>();
+
+        // 물통 채우기
+        solve();
 
         // 결과 출력
-        Collections.sort(result);
-        for(int element: result)
-            System.out.print(element+" ");
+        System.out.println(save());
     }
 }
