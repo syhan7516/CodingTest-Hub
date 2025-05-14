a27dec8dc098e0736f9b0cb7e0c1f375c774ae91
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-05-14 18:16:47 +0900

    [Gold V] Title: ZOAC, Time: 68 ms, Memory: 11740 KB -BaekjoonHub

--- 백준/Gold/16719. ZOAC/ZOAC.java
+++ 백준/Gold/16719. ZOAC/ZOAC.java
@@ -1,59 +1,62 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
     // 문자열
-    public static String line;
-    // 문자 사용 여부
-    public static boolean visited[];
-    // 결과 저장
+    public static String word;
+
+    // 문자열 저장 빌더
     public static StringBuilder sb;
 
-    // 문자열 확인 함수
-    static void solve(int start, int end) {
+    // 문자 저장 여부 배열
+    public static boolean[] visited;
 
-        // 종료 조건
-        if(start>end)
-            return;
+    // ZOAC 수행 메서드
+    public static void solve(int startIndex, int endIndex) {
 
-        // 가장 작은 알파벳 찾기
-        int idx=start;
-        for(int a=start; a<=end; a++) {
-            // 더 작은 알파벳 발견한 경우
-            if(line.charAt(idx)>line.charAt(a)) {
-                idx = a;
+        // 범위 확인
+        if(startIndex>endIndex) return;
+
+        // 가장 작은 알파벳 위치
+        int minAlphabetIndex = startIndex;
+
+        // 문자열 확인
+        for(int wordIndex=startIndex; wordIndex<=endIndex; wordIndex++) {
+
+            // 기준보다 작은 알파벳을 찾은 경우
+            if(word.charAt(minAlphabetIndex)>word.charAt(wordIndex)) {
+                minAlphabetIndex = wordIndex;
             }
         }
 
-        // 가장 작은 알파벳 방문 처리
-        visited[idx] = true;
+        // 문자 저장
+        visited[minAlphabetIndex] = true;
 
-        // 출력
-        for(int a=0; a<line.length(); a++) {
-            if(visited[a]) sb.append(line.charAt(a));
+        // 현재까지 만든 문자열 저장
+        for(int index=0; index<word.length(); index++) {
+            if(visited[index]) sb.append(word.charAt(index));
         }
         sb.append("\n");
 
-        // solve
-        solve(idx+1,end);
-        solve(start,idx-1);
+        // 가장 작은 알파벳 기준으로 분리해서 재탐색
+        solve(minAlphabetIndex+1,endIndex);
+        solve(startIndex,minAlphabetIndex-1);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
         // 문자열 입력
-        line = br.readLine();
+        word = br.readLine();
 
-        // 문자 방문 여부
-        visited = new boolean[line.length()];
+        // 문자 저장 여부 배열 생성
+        visited = new boolean[word.length()];
 
-        // 문자열 확인
-        int start = 0;
-        int end = line.length()-1;
-        solve(start,end);
+        // ZOAC 수행
+        solve(0,word.length()-1);
 
         // 결과 출력
         System.out.println(sb.toString());
