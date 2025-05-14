aaa62de4c8417062ed85d36f2b0190e9115ce038
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-05-14 15:09:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 51180 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,16 +1,54 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 물건 클래스
+class Thing {
+    int weight;
+    int value;
+
+    public Thing(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+}
 
 public class Main {
 
     // 물건 개수, 가방 무게
-    public static int cnt, bagWeight;
+    public static int thingCount, maxWeight;
 
-    // DP 테이블
-    public static int DP[][];
+    // 물건 정보 배열
+    public static Thing[] things;
 
-    // 물건 무게, 가치 배열
-    public static int[] weight, value;
+    // 무게별 최대 가치 저장 배열
+    public static int[][] DP;
+
+    // 물건 담기 메서드
+    public static void solve() {
+
+        // 물건 확인
+        for(int thingIndex=1; thingIndex<=thingCount; thingIndex++) {
+
+            // 할당 무게 확인
+            for(int weight=0; weight<=maxWeight; weight++) {
+
+                // 현재 물건을 담을 수 있는 경우
+                if(things[thingIndex].weight<=weight) {
+
+                    // 물건을 담을 때와 담지 않을 때 비교
+                    DP[thingIndex][weight] = Math.max(DP[thingIndex-1][weight],
+                            DP[thingIndex-1][weight-things[thingIndex].weight]+things[thingIndex].value);
+                }
+
+                // 물건을 담지 못하는 경우
+                else {
+                    DP[thingIndex][weight] = DP[thingIndex-1][weight];
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,36 +56,27 @@ public class Main {
 
         // 물건 개수, 가방 무게 입력
         st = new StringTokenizer(br.readLine());
-        cnt = Integer.parseInt(st.nextToken());
-        bagWeight = Integer.parseInt(st.nextToken());
+        thingCount = Integer.parseInt(st.nextToken());
+        maxWeight = Integer.parseInt(st.nextToken());
 
-        // 무게, 가치 배열 생성
-        weight = new int[cnt+1];
-        value = new int[cnt+1];
+        // 물건 정보 배열 생성
+        things = new Thing[thingCount+1];
 
-        // 무게, 가치 정보 입력
-        for(int i=1; i<=cnt; i++) {
+        // 물건 정보 입력
+        for(int thingIndex=0; thingIndex<thingCount; thingIndex++) {
             st = new StringTokenizer(br.readLine());
-            weight[i] = Integer.parseInt(st.nextToken());
-            value[i] = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            things[thingIndex+1] = new Thing(weight,value);
         }
 
-        // DP 테이블 생성
-        DP = new int[101][100001];
+        // 무게별 최대 가치 저장 배열 생성
+        DP = new int[thingCount+1][maxWeight+1];
 
-        // 테이블 채우기
-        for(int i=1; i<=cnt; i++) {
-            for(int j=1; j<100001; j++) {
-
-                // 넣었을 경우 무게가 넘치는 경우
-                if(j-weight[i]<0) DP[i][j] = DP[i-1][j];
-
-                // 넣었을 경우 무게가 넘치지 않는 경우
-                else DP[i][j] = Math.max(DP[i-1][j],DP[i-1][j-weight[i]]+value[i]);
-            }
-        }
+        // 물건 담기
+        solve();
 
         // 결과 출력
-        System.out.println(DP[cnt][bagWeight]);
+        System.out.println(DP[thingCount][maxWeight]);
     }
 }
