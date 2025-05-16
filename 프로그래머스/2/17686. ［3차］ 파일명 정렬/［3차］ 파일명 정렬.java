b6f89a91a345fa815816f75dfd46f1159711f851
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-05-16 19:20:52 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 7.70 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -25,8 +25,10 @@ class File implements Comparable<File> {
             if(this.number==other.number) {
                 return this.index - other.index;
             }
+            
             return this.number - other.number;
         }
+        
         return this.head.toLowerCase().compareTo(other.head.toLowerCase());
     }
 }
@@ -36,41 +38,55 @@ class Solution {
     // 파일 저장 배열
     public static File[] fileClassArray;
     
+    // HEAD 분리 메서드
+    public static String extractHead(String fileName, int startIndex) {
+        
+        // 자르는 기준 인덱스
+        int fileNameCutIndex = -1;
+        
+        for(int index=startIndex; index<fileName.length(); index++) {
+            if(fileName.charAt(index)>='0' && fileName.charAt(index)<='9') {
+                fileNameCutIndex = index;
+                break;
+            }
+        }
+        
+        return fileName.substring(startIndex,fileNameCutIndex);
+    }
+    
+    // NUMBER 분리 메서드
+    public static String extractNumber(String fileName, int startIndex) {
+    
+        for(int index=startIndex; index<fileName.length(); index++) {
+            if(fileName.charAt(index)<'0' || fileName.charAt(index)>'9') {
+                return fileName.substring(startIndex,index);
+            }
+        }
+        
+        // TAIL이 없는 경우
+        return fileName.substring(startIndex);
+    }
+        
+    // TAIL 분리 메서드
+    public static String extractTail(String fileName, int startIndex) {
+        return fileName.substring(startIndex);
+    }
+    
     public static void extractFileName(String fileName, int fileIndex) {
         
         // 문자열 분리 기준 인덱스
         int fileCutIndex = 0;
         
         // HEAD, NUMBER, TAIL
-        String head = null;
-        int number = -1;
-        String tail = null;
-        
-        // HEAD 분리
-        for(int index=0; index<fileName.length(); index++) {
-            if(fileName.charAt(index)>='0' && fileName.charAt(index)<='9') {
-                head = fileName.substring(fileCutIndex,index);
-                fileCutIndex = index;
-                break;
-            }
-        }
-        
-        // NUMBER, TAIL 분리
-        for(int index=fileCutIndex; index<fileName.length(); index++) {
-            if(fileName.charAt(index)<'0' || fileName.charAt(index)>'9') {
-                number = Integer.parseInt(fileName.substring(fileCutIndex,index));
-                tail = fileName.substring(index);
-                break;
-            }
-        }
-        
-        // TAIL이 없는 경우
-        if(number==-1) {
-            number = Integer.parseInt(fileName.substring(fileCutIndex));
-        }
+        String head = extractHead(fileName,0);
+        String number = extractNumber(fileName,head.length());
+        String tail 
+            = fileName.length()==head.length()+number.length() 
+            ? null 
+            : extractTail(fileName,head.length()+number.length());
         
         // File 생성
-        fileClassArray[fileIndex] = new File(fileIndex,fileName,head,number,tail);
+        fileClassArray[fileIndex] = new File(fileIndex,fileName,head,Integer.parseInt(number),tail);
     }
     
     public String[] solution(String[] files) {
