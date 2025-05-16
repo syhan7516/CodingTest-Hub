0f1240dd83ca2a1c71b3decadf1a9b6933d61b56
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-05-16 17:51:35 +0900

    [Silver II] Title: 연속합, Time: 156 ms, Memory: 22040 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -9,28 +9,27 @@ public class Main {
         StringTokenizer st;
 
         // 수열 개수 입력
-        int number = Integer.parseInt(br.readLine());
+        int numCount = Integer.parseInt(br.readLine());
         
         // 수열 생성
-        int nums[] = new int[number];
-
-        // 결과
-        long answer = -(int)1e9;
+        int nums[] = new int[numCount];
+        
+        // 연속합 배열 생성
+        int[] DP = new int[numCount];
 
         // 수열 정보 입력
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<number; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-            answer = Math.max(answer,nums[i]);
+        for(int numIndex=0; numIndex<numCount; numIndex++) {
+            nums[numIndex] = Integer.parseInt(st.nextToken());
         }
+        
+        // 결과
+        int answer = nums[0];
+        DP[0] = nums[0];
 
-        // 최고 합 구하기
-        long sum = 0;
-
-        for(int i=0; i<number; i++) {
-            sum += nums[i];
-            if(sum<0) sum = 0;
-            else answer = Math.max(answer,sum);
+        for(int numIndex=1; numIndex<numCount; numIndex++) {
+            DP[numIndex] = Math.max(DP[numIndex-1]+nums[numIndex],nums[numIndex]);
+            answer = Math.max(answer,DP[numIndex]);
         }
 
         // 결과 출력
