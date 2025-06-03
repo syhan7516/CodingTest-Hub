b1951acda88b2cd5963c224015ea34caa262f312
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-06-03 11:07:10 +0900

    [Silver III] Title: 돌 게임 3, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -1,22 +1,17 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 돌 개수 입력
-        int stone = Integer.parseInt(br.readLine());
-
-        // 승자 확인
-
-        // 돌 개수가 7배수 또는 나머지가 2일 경우
-        if(stone%7==0 || stone%7==2)
-            System.out.println("CY");
         
-        // 아닌 경우
-        else {
-            System.out.println("SK");
-        }
+        // 돌 개수 입력
+        int stoneCount = Integer.parseInt(br.readLine());
+        
+        // 승리자 확인
+        String winner = (stoneCount%7==0 || stoneCount%7==2) ? "CY" : "SK";
+        
+        // 결과 출력
+        System.out.println(winner);
     }
 }
