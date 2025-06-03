74fc3cf44a69e0c8ba9bddff66e4d76f03b1f76f
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-06-03 10:35:38 +0900

    [Silver V] Title: 돌 게임 2, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String []args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 돌 개수 입력
+        int stoneCount = Integer.parseInt(br.readLine());
+        
+        // 승리자 확인
+        String winner = stoneCount%2==0 ? "SK" : "CY";
+        
+        // 결과 출력
+        System.out.println(winner);
+    }
+}
