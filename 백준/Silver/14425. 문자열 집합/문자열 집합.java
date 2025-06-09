2d6b79482dd7a506e1c8864f572b042edc47d23d
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-06-09 21:20:48 +0900

    [Silver IV] Title: 문자열 집합, Time: 280 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,34 +1,45 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    // 결과, 찾을 문자열 개수, 비교 문자열 개수
+    public static int answer, findLetterCount, compareLetterCount;
+
+    // 찾을 문자열 저장 해시
+    public static Map<String,Boolean> words;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        // 결과
-        int count = 0;
+        // 찾을 문자열 개수, 비교 문자열 개수 입력
+        st = new StringTokenizer(br.readLine());
+        findLetterCount = Integer.parseInt(st.nextToken());
+        compareLetterCount = Integer.parseInt(st.nextToken());
 
-        // 문자열1 & 문자열2 개수 입력
-        int firLetterCnt = Integer.parseInt(st.nextToken());
-        int secLetterCnt = Integer.parseInt(st.nextToken());
+        // 찾을 문자열 저장 해시 생성
+        words = new HashMap<>();
 
-        // 해시셋
-        Set<String> result = new HashSet<>();
-
-        // 문자열 입력
-        for(int idx=0; idx<firLetterCnt; idx++) {
-            result.add(br.readLine());
+        // 찾을 문자열 입력
+        for(int letterIndex=0; letterIndex<findLetterCount; letterIndex++) {
+            words.put(br.readLine(),true);
         }
 
-        // 문자열 확인
-        for(int idx=0; idx<secLetterCnt; idx++) {
-            if(result.contains(br.readLine()))
-                count += 1;
+        // 비교 문자열 입력
+        for(int letterIndex=0; letterIndex<compareLetterCount; letterIndex++) {
+            
+            // 동일한 경우
+            if(words.containsKey(br.readLine())) {
+                answer++;
+            }
         }
 
         // 결과 출력
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
