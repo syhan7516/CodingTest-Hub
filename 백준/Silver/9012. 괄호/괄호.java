67205f4c7e45954fe0b750fe4123c80c001a5789
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-06-09 18:36:17 +0900

    [Silver IV] Title: 괄호, Time: 72 ms, Memory: 11680 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,47 +1,70 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+
+    // 괄호 문자열
+    public static String parenthesisLetters;
+
+    // 괄호 저장 스택
+    public static Stack<Character> stack;
+
+    // 괄호 문자열 확인 메서드
+    public static String solve() {
+
+        // 괄호 저장 스택 생성
+        stack = new Stack<>();
+
+        // 괄호 확인
+        for(int letterIndex=0; letterIndex<parenthesisLetters.length(); letterIndex++) {
+
+            // 확인 괄호
+            char parenthesis = parenthesisLetters.charAt(letterIndex);
+
+            // '(' 경우
+            if(parenthesis=='(') {
+                stack.push(parenthesis);
+            }
+
+            // ')' 경우
+            else {
+
+                // 확인 괄호가 없는 경우
+                if(stack.isEmpty()) return "NO";
+
+                // 짝이 맞지 않는 경우
+                if(stack.peek()==')') return "NO";
+
+                // 짝이 맞는 경우
+                stack.pop();
+            }
+        }
+
+        // 짝이 맞지 않아 남은 경우
+        if(!stack.isEmpty()) return "NO";
+
+        // 짝이 다 맞는 경우
+        return "YES";
+    }
+
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         // 테스트 케이스 수 입력
-        int caseNum = Integer.parseInt(br.readLine());
+        int caseCount = Integer.parseInt(br.readLine());
 
-        // 케이스 수 만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+        // 케이스 수행
+        for(int caseIndex=0; caseIndex<caseCount; caseIndex++) {
 
             // 괄호 문자열 입력
-            String letters = br.readLine();
+            parenthesisLetters = br.readLine();
 
-            // 괄호 짝 확인
-            Stack<Character> stack = new Stack<>();
-            boolean flag = true;
-            for(int c=0; c<letters.length(); c++) {
-                char letter = letters.charAt(c);
-
-                // ( 인 경우
-                if(letter=='(')
-                    stack.push('(');
-
-                // ) 인 경우
-                else {
-                    if(!stack.isEmpty())
-                        stack.pop();
-                    else {
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-
-            // VPS 확인 여부
-            if(stack.isEmpty() && flag)
-                sb.append("YES"+"\n");
-            else
-                sb.append("NO"+"\n");
+            // 괄호 확인
+            sb.append(solve()).append("\n");
         }
 
         // 결과 출력
