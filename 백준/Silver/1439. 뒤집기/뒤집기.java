2c09e5c68650ba2cba4d067905c55decee062d49
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-06-09 21:41:33 +0900

    [Silver V] Title: 뒤집기, Time: 60 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,31 +1,45 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
-		
-        // 한 자리 수 배열로 변환
-		char nums[] = line.toCharArray();
-        // 자리 길이
-		int numsLen = nums.length;
-        // 0,1 개수 배열
-		int count[] = new int[2];
-        
-        // 시작 숫자 설정
-		char temp = nums[0];
-		for(int n=1; n<numsLen; n++) {
-			if(temp!=nums[n]) {
-				count[temp-48]++;
-				temp = nums[n];
-			}
-		}
-		
-		// 연속된 수의 끝 처리
-		count[temp-48]++;
-		
+
+    // 결과, 0,1 그룹 개수
+    public static int answer, zeroGroupCount, oneGroupCount;
+
+    // 그룹 카운드 메서드
+    public static void groupCount(char findGroup) {
+        if(findGroup-'0'==0) zeroGroupCount++;
+        else oneGroupCount++;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 문자열 입력
+        String letter = br.readLine();
+
+        // 확인 그룹
+        char findGroup = letter.charAt(0);
+        groupCount(findGroup);
+
+        // 문자열 확인
+        for(int letterIndex=0; letterIndex<letter.length(); letterIndex++) {
+
+            // 그룹
+            char group = letter.charAt(letterIndex);
+
+            // 확인
+            if(findGroup!=group) {
+                findGroup = group;
+                groupCount(findGroup);
+            }
+        }
+
+        // 결과 저장
+        answer = Math.min(zeroGroupCount,oneGroupCount);
+
         // 결과 출력
-		System.out.println(Math.min(count[0], count[1]));
-	}
+        System.out.println(answer);
+    }
 }
