bfab08534100e1f73513adf64dbafe7f6b30ece7
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-06-09 22:20:07 +0900

    [Silver IV] Title: 접미사 배열, Time: 92 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -1,22 +1,52 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> result = new ArrayList<>();
 
-        // 단어 입력
-        String letters = scanner.next();
+    // 문자열
+    public static String letter;
 
-        // 접미사 찾기
-        for(int idx=0; idx<letters.length(); idx++) {
-            result.add(letters.substring(idx));
+    // 접미사 저장 리스트
+    public static ArrayList<String> suffix;
+
+    // 접미사 확인 메서드
+    public static void solve() {
+
+        for(int letterIndex=0; letterIndex<letter.length(); letterIndex++) {
+            suffix.add(letter.substring(letterIndex));
         }
+    }
+
+    // 결과 저장 메서드
+    public static String saveResult() {
+        StringBuilder sb = new StringBuilder();
+
+        for(String word: suffix) {
+            sb.append(word).append("\n");
+        }
+        
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 문자열 입력
+        letter = br.readLine();
+
+        // 접미사 저장 리스트 생성
+        suffix = new ArrayList<>();
+
+        // 접미사 확인
+        solve();
+
         // 정렬
-        Collections.sort(result);
+        Collections.sort(suffix);
 
         // 결과 출력
-        for(String element: result)
-            System.out.println(element);
+        System.out.println(saveResult());
     }
 }
