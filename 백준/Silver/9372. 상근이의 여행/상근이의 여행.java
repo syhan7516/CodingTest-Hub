3a99dfe97e865cd80836aff6b12c9095513f37c7
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-07-16 14:35:43 +0900

    [Silver IV] Title: 상근이의 여행, Time: 436 ms, Memory: 54456 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,61 +1,80 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int find(int group[], int node) {
-        if(group[node]==node) {
-            return node;
-        }
-        else {
-            return group[node] = find(group,group[node]);
+    // 결과, 국가 수, 비행기 수
+    public static int answer, countryCount, airplaneCount;
+
+    // 관계 정보 리스트
+    public static ArrayList<ArrayList<Integer>> relations;
+
+    // 방문 여부 배열
+    public static boolean[] visited;
+
+    // 비행기 타기 메서드
+    public static void solve(int country) {
+
+        // 이동 가능한 비행기 종류 확인
+        for(int relation=0; relation<relations.get(country).size(); relation++) {
+
+            // 이동 가능한 나라
+            int connectedCountry = relations.get(country).get(relation);
+
+            // 미방문 나라인 경우
+            if(!visited[connectedCountry]) {
+                visited[connectedCountry] = true;
+                answer++;
+                solve(connectedCountry);
+            }
         }
     }
 
-    static void union(int group[], int firNode, int secNode) {
-        int firNodeRoot = find(group,firNode);
-        int secNodeRoot = find(group,secNode);
-        group[firNodeRoot] = secNodeRoot;
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         // 테스트 케이스 수 입력
-        int caseNum = Integer.parseInt(br.readLine());
+        int caseCount = Integer.parseInt(br.readLine());
 
-        // 케이스 수만큼 수행
-        for(int caseIdx=0; caseIdx<caseNum; caseIdx++) {
+        // 케이스 수행
+        for(int caseNum=0; caseNum<caseCount; caseNum++) {
 
-            // 국가 수, 비행기 수
+            // 국가 수, 비행기 수 입력
             st = new StringTokenizer(br.readLine());
-            int cityCnt = Integer.parseInt(st.nextToken());
-            int airPlaneCnt = Integer.parseInt(st.nextToken());
+            countryCount = Integer.parseInt(st.nextToken());
+            airplaneCount = Integer.parseInt(st.nextToken());
 
-            // 각 집합 초기화
-            int group[] = new int[cityCnt+1];
-            for(int idx=0; idx<cityCnt+1; idx++) {
-                group[idx] = idx;
+            // 관계 정보 리스트 생성 및 초기화
+            relations = new ArrayList<>();
+            for(int country=0; country<=countryCount; country++) {
+                relations.add(new ArrayList<>());
             }
 
-            // 비행기 확인
-            int count = 0;
-            for(int idx=0; idx<airPlaneCnt; idx++) {
+            // 비행기 종류 입력
+            for(int airplane=0; airplane<airplaneCount; airplane++) {
                 st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-
-                // 집합 확인
-                if(find(group,start)!=find(group,end)) {
-                    union(group,start,end);
-                    count += 1;
-                }
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                relations.get(from).add(to);
+                relations.get(to).add(from);
             }
 
-            // 결과 확인
-            System.out.println(count);
+            // 방문 여부 배열 생성
+            visited = new boolean[countryCount+1];
+
+            // 비행기 타기
+            answer = 0;
+            visited[1] = true;
+            solve(1);
+
+            // 결과 출력
+            System.out.println(answer);
         }
+
     }
 }
