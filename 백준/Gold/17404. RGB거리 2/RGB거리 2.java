8565c46be2a6a1ff365d46cf3ccfe8ee435e6217
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-10-14 18:28:02 +0900

    [Gold IV] Title: RGB거리 2, Time: 72 ms, Memory: 12092 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -1,58 +1,64 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    // 최대값
+    public static final int MAX_VALUE = 1000000;
+
+    // 결과, 집 개수
+    public static int answer, houseCount;
+
+    // 비용 저장 배열, 비용 누적 배열
+    public static int[][] cost, sum;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 집의 수 입력
-        int houseCnt = Integer.parseInt(br.readLine());
+        // 집 개수 입력
+        houseCount = Integer.parseInt(br.readLine());
 
-        // 거리 생성
-        int rgbTown[][] = new int[houseCnt+1][3];
-        int DP[][] = new int[houseCnt+1][3];
+        // 배열 생성
+        cost = new int[houseCount+1][3];
+        sum = new int[houseCount+1][3];
 
-        // RGB 비용 입력
-        for(int h=1; h<=houseCnt; h++) {
+        // 비용 입력
+        for(int house=1; house<=houseCount; house++) {
             st = new StringTokenizer(br.readLine());
-            rgbTown[h][0] = Integer.parseInt(st.nextToken());
-            rgbTown[h][1] = Integer.parseInt(st.nextToken());
-            rgbTown[h][2] = Integer.parseInt(st.nextToken());
+            for(int color=0 ; color<3; color++) {
+                cost[house][color] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        // 집에 색깔 칠하기
-        int result = Integer.MAX_VALUE;
-        for(int rgb=0; rgb<3; rgb++) {
+        // 결과 초기화
+        answer = MAX_VALUE;
 
-            // 첫 집의 색 칠하기
-            for(int first=0; first<3; first++) {
-                // 확인하려는 기준 색깔과 같은 경우
-                if(rgb==first) {
-                    DP[1][first] = rgbTown[1][first];
+        // 시작 색깔 순회
+        for(int color=0; color<3; color++) {
+
+            // 시작 색깔 이외 모두 큰 값으로 초기화
+            for(int startColor=0 ; startColor<3; startColor++) {
+                if(startColor == color) sum[1][startColor] = cost[1][startColor];
+                else sum[1][startColor] = MAX_VALUE;
+            }
+
+            // 조건에 맞게 색칠 비용 확인
+            for (int house=2; house<=houseCount; house++) {
+                sum[house][0] = Math.min(sum[house-1][1], sum[house-1][2]) + cost[house][0];
+                sum[house][1] = Math.min(sum[house-1][0], sum[house-1][2]) + cost[house][1];
+                sum[house][2] = Math.min(sum[house-1][0], sum[house-1][1]) + cost[house][2];
+            }
+
+            // 최솟값 확인
+            for(int index=0 ; index<3; index++)
+                if(index != color) {
+                    answer = Math.min(answer, sum[houseCount][index]);
                 }
-                // 다른 경우
-                else {
-                    DP[1][first] = (int)1e9;
-                }
-            }
-
-            // 나머짖 집 색 칠하기
-            for(int h=2; h<=houseCnt; h++) {
-                DP[h][0] = Math.min(DP[h-1][1],DP[h-1][2]) + rgbTown[h][0];
-                DP[h][1] = Math.min(DP[h-1][0],DP[h-1][2]) + rgbTown[h][1];
-                DP[h][2] = Math.min(DP[h-1][0],DP[h-1][1]) + rgbTown[h][2];
-            }
-
-            // 마지막 집 색깔 조건 고려
-            for(int h=0; h<3; h++) {
-                if(h!=rgb && result>DP[houseCnt][h])
-                    result = DP[houseCnt][h];
-            }
         }
 
         // 결과 출력
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
