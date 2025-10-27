73e4ead5e03bb80557ed36706fc70d1ab94786ab
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2025-10-27 23:09:44 +0900

    [Gold IV] Title: 그룹 나누기 (Subset), Time: 64 ms, Memory: 11852 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/16132. 그룹 나누기 （Subset）/그룹 나누기 （Subset）.java
+++ 백준/Gold/16132. 그룹 나누기 （Subset）/그룹 나누기 （Subset）.java
@@ -6,30 +6,13 @@ import java.util.*;
 public class Main {
 
     // 학생 수
-    public static long num;
+    public static long num, target;
 
     // DP 배열 생성
     public static long[][] DP;
 
     // 경우의 수 확인 메서드
     public static long solve(int currentNum, int count) {
-
-        // 합 계산
-        long target = num * (num+1) /2;
-
-        // 전체 합이 홀수인 경우
-        if(target%2 != 0) return 0;
-
-        // 합의 절반
-        target /= 2;
-
-        // DP 테이블 생성
-        DP = new long[(int)(num+2)][(int)(target+2)];
-
-        // 초기화
-        for(int index=0; index<= num+1; index++) {
-            Arrays.fill(DP[index], -1);
-        }
 
         // 학생 수보다 많거나 합을 초과하는 경우
         if (currentNum>num || count>target) return 0;
@@ -50,7 +33,29 @@ public class Main {
         // 학생 수 입력
         num = Long.parseLong(br.readLine());
 
-        // 경우의 수 확인
-        System.out.println(solve(1, 0));
+        // 합 계산
+        target = num * (num+1) / 2;
+
+        // 전체 합이 홀수인 경우
+        if(target%2 != 0) {
+            System.out.println(0);
+        }
+        
+        else {
+            
+            // 합의 절반
+            target /= 2;
+
+            // DP 테이블 생성
+            DP = new long[(int)(num+2)][(int)(target+2)];
+
+            // 초기화
+            for(int index=0; index<=num+1; index++) {
+                Arrays.fill(DP[index], -1);
+            }
+
+            // 경우의 수 확인
+            System.out.println(solve(1, 0));
+        }
     }
 }
